387febac36248353a91de226b9265f6fa25a3094
rodnovspb <rodnovspb@mail.ru>
2021-11-26 02:33:05 +0300

    изучение React

--- test-http-server/public/client.js
+++ test-http-server/public/client.js
@@ -3,23 +3,27 @@ let div = document.querySelector('div')
 let i = 0
 
       btn1.addEventListener('click', function func() {
+
          if(i==0){
                fetch('ajax1.html').then(response=>{return response.text()}).then(text=>{div.innerHTML+=text})
-               i++
-               btn1.removeEventListener('click', func)
+
          }
-         }
+      if(i==1){
+      fetch('ajax2.html').then(response=>{return response.text()}).then(text=>{div.innerHTML+=text})
+      }
+
+      if(i==2){
+      fetch('ajax3.html').then(response=>{return response.text()}).then(text=>{div.innerHTML+=text})
+      }
+i++
+      if(i==3){
+      i=0
+      }
+      }
       )
 
 
-      btn1.addEventListener('click', function func() {
-              if(i==1){
-                    fetch('ajax2.html').then(response=>{return response.text()}).then(text=>{div.innerHTML+=text})
-                    i++
-                    btn1.removeEventListener('click', func)
-              }
-        }
-      )
+
 
 
 
