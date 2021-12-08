8764ca66ec1465af051282d0bd7661d597873e16
rodnovspb <rodnovspb@mail.ru>
2021-12-09 00:46:48 +0300

    node js

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,25 +1,17 @@
 import fs from 'fs'
 // node index.js
 
-fs.readFile('1.txt', 'utf8', function (e,data1) {
+
+fs.readFile('1.txt', 'utf8', function (e, data1) {
     if(!e){
       fs.readFile('2.txt', 'utf8', function (e, data2) {
           if(!e){
             fs.readFile('3.txt', 'utf8', function (e, data3) {
                 if(!e){
-                  fs.readFile('4.txt', 'utf8', function (e, data4) {
-                      if(!e){
-                        fs.readFile('5.txt', 'utf8', function (e, data5) {
-                            if(!e){
-                              console.log(~~data1+~~data2+~~data3+~~data4+~~data5)
-                            }
-                            else {
-                              console.log('ошибка чтения файла 5')
-                            }
-                        })
-                      }
-                      else {
-                        console.log('ошибка чтения файла 4')
+                  let a = ~~data1+~~data2+~~data3 + '!'
+                  fs.writeFile('8.txt', a, function (e) {
+                      if(e){
+                        console.log('ошибка записи')
                       }
                   })
                 }
@@ -37,6 +29,3 @@ fs.readFile('1.txt', 'utf8', function (e,data1) {
       console.log('ошибка чтения файла 1')
     }
 })
-
-
-
