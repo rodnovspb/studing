81cdb2dc72104015d0a72d4d361315acd4b99ecb
rodnovspb <rodnovspb@mail.ru>
2021-12-09 00:48:54 +0300

    node js

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,14 +2,14 @@ import fs from 'fs'
 // node index.js
 
 
-fs.readFile('1.txt', 'utf8', function (e, data1) {
+fs.readFile('1.txt', 'utf8',  (e, data1) => {
     if(!e){
-      fs.readFile('2.txt', 'utf8', function (e, data2) {
+      fs.readFile('2.txt', 'utf8',  (e, data2) => {
           if(!e){
-            fs.readFile('3.txt', 'utf8', function (e, data3) {
+            fs.readFile('3.txt', 'utf8',  (e, data3) => {
                 if(!e){
                   let a = ~~data1+~~data2+~~data3 + '!'
-                  fs.writeFile('8.txt', a, function (e) {
+                  fs.writeFile('8.txt', a, e => {
                       if(e){
                         console.log('ошибка записи')
                       }
