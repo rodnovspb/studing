8456e0ace3c0be4bbbf07f1e0f115615cce481ee
rodnovspb <rodnovspb@mail.ru>
2021-12-09 00:58:55 +0300

    node js

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,30 +2,20 @@ import fs from 'fs'
 // node index.js
 
 
-fs.readFile('1.txt', 'utf8',  (e, data1) => {
-    if(!e){
-      fs.readFile('2.txt', 'utf8',  (e, data2) => {
-          if(!e){
-            fs.readFile('3.txt', 'utf8',  (e, data3) => {
-                if(!e){
-                  let a = ~~data1+~~data2+~~data3 + '!'
-                  fs.writeFile('8.txt', a, e => {
-                      if(e){
-                        console.log('ошибка записи')
-                      }
-                  })
-                }
-                else {
-                  console.log('ошибка чтения файла 3')
-                }
-            })
+fs.readFile('readme1.txt', 'utf8', (err, data1) => {
+  if (!err) {
+    fs.readFile('readme2.txt', 'utf8', (err, data2) => {
+      if (!err) {
+        fs.writeFile('readme.txt', data1 + data2, err => {
+          if (err) {
+            console.log('ошибка записи файла');
           }
-          else {
-            console.log('ошибка чтения файла 2')
-          }
-      })
-    }
-    else {
-      console.log('ошибка чтения файла 1')
-    }
-})
+        });
+      } else {
+        console.log('ошибка чтения файла readme2');
+      }
+    });
+  } else {
+    console.log('ошибка чтения файла readme1');
+  }
+});
