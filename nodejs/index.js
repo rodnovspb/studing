6cacf338cf0cdc8661132c06b7dae4c0c44ca89b
rodnovspb <rodnovspb@mail.ru>
2021-12-10 13:12:53 +0300

    10.12.2021

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,20 +2,16 @@ import fs from 'fs'
 // node index.js
 
 
-fs.readFile('readme1.txt', 'utf8', (err, data1) => {
-  if (!err) {
-    fs.readFile('readme2.txt', 'utf8', (err, data2) => {
-      if (!err) {
-        fs.writeFile('readme.txt', data1 + data2, err => {
-          if (err) {
-            console.log('ошибка записи файла');
-          }
-        });
-      } else {
-        console.log('ошибка чтения файла readme2');
-      }
-    });
-  } else {
-    console.log('ошибка чтения файла readme1');
+async function f() {
+  try {
+    let a = await fs.promises.readFile('1.txt', 'utf8')
+    let b = await fs.promises.readFile('2.txt', 'utf8')
+    let c = await fs.promises.readFile('3.txt', 'utf8')
+    console.log(a+b+c)
   }
-});
+  catch (e) {
+    console.log('error')
+  }
+
+}
+f()
