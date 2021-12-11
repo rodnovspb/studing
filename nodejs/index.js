4cb28fa7fb3ffb15466a404427b9145dd6185be8
rodnovspb <rodnovspb@mail.ru>
2021-12-11 23:56:16 +0300

    сервер

--- nodejs/index.js
+++ nodejs/index.js
@@ -6,18 +6,22 @@ import http from 'http'
 
 // node index.js
 
-http.createServer(async (request, response) => {
-  if (request.url != '/favicon.ico') {
-    let path = 'root' + request.url;
+http.createServer(async (request, response)=>{
 
+  let path = 'root'+request.url + '/index.html'
+  let status
+  let data
 
-
-      path += 'index.html';
-
-    let text = await fs.promises.readFile(path, 'utf8');
-
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write(text);
-    response.end();
+  try {
+    data = await fs.promises.readFile(path, 'utf8')
+    status=200
   }
-}).listen(3000);
+  catch (e) {
+    status=404
+    path='root/dir/404.html'
+    data = await fs.promises.readFile(path, 'utf8')
+  }
+  response.writeHead(status, {'Content-Type': 'text/html'})
+  response.write(data)
+  response.end()
+}).listen(3000)
