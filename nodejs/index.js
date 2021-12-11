9ef268913062e53ecea62ab977115a8a7d72cef2
rodnovspb <rodnovspb@mail.ru>
2021-12-11 19:07:54 +0300

    сервер

--- nodejs/index.js
+++ nodejs/index.js
@@ -7,29 +7,21 @@ import http from 'http'
 // node index.js
 
 
-http.createServer((request, response)=>{
+http.createServer(async (request, response)=> {
   let text
   let status = 200
-  let obj = {
-    '/page1': '1',
-    '/page2': '2',
-    '/page3': '3',
-  }
 
   switch (request.url) {
-    case '/page1' : text = obj['/page1']
+    case "/page" :
+      text = await fs.promises.readFile("page.html", "utf8")
       break
-    case '/page2' : text = obj['/page2']
-      break
-    case '/page3' : text = obj['/page3']
-      break
-    default : text = "not found"
-              status = 404
+    default :
+      text = 'not found';
+      status = 404
       break
   }
-
   response.writeHead(status, {"Content-Type": "text/html"})
-  response.write("<h1>"+text+"</h1>")
+  response.write(text)
   response.end()
 
 }).listen(3000)
