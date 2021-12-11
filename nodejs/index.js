b39f4b0fb5de32fb2c868ca410c4d10bf0a6937f
rodnovspb <rodnovspb@mail.ru>
2021-12-11 22:37:53 +0300

    сервер

--- nodejs/index.js
+++ nodejs/index.js
@@ -6,56 +6,18 @@ import http from 'http'
 
 // node index.js
 
-http.createServer(async (request, response)=>{
+http.createServer(async (request, response) => {
+  if (request.url != '/favicon.ico') {
+    let path = 'root' + request.url;
 
 
 
+      path += 'index.html';
 
-    let path = './root'+request.url
-      if(/css/.test(path)){
-        path = './root'+"/dir"+request.url
-        let text = await fs.promises.readFile(path)
-        response.writeHead(200, {"Content-Type": 'text/css'})
-        response.write(text)
-        response.end()
-      }
-      else if(/js/.test(path)){
-        path = './root'+"/dir"+request.url
-        let text = await fs.promises.readFile(path)
-        response.writeHead(200, {"Content-Type": 'text/javascript'})
-        response.write(text)
-        response.end()
-      }
-      else if(/png/.test(path)){
-        path = './root'+"/dir"+request.url
-        let text = await fs.promises.readFile(path)
-        response.writeHead(200, {"Content-Type": 'image/png'})
-        response.write(text)
-        response.end()
-      }
-      else if(/dir/.test(path)){
-       path += '/index.html';
-        let text = await fs.promises.readFile(path)
-        response.writeHead(200, {"Content-Type": 'text/html'})
-        response.write(text)
-        response.end()
-      }
-      else {
-        let text = 'не найдена'
-        response.writeHead(404, {"Content-Type": 'text/html'})
-        response.write(text)
-        response.end()
-      }
+    let text = await fs.promises.readFile(path, 'utf8');
 
-
-
-
-
-
-
-
-
-
-
-
-}).listen(3000)
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(text);
+    response.end();
+  }
+}).listen(3000);
