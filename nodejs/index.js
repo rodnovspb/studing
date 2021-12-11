49a9c298de90ebf161376b744d0c9eef3d18fd5c
rodnovspb <rodnovspb@mail.ru>
2021-12-11 19:44:10 +0300

    сервер

--- nodejs/index.js
+++ nodejs/index.js
@@ -7,22 +7,35 @@ import http from 'http'
 // node index.js
 
 
-http.createServer(async (request, response)=> {
-  let text
-  let status = 200
-
-  switch (request.url) {
-    case "/page" :
-      text = await fs.promises.readFile("page.html", "utf8")
-      break
-    default :
-      text = 'not found';
-      status = 404
-      break
+http.createServer(async (request, response)=>{
+  let obj = {
+    '/page1': '19.html',
+    '/page2': '20.html',
+    '/page3': '21.html',
   }
-  response.writeHead(status, {"Content-Type": "text/html"})
-  response.write(text)
-  response.end()
+
+  if (request.url != '/favicon.ico') {
+    let text
+    let status = 200
+
+    switch (request.url) {
+      case '/page1' : text = await fs.promises.readFile(obj['/page1'], 'utf8')
+        break
+      case '/page2' : text = await fs.promises.readFile(obj['/page2'], 'utf8')
+        break
+      case '/page3' : text = await fs.promises.readFile(obj['/page3'], 'utf8')
+        break
+      default : text = 'not found'; status = 404
+        break
+    }
+
+    response.writeHead(status, {'Content-Type': 'text/html'})
+    response.write(text)
+    response.end()
+
+  }
+
+
 
 }).listen(3000)
 
