02cbc64dcdfab3c8d689addc438dc448f542c7e9
rodnovspb <rodnovspb@mail.ru>
2021-12-11 18:30:54 +0300

    сервер

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,15 +1,33 @@
 import fs from 'fs'
 import __dirname from './__dirname.js';
 import {constants} from 'fs'
+import {createGzip} from 'zlib'
+import http from 'http'
+
 // node index.js
 
+let i=30
+http.createServer((request, response) => {
+let text
+  let status = 200
+  if(request.url=='/page1'){
+    text='text1'
+  }
+  else if(request.url=='/page2'){
+    text='text2'
+  }
+  else if(request.url=='/page3'){
+    text='text3'
+  }
+  else {
+    text='page not found'
+    status=404
+  }
+  response.writeHead(status, {'Content-Type': 'text/html'})
+  response.write("<h1>"+text+"</h1>")
+  response.end()
 
-let read = fs.createReadStream('7.txt', 'utf8')
+
+}).listen(3000);
 
 
-    for(let i=1; i<4; i++){
-      let write = fs.createWriteStream(i+'.txt')
-      read.on('data', function (chunk) {
-          write.write(chunk)
-      })
-    }
