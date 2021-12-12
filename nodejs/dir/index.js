9fecb587f26f47931dd7307c11b491ea58d5a67b
rodnovspb <rodnovspb@mail.ru>
2021-12-13 02:14:01 +0300

    задачи

--- nodejs/dir/index.js
+++ nodejs/dir/index.js
@@ -9,14 +9,17 @@ http.createServer(async (request, response)=>{
   console.log(request.url)
  try {
     if(!/css|js|img/.test(request.url)){
-      let mainPath = '.'+request.url + 'layout.html'
-      let titlePath = '.'+request.url + 'name/'+'title.html'
-      let contentPath = '.'+request.url + 'name/'+'content.html'
+      let mainPath = 'layout.html'
+      let titlePath = './name/'+'title.html'
+      let contentPath = './name/content.html'
+      let metaPath = '.'+request.url + 'meta.html'
       layout = await fs.promises.readFile(mainPath, 'utf8')
+      let meta = await fs.promises.readFile(metaPath, 'utf-8')
       let title = await fs.promises.readFile(titlePath, 'utf8')
       let content = await fs.promises.readFile(contentPath, 'utf8')
       layout = layout.replace(/{% get title %}/,   title);
       layout = layout.replace(/{% get content %}/, content)
+      layout = layout.replace(/{% get meta %}/, meta)
       response.writeHead(status, {'Content-Type': 'text/html'})
       console.log('1 иф')
     }
