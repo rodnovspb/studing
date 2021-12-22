81233d26d78dcfe7706c19a53b4dfda2b0a8b9f0
rodnovspb <rodnovspb@mail.ru>
2021-12-22 19:27:51 +0300

    express

--- express/index.js
+++ express/index.js
@@ -3,15 +3,14 @@ let app = express();
 
 // node index.js
 
-app.get('/1/', function (req, res) {
-  res.send('page1')
+app.get('/1.html/', function (req, res) {
+  res.send('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum facere impedit laboriosam perspiciatis quae reprehenderit similique tempora. Aliquam animi eius ipsam neque obcaecati placeat quae quos similique voluptates! Alias aspernatur assumenda dolorum eaque eius, esse impedit ipsa itaque laboriosam maiores neque nulla odio provident quae sit? Aliquam, nesciunt nostrum? Eaque?')
 })
-app.get('/2/', function (req, res) {
-  res.send('page2')
+app.get('/2.html', function (req, res) {
+  res.send('вторая страница')
 })
-
-app.use(function (req,res) {
-  res.status(404).send('404 ошибка')
+app.use(function (req, res) {
+  res.status(404).send('error')
 })
 
 app.listen(3000, function() {
