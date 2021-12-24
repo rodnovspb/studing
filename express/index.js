30650e5c047445c5330b9601fed97984d6d14bb2
rodnovspb <rodnovspb@mail.ru>
2021-12-25 00:20:47 +0300

    express

--- express/index.js
+++ express/index.js
@@ -13,17 +13,19 @@ app.set('view engine', 'hbs');
 
 // node index.js
 
-let titles = {
-  index:    'главная страница',
-  about:    'о нас',
-  conctacs: 'контакты',
-  price:    'наш прайс'
-}
 
-app.get('/:page', function (req, res) {
-  res.render(req.params.page, {title: titles[req.params.page]})
+
+app.get('/', function (req, res) {
+  res.render('admin');
 })
 
+
+
+
+
+app.use(function (req, res) {
+  res.status(404).send('error')
+})
 
 
 
