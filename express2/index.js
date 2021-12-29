d45d325b62bde17ec405441a76dddb3fad99c1b9
rodnovspb <rodnovspb@mail.ru>
2021-12-29 20:46:05 +0300

    express куки

--- express2/index.js
+++ express2/index.js
@@ -1,4 +1,5 @@
 import expressHandlebars from 'express-handlebars';
+import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import __dirname from './__dirname.js';
 const handlebars = expressHandlebars.create({
@@ -17,45 +18,22 @@ app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({extended: true}));
 // node index.js
 
+let secret = 'qwerty'
+app.use(cookieParser(secret))
 
-app.get('/', function (req, res) {
-    let horoscope
-    if(req.query.submit){
-       horoscope = getHoroscope(req.query.date)
-    }
-    res.render('form', {
-        query: req.query,
-        horoscope
-    })
+app.get('/write', function (req, res) {
+    res.cookie('myCookies', 'мои куки')
+    res.send('записываю куки')
 })
 
-let Horoscope = {
-    'ves': ['весы плохо', 'весы нормально', "весы хорошо"],
-    'rak': ['рак плохо', 'рак нормально', "рак хорошо"],
-    'deva': ['дева плохо', 'дева нормально', "дева хорошо"],
-    'scorp': ['скорпион плохо', 'скорпион нормально', "скорпион хорошо"]
-}
-function getHoroscope (date){
-    let horoscope
-    let arr = date.split('-')
-    let month = arr[1]
-    let day = arr[2]
-    let date1 = month + '.'+day
-    if(date1>='00.01' && date1<='03.31'){
-        horoscope='ves'
-    }
-    else if(date1>'03.31' && date1<='06.31'){
-        horoscope='rak'
-    }
-    else if(date1>'06.31' && date1<='09.31'){
-        horoscope='deva'
-    }
-    else if(date1>'09.31' && date1<='11.31'){
-        horoscope='scorp'
-    }
-    return Horoscope[horoscope][1]
-}
-
+app.get('/read', function (req, res) {
+    res.cookie('myCookies', 'мои куки')
+    res.send('Читаю куки:' + req.cookies['myCookies'])
+})
+app.get('/delete', function (req, res) {
+    res.clearCookie('myCookies')
+    res.send('Удаляю куки')
+})
 
 
 
