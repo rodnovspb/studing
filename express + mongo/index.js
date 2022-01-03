8b732d7e69817a271cacc06c4077634e4221748e
rodnovspb <rodnovspb@mail.ru>
2022-01-04 00:53:32 +0300

    express + mongo

--- express + mongo/index.js
+++ express + mongo/index.js
@@ -1,57 +1,54 @@
+import expressHandlebars from 'express-handlebars';
+const handlebars = expressHandlebars.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+
+import express from 'express';
+let app = express();
+
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs');
+
 import mongodb from 'mongodb';
 let mongoClient = new mongodb.MongoClient('mongodb://localhost:27017/', {
   useUnifiedTopology: true
 });
 
-let obj = [
-  {
-    "name": "cloth1",
-    "sizes": [1, 2],
-    "colors": ["black", "blue"]
-  },
-  {
-    "name": "cloth2",
-    "sizes": [1, 2, 3],
-    "colors": ["black", "white"]
-  },
-  {
-    "name": "cloth3",
-    "sizes": [2, 3, 4],
-    "colors": ["green", "blue"]
-  },
-  {
-    "name": "cloth4",
-    "sizes": [4, 5, 6],
-    "colors": ["black", "blue", "green"]
-  },
-];
-
 // node index.js
-
 
 mongoClient.connect(async function (error, mongo) {
 
-try {
-  let coll = mongo.db('test').collection('clothes')
-  let res = await coll.find({"sizes": {$elemMatch: {$gt: 2, $lt: 5}}}).toArray()
-  console.log(res)
+if(!error) {
+  let coll = mongo.db('test').collection('users')
+
+ app.get('/users/:name', async function (req, res) {
+    let name = req.params.name
+    let user = await coll.findOne({name : name})
+    if(user){
+      res.render('user', {user: user})
+    }
+    else {
+      res.render('error')
+    }
+
+
+ })
+  app.get('/users', async function (req, res) {
+    let users = await coll.find().toArray()
+    res.render('users', {users: users})
+  })
 }
 
-catch (e) {
-  console.log(e)
+else {
+  console.log(error)
 }
 
 
+})
 
 
 
-
-
-
-
-
-
-
-
+app.listen(3000, function () {
 
 })
