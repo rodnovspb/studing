cf9e2676ab45da899744d2e8f72a0fafaa6f6acf
rodnovspb <rodnovspb@mail.ru>
2022-01-04 03:55:00 +0300

    закончил монго

--- express + mongo/index.js
+++ express + mongo/index.js
@@ -1,4 +1,5 @@
 import expressHandlebars from 'express-handlebars';
+import bodyParser from 'body-parser';
 const handlebars = expressHandlebars.create({
   defaultLayout: 'main',
   extname: 'hbs'
@@ -9,21 +10,23 @@ let app = express();
 
 app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 import mongodb from 'mongodb';
 let mongoClient = new mongodb.MongoClient('mongodb://localhost:27017/', {
   useUnifiedTopology: true
 });
 
+
 // node index.js
 
 mongoClient.connect(async function (error, mongo) {
-
-if(!error) {
   let coll = mongo.db('test').collection('users')
   let collProd = mongo.db('test').collection('prods')
 
- app.get('/users/:name', async function (req, res) {
+
+if(!error) {
+  app.get('/users/:name', async function (req, res) {
     let name = req.params.name
     let user = await coll.findOne({name : name})
     if(user){
@@ -32,12 +35,52 @@ if(!error) {
     else {
       res.render('error')
     }
-
-
  })
   app.get('/users', async function (req, res) {
     let users = await coll.find().toArray()
     res.render('users', {users: users})
+  })
+
+  app.get('/users/delete/:name', async function (req, res) {
+      let user = req.params.name
+      await coll.deleteOne({name: user})
+      res.send('удален')
+  })
+
+  app.get('/users/edit/:name', async function (req, res) {
+    let name = req.params.name
+    let user = await coll.findOne({name: name})
+    if(user){
+      res.render('edit', {name: user.name, salary: user.salary})
+    }
+    else {
+      res.render('error')
+    }
+
+  })
+
+  app.post('/users/edit/:name', async function (req, res) {
+    let user = req.body
+    await coll.updateOne({name: user.name}, {$set: user})
+    res.send('Юзер обновлен')
+  })
+
+  app.get('/prods/edit/:name', async function (req, res) {
+    let name = req.params.name
+    let prod = await collProd.findOne({name: name})
+    if(prod){
+      res.render('editProd', {prod})
+    }
+    else {
+      res.render('error')
+    }
+
+  })
+
+  app.post('/prods/edit/:name', async function (req, res) {
+    let prod = await req.body
+    await collProd.updateOne({name: prod.name}, {$set: prod})
+    res.redirect('/prods')
   })
 
   app.get('/prods/:num', async function (req, res) {
@@ -54,6 +97,33 @@ if(!error) {
     let prods = await collProd.find().toArray()
     res.render('prods', {prods})
   })
+
+  app.get('/prods/delete/:name', async function (req, res) {
+    let prod = req.params.name
+    await collProd.deleteOne({name: prod})
+    res.render('delete', {name: prod})
+
+  })
+  app.get('/user/add', function (req, res) {
+    res.render('add')
+  })
+  app.post('/user/add', async function (req, res) {
+      let data = req.body
+      await coll.insertOne(data)
+      res.send('юзер успешно добавлен')
+  })
+
+  app.get('/prod/add', function (req, res) {
+    res.render('addProd')
+  })
+  app.post('/prod/add', async function (req, res) {
+    let prod = req.body
+    await collProd.insertOne(prod)
+    res.redirect('/prods')
+  })
+
+
+
 
 }
 
