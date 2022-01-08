4f7e9c81dd48454b5913e7e59272df065cb03535
rodnovspb <rodnovspb@mail.ru>
2022-01-08 21:21:11 +0300

    изучение php

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -1,10 +1,20 @@
 <?php
 
+$kilo = 1024;
+$mega = 1024*$kilo;
+$giga = 1024*1024*$kilo;
+$tera = 1024*1024*1024*$kilo;
 
-$a = '1.jpg';
-$alt = "nature";
 
-echo "<img src='".$a."' alt='".$alt."' </img>"
+echo $tera/$kilo
+
+
+
+
+
+
+
+
 
 
 
