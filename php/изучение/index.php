57cd4f874cce7dc609bfb424f2be5773da90f328
rodnovspb <rodnovspb@mail.ru>
2022-01-08 18:27:27 +0300

    изучение php

---
+++ php/изучение/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Документ</title>
+<style>
+	body {
+        font-size: 30px;
+	}
+
+</style>
+</head>
+<body>
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+mb_internal_encoding("UTF-8");
+
+include 'code.php';
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
