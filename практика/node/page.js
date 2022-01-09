14c21609b7a2817293c224179ffa98a15dbc3b93
rodnovspb <rodnovspb@mail.ru>
2022-01-09 21:57:04 +0300

    node

--- практика/node/page.js
+++ практика/node/page.js
@@ -1,8 +1,9 @@
 import http from 'http'
 
 http.createServer(((req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8;")
   if(req.url=='/'){
-    res.write('request main page')
+    res.write('request main Привет, мир page')
   }
   if(req.url=='/hello'){
     res.write('hello')
