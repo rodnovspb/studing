0b784ff80288e2603797effa0a93f086f27ede5c
rodnovspb <rodnovspb@mail.ru>
2022-01-11 01:07:51 +0300

    php

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -1,24 +1,19 @@
 <?php
 
 
-$arr = ['a' => 1, 'b' => 2, 'c' => 3];
-
-$key = "b";
-
-echo $arr[$key]
 
 
 
+$age = 19;
 
+if($age >10 and $age <99) {
+    $age = (string)$age;
+    $sum = $age[0] + $age[1];
+    if(strlen($sum)<2) echo 'сумма цифр однозначна';
+    else echo 'двузначна';
+} else echo 'меньше 10 или больше 99';
 
-
-
-
-
-
-
-
-
+echo strlen((string)23);
 
 
 
