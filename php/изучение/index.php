85a6c602577a7aade3d88ba7ae3e97c3f6d164d0
rodnovspb <rodnovspb@mail.ru>
2022-01-12 21:40:22 +0300

    php

--- php/изучение/index.php
+++ php/изучение/index.php
@@ -16,17 +16,21 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
-mb_internal_encoding("UTF-8");
+//mb_internal_encoding("UTF-8");
 
 include 'code.php';
 ?>
 
 
+<script>
 
 
 
 
 
+
+
+</script>
 
 
 
