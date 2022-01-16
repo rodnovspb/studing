b3ec01059ad8983ed89ced3cfeddce6528531d55
UpdatedUserName <rodnovspb@mail.ru>
2022-01-16 09:14:20 +0300

    Revert "15.01.22"
    This reverts commit daae478a

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -4,22 +4,29 @@
 
 
 
-$arr = [
-    '2019-12-29'=> ['name1', 'name2', 'name3', 'name4'],
-    '2019-12-30'=> ['name5', 'name6', 'name7'],
-    '2019-12-31'=> ['name8', 'name9'],
-];
 
-$obj = [];
-foreach ($arr as $key=>$item){
-    foreach ($item as $value){
-        $a['date'] = $key;
-        $a['event'] = $value;
-        $obj[] = $a;
-    }
-}
 
-var_dump($obj);
+
+
+
+
+$url = 'ab--cd--ef';
+
+echo strstr($url, '--cd')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
