a7134bfed616c4f964b23730d817143d5a6bf563
UpdatedUserName <rodnovspb@mail.ru>
2022-01-18 04:31:25 +0300

    18.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -6,30 +6,19 @@
 
 
 
+$today = date_create('2022-01-18');
+
+date_modify($today, '-100 days');
+
+$day = date_format($today, 'Y-m-d');
+
+echo date('w', strtotime($day))
 
 
 
 
 
 
-$year = '2022';
-
-$w = 5;
-$d = 13;
-$arr = [];
-
-for($i = 1; $i<=12; $i++){
-    $timestamp = strtotime($year.'-'.$i."-".$d);
-    $day = date('w', $timestamp);
-    if($day==$w){
-        array_push($arr, date('d-m-Y', $timestamp));
-    }
-
-}
-
-echo '<pre>';
-print_r($arr);
-echo '</pre>';
 
 
 
