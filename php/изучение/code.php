0f1f2bba02bcf034065978d3234a6728ce3cef83
UpdatedUserName <rodnovspb@mail.ru>
2022-01-20 00:51:02 +0300

    19.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -18,22 +18,10 @@
 
 
 
+function func($num) {
+        return $num >= 0;
 
-function check($arr){
-    for ($i=0; $i<count($arr); $i++){
-        if($arr[$i]===$arr[$i++]){
-            return true;
-        }
-    }
-    return false;
 }
-
-var_dump(check([1,2,3,4,5,5,6,7]));
-
-
-
-
-
 
 
 
