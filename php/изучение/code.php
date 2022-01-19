3e9bffc0ce8ea450bbf64eb66bd810c4680d1660
UpdatedUserName <rodnovspb@mail.ru>
2022-01-20 00:33:16 +0300

    19.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -19,20 +19,14 @@
 
 
 
-function delElem ($elem, $arr){
-   for($i=0; $i<count($arr); $i++){
-       if($elem===$arr[$i]){
-           array_splice($arr, $i, 1);
-       }
-   }
-   return $arr;
+function checkPositiv($arr){
+    foreach ($arr as $item){
+        if($item<0){
+            return false;
+        }
+    }
+    return true;
 }
-
-
-echo '<pre>';
-print_r(delElem(5, [1,2,3,4,5,6,7,5,8]));
-echo '</pre>';
-
 
 
 
