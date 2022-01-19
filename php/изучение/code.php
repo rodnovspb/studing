a66a4ba07652214c2c841a9cdfb1b1937eddd862
UpdatedUserName <rodnovspb@mail.ru>
2022-01-20 02:00:59 +0300

    19.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -14,14 +14,29 @@
 
 
 
+function getDivisiors($num){
+    $arr=[];
+    for($i=2; $i<$num; $i++){
+        if($num%$i===0){
+            $arr[]=$i;
+        }
+    }
+    return $arr;
+};
+
+function findMean($arr){
+    if(!empty($arr)){
+    return array_sum($arr)/count($arr);
+ }
+};
+
+echo findMean(getDivisiors(4))
 
 
 
 
-function func($num) {
-        return $num >= 0;
 
-}
+
 
 
 
