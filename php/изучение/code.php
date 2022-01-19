f8bc92871501f621fcee789cd8bb17ed25bed058
UpdatedUserName <rodnovspb@mail.ru>
2022-01-20 00:46:40 +0300

    19.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -19,17 +19,16 @@
 
 
 
-function check($num){
-    $n = str_split($num, 1);
-    foreach ($n as $item){
-        if($item%2===0){
-            return false;
+function check($arr){
+    for ($i=0; $i<count($arr); $i++){
+        if($arr[$i]===$arr[$i++]){
+            return true;
         }
     }
-    return true;
+    return false;
 }
 
-var_dump(check(14));
+var_dump(check([1,2,3,4,5,5,6,7]));
 
 
 
