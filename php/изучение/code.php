eafd47dc801c6e17b40d42e260edc76f11507cb9
UpdatedUserName <rodnovspb@mail.ru>
2022-01-21 00:08:59 +0300

    20.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -6,31 +6,48 @@
 
 
 
+echo "<pre>";
+print_r(getPrimeDivisors(853));
+print_r(getDivisors(853));
+echo "</pre>";
 
 
 
 
 
 
-
-
-function getDivisiors($num){
-    $arr=[];
-    for($i=2; $i<$num; $i++){
-        if($num%$i===0){
-            $arr[]=$i;
+// Функция, которая находит массив простых делителей
+function getPrimeDivisors($num){
+    $arr = [];
+    $div = getDivisors($num);
+    foreach ($div as $item){
+        if(isSimple($item)){
+            $arr[] = $item;
         }
     }
     return $arr;
-};
+}
 
-function findMean($arr){
-    if(!empty($arr)){
-    return array_sum($arr)/count($arr);
- }
-};
+// Функция, которая находит массив делителей
 
-echo findMean(getDivisiors(4))
+function getDivisors($num){
+    $arr = [];
+    $arr1 = range(2, $num-1);
+    foreach ($arr1 as $item){
+        if($num % $item ===0){
+            $arr[] = $item;
+        }
+    }
+    return $arr;
+}
+
+// Функция, которая проверяет число на простоту
+    function isSimple($num){
+        for($i=2; $i<$num; $i++){
+            if($num % $i ===0) return false;
+        }
+        return true;
+    }
 
 
 
