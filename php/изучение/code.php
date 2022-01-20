c7510e766b7e18dee1783b65a3394737ccc74217
UpdatedUserName <rodnovspb@mail.ru>
2022-01-21 00:21:27 +0300

    20.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -6,48 +6,38 @@
 
 
 
-echo "<pre>";
-print_r(getPrimeDivisors(853));
-print_r(getDivisors(853));
-echo "</pre>";
+$i = 1;
 
+function func($num){
+    echo $num;
+    $num++;
 
-
-
-
-
-// Функция, которая находит массив простых делителей
-function getPrimeDivisors($num){
-    $arr = [];
-    $div = getDivisors($num);
-    foreach ($div as $item){
-        if(isSimple($item)){
-            $arr[] = $item;
-        }
+    if ($num <= 10){
+        func($num); // здесь функция вызывает сама себя
     }
-    return $arr;
 }
+func($i);
 
-// Функция, которая находит массив делителей
 
-function getDivisors($num){
-    $arr = [];
-    $arr1 = range(2, $num-1);
-    foreach ($arr1 as $item){
-        if($num % $item ===0){
-            $arr[] = $item;
-        }
-    }
-    return $arr;
-}
 
-// Функция, которая проверяет число на простоту
-    function isSimple($num){
-        for($i=2; $i<$num; $i++){
-            if($num % $i ===0) return false;
-        }
-        return true;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
