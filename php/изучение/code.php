e56d1e6ae1bf4688d22ffcafb36e3534b5b30dce
UpdatedUserName <rodnovspb@mail.ru>
2022-01-21 00:23:37 +0300

    20.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -6,17 +6,16 @@
 
 
 
-$i = 1;
+$arr = [1,2,3,4,5,6,7];
 
-function func($num){
-    echo $num;
-    $num++;
-
-    if ($num <= 10){
-        func($num); // здесь функция вызывает сама себя
+function func ($arr){
+    var_dump(array_shift($arr));
+    var_dump($arr);
+    if(count($arr)>0){
+        func($arr);
     }
 }
-func($i);
+func($arr)
 
 
 
