1ab3534147201d761f346a0379fb10bf842fb47f
UpdatedUserName <rodnovspb@mail.ru>
2022-01-21 04:06:55 +0300

    20.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -17,8 +17,16 @@ function checkFriendlyNum ($num1, $num2){
     return (findSumDivisors($num1) === $num2 and findSumDivisors($num2) === $num1);
 }
 
-var_dump(checkFriendlyNum(220, 284));
+$a = range(1,3000);
 
+for($i=0; $i<count($a); $i++){
+    for($k=$i+1; $k<count($a); $k++){
+        if(checkFriendlyNum ($a[$i], $a[$k])){
+            echo $a[$i]." / ".$a[$k]."<br>";
+        }
+    }
+
+}
 
 
 
