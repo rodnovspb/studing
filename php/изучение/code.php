8d7b7d2c2d14f2552db9bf2db9a58777e9f5862a
UpdatedUserName <rodnovspb@mail.ru>
2022-01-22 22:38:33 +0300

    22.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -2,34 +2,15 @@
 
 
 
-
-function findSumDivisors ($num) {
-    $sum=0;
-    for($i=1; $i<$num; $i++){
-        if($num % $i === 0) {
-            $sum+=$i;
-        }
-    }
-    return $sum;
-}
-
-function checkFriendlyNum ($num1, $num2){
-    return (findSumDivisors($num1) === $num2 and findSumDivisors($num2) === $num1);
-}
-
-$a = range(1,3000);
-
-for($i=0; $i<count($a); $i++){
-    for($k=$i+1; $k<count($a); $k++){
-        if(checkFriendlyNum ($a[$i], $a[$k])){
-            echo $a[$i]." / ".$a[$k]."<br>";
-        }
-    }
-
+function f($item){
+    echo $item;
+    echo "<br>";
 }
 
 
+$arr = range(1,100);
 
+array_map('f', $arr)
 
 
 
