18f31a12a3e1a6b25a08cf77b470d82d69a6dfc8
UpdatedUserName <rodnovspb@mail.ru>
2022-01-27 00:19:47 +0300

    26.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -18,10 +18,14 @@
 
 
 
+$str = '2+3= 3+5= 7+8=';
 
-$str = '12 34 56 78';
-
-$res = preg_replace("/(\d)(\d)/",'$2$1' , $str);
+$res = preg_replace_callback('#(\d+)\+(\d+)=#', function ($match){
+    return $match[0].($match[1]+$match[2]);
+//   echo "<pre>";
+//   print_r($match);
+//   echo "</pre>";
+}, $str);
 
 echo $res;
 
