76661ce4b293e4e7e9b9eff0f52c7639a3441243
UpdatedUserName <rodnovspb@mail.ru>
2022-01-29 23:48:07 +0300

    29.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -7,16 +7,16 @@
 
 
 $arr = [
-    ['href'=>'page1.html', 'text'=>'text1'],
-    ['href'=>'page2.html', 'text'=>'text2'],
-    ['href'=>'page3.html', 'text'=>'text3'],
+    ['value' => '1', 'text' => 'text1'],
+    ['value' => '2', 'text' => 'text2'],
+    ['value' => '3', 'text' => 'text3'],
 ];
 
-echo "<ul>";
-    foreach ($arr as $elem){
-        echo "<li><a href=\"$elem[href]\">$elem[text]</a></li>";
+echo "<select>";
+    foreach ($arr as $item){
+        echo "<option value='{$item['value']}'>$item[text]</option>";
     }
-echo "</ul>";
+echo "</select>";
 
 
 
