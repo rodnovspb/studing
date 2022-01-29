df2d8be435b9c7de701d5bad4b9c4c864944fb97
UpdatedUserName <rodnovspb@mail.ru>
2022-01-29 23:43:46 +0300

    29.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -7,14 +7,16 @@
 
 
 $arr = [
-    ['href'=>'1.html', 'text'=>'link1'],
-    ['href'=>'2.html', 'text'=>'link2'],
-    ['href'=>'3.html', 'text'=>'link3'],
+    ['href'=>'page1.html', 'text'=>'text1'],
+    ['href'=>'page2.html', 'text'=>'text2'],
+    ['href'=>'page3.html', 'text'=>'text3'],
 ];
 
-foreach ($arr as $elem){
-    echo "<a href=\"{$elem['href']}\">{$elem['text']}</a><br>";
-}
+echo "<ul>";
+    foreach ($arr as $elem){
+        echo "<li><a href=\"$elem[href]\">$elem[text]</a></li>";
+    }
+echo "</ul>";
 
 
 
