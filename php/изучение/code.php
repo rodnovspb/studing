0f247f10a8c43ccf3865602f2c9e1ba73ec6571f
UpdatedUserName <rodnovspb@mail.ru>
2022-01-30 03:22:29 +0300

    29.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -1,88 +1,11 @@
 <?php
 
-
-
-
-
-
-
-$arr = [
-    ['name' => 'user1', 'age' => 30, 'salary' => 500],
-    ['name' => 'user2', 'age' => 31, 'salary' => 600],
-    ['name' => 'user3', 'age' => 32, 'salary' => 700],
-];
-
-
-
-
-echo "<table>";
-    foreach ($arr as $row){
-        echo "<tr>";
-        foreach ($row as $key=>$value){
-            if($key==='salary'){
-                echo "<td>$value dollars</td>";
-            }
-            else {
-                echo "<td>$value</td>";
-            }
-
-        }
-        echo "</tr>";
-    }
-echo "</table>";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$arr = [1, 2, 3, 4, 5];
 
 
 ?>
+
+
+<?php foreach ($arr as $item):  ?>
+<p><?=  $item ?></p>
+<?php endforeach; ?>
