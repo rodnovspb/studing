bd700ab390528491a53455f51c3fd48c25858d2c
UpdatedUserName <rodnovspb@mail.ru>
2022-01-31 02:09:22 +0300

    30.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -1,22 +1,23 @@
 
 
 <form action="" method="get">
-  <input name="year" value="<?= $_GET["year"] ??  date("Y", time()) ?>"><br>
+  <input name="year" placeholder="год" value="<?= $_GET["year"] ?? date('Y', time()) ?>"><br>
+  <input name="month" placeholder="месяц" value="<?= $_GET["month"] ?? date('n', time()) ?>"><br>
+  <input name="day" placeholder="день" value="<?= $_GET["day"] ?? date('d', time()) ?>"><br>
   <input type="submit">
 </form>
 
 <?php
-if(!empty($_GET)){
-    $year = $_GET["year"];
-    $a = date("L", mktime(0,0,0,1,1, $year));
-    if($a==1){
-      echo 'Високосный';
-	}
-    else {
-      echo 'Не високосный';
-	}
-}
 
+if(!empty($_GET)){
+  $year = $_GET["year"];
+  $month = $_GET["month"];
+  $day = $_GET["day"];
+  $newYear = mktime(12, 59, 59, 12, 31, date("Y", time()));
+  $now = mktime(0,0,0, $month, $day, $year);
+  echo floor(($newYear-$now)/60/60/24) . ' суток до Нового года';
+
+}
 
 
 ?>
