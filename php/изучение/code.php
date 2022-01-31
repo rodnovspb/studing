553f9d2c7c4ca6a60ffa31460e1c7f42d7a1a2a2
UpdatedUserName <rodnovspb@mail.ru>
2022-02-01 01:57:21 +0300

    31.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -1,17 +1,16 @@
 
 
 
-
 <form action="" method="get">
-  <span>Выберите страну</span><br>
-  <select name="test">
-	<option <?php if(isset($_GET['test']) and $_GET['test']=='Англия') echo 'selected'?>>Англия</option>
-	<option <?php if(isset($_GET['test']) and $_GET['test']=='Россия') echo 'selected'?>>Россия</option>
-	<option <?php if(isset($_GET['test']) and $_GET['test']=='США') echo 'selected'?>>США</option>
-  </select>
+  <span>Выберите язык</span><br>
+	<select name="lang">
+	  <option value="1" <?= (!empty($_GET['lang']) and $_GET['lang']==1) ? 'selected': null ?>>Английский</option>
+	  <option value="2" <?= (!empty($_GET['lang']) and $_GET['lang']==2) ? 'selected': null ?>>Русский</option>
+	  <option value="3" <?= (!empty($_GET['lang']) and $_GET['lang']==3) ? 'selected': null ?>>Итальянский</option>
+	</select>
   <input type="submit">
 </form>
 
-<?php  if(!empty($_GET)){
-  echo $_GET['test'];
-} ?>
+<?php if(!empty($_GET)) echo $_GET['lang']==1 ? "Английский" : ($_GET['lang']==2 ? "Русский" : "Итальянский");
+
+  ?>
