ce81caa3c482df7d2cb23fd2f028c29d9b083e63
UpdatedUserName <rodnovspb@mail.ru>
2022-02-01 00:53:32 +0300

    31.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -2,16 +2,16 @@
 
 
 <form action="" method="get">
-  <input type="radio" name="radio" value="1" checked>
-  <input type="radio" name="radio" value="2">
-  <input type="radio" name="radio" value="3">
+  <span>Мужской</span>
+  <input type="radio" name="sex" value="1"><br>
+  <span>Женский</span>
+  <input type="radio" name="sex" value="2"><br>
   <input type="submit">
 </form>
 
-<?php if(true){
-echo "<pre>";
-	var_dump($_GET);
-echo "</pre>";
+<?php
+  if($_GET['sex']==1) echo 'Мужской';
+  elseif($_GET['sex']==2) echo 'Женский';
+  else echo 'Выберите пол';
 
-}
-?>
+ ?>
