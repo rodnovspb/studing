72033e37de8398b6312dc5b30cb15f1e696affd0
UpdatedUserName <rodnovspb@mail.ru>
2022-02-01 02:36:24 +0300

    31.01.22

--- php/изучение/result.php
+++ php/изучение/result.php
@@ -3,14 +3,10 @@
 
 
 
-<?php if(isset($_GET['num'])) echo $_GET['num'];
-        else {
-            echo
+<?php  for($i=1; $i<11; $i++){
+    echo "<a href=\"?num=$i\">Передаем $i</a><br>";
+}
 
-"<a href=\"?num=1\">Передаем 1</a><br>
-<a href=\"?num=2\">Передаем 2</a><br>
-<a href=\"?num=3\">Передаем 3</a>";
-
-        }
+    if(isset($_GET['num'])) echo "<p>$_GET[num]</p>"
 
 ?>
