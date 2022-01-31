5ce4815558ab380768cda1746866cff7603ecb88
UpdatedUserName <rodnovspb@mail.ru>
2022-02-01 02:14:08 +0300

    31.01.22

--- php/изучение/result.php
+++ php/изучение/result.php
@@ -1,14 +1,6 @@
 <?php
 
-$year = $_GET['year'];
-$month = $_GET['month'];
-$day = $_GET['day'];
-
-
-
-$timestamp = mktime(0,0,0,$month, $day, $year);
-
-echo "День недели: " . date('w', $timestamp);
+if(isset($_GET['num1']) and isset($_GET['num2'])) echo $_GET['num1']+$_GET['num2'];
 
 
 
