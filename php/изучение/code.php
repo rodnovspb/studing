0c0d472752b0150ca030ca5add2786e8e788e467
UpdatedUserName <rodnovspb@mail.ru>
2022-01-31 23:47:22 +0300

    31.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -1,12 +1,14 @@
 
 
+
 <form action="" method="get">
-  <input type="hidden" name="flag" value="0">
-  <input type="checkbox" name="flag" value="1">
+  <input type="hidden" value="0" name="flag">
+  <span>Есть 18, мелкий гавнюк?</span>
+  <input type="checkbox" value="1" name="flag" <?php if(isset($_GET["flag"]) and $_GET["flag"] == 1) echo 'checked'?>>
   <input type="submit">
 </form>
 
-<?php if(!empty($_GET)){
-  	if($_GET['flag']==0) echo '<p>Не отмечен</p>';
-  	elseif($_GET['flag']==1) echo '<p>отмечен</p>';
+<?php if(!empty($_GET)) {
+  if($_GET['flag']==0) echo "<p>Не разрешаем</p>";
+  else echo "<p>Разрешаем</p>";
 }  ?>
