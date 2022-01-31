fd36400c8efe0d375a3789a3336cac217928de33
UpdatedUserName <rodnovspb@mail.ru>
2022-02-01 02:30:20 +0300

    31.01.22

--- php/изучение/result.php
+++ php/изучение/result.php
@@ -1,16 +1,16 @@
-<?php
-$arr = ['a', 'b', 'c', 'd', 'e'];
-if(isset($arr[$_GET['num']])){
-     echo $arr[$_GET['num']];
-}
 
 
 
 
 
+<?php if(isset($_GET['num'])) echo $_GET['num'];
+        else {
+            echo
 
+"<a href=\"?num=1\">Передаем 1</a><br>
+<a href=\"?num=2\">Передаем 2</a><br>
+<a href=\"?num=3\">Передаем 3</a>";
 
-
-
+        }
 
 ?>
