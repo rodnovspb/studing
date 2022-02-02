0e235cb62e15c54178c6e3a84978ff013f4d35f8
UpdatedUserName <rodnovspb@mail.ru>
2022-02-02 03:51:33 +0300

    02.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -2,15 +2,13 @@
 
 
 <form action="" method="get">
-  <span>Выберите язык</span><br>
-	<select name="lang">
-	  <option value="1" <?= (!empty($_GET['lang']) and $_GET['lang']==1) ? 'selected': null ?>>Английский</option>
-	  <option value="2" <?= (!empty($_GET['lang']) and $_GET['lang']==2) ? 'selected': null ?>>Русский</option>
-	  <option value="3" <?= (!empty($_GET['lang']) and $_GET['lang']==3) ? 'selected': null ?>>Итальянский</option>
-	</select>
-  <input type="submit">
+  <input name="elem" placeholder="введите градусы">
+  <button type="submit">Кнопка</button>
 </form>
 
-<?php if(!empty($_GET)) echo $_GET['lang']==1 ? "Английский" : ($_GET['lang']==2 ? "Русский" : "Итальянский");
+<?php if(isset($_GET['elem'])){
+  $a = (int)($_GET['elem']);
+	$far = $a * 1.8 + 32;
+  echo "<p>$far по Фаренгейту</p>";
 
-  ?>
+} ?>
