483749c179920d583635c7627cff77af2e3742c4
UpdatedUserName <rodnovspb@mail.ru>
2022-02-02 04:29:38 +0300

    02.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -1,14 +1,31 @@
 
 
-
 <form action="" method="get">
   <input name="elem" placeholder="введите число">
-  <button type="submit">Кнопка</button>
+  <input name="elem1" placeholder="введите число">
+  <button type="submit">Получить общие делители</button>
 </form>
 
-<?php if(isset($_GET['elem'])){
-  $a = (int)($_GET['elem']);
-	$sum = 1;
-	for($i = 1; $i <= $a; $sum*=($i++));
- 	echo "<p>$sum</p>";
+<?php
+
+function getDivisors($num){
+    $arr = [];
+    for($i=2; $i<=$num; $i++){
+        if($num%$i==0){
+            array_push($arr, $i);
+        }
+    }
+    return $arr;
+}
+
+
+if(isset($_GET["elem"]) and isset($_GET["elem1"])){
+ 	$num1 = $_GET['elem'];
+ 	$num2 = $_GET['elem1'];
+ 	$arr1 = getDivisors($num1);
+ 	$arr2 = getDivisors($num2);
+ 	$common = array_intersect($arr1, $arr2);
+ 	$w = implode(',', $common);
+ 	echo "<p>$w</p>";
+
 } ?>
