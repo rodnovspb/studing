7a650d3496ed6b2c34e17199c2dd41af813ee1c4
UpdatedUserName <rodnovspb@mail.ru>
2022-02-04 02:08:56 +0300

    02.01.22

--- php/изучение/test2.php
+++ php/изучение/test2.php
@@ -1,15 +1,1 @@
 
-
-<?php
-session_start();
-if(!empty($_SESSION['user'])){
-    echo "<ul>";
-    foreach ($_SESSION['user'] as $item){
-       echo "<li>$item</li>";
-    }
-    echo "</ul>";
-
-}
-
-
-?>
