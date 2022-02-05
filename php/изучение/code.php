0f9234e122c1ccd854bfe402ebc4c922cabc83ea
UpdatedUserName <rodnovspb@mail.ru>
2022-02-06 00:21:50 +0300

    05.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -4,16 +4,10 @@
 
 <?php
 
-$arr = ['1.txt', '2.txt','3.txt'];
-$res = '';
-foreach ($arr as $elem){
-  $res.=file_get_contents($elem);
+
+for($i=1; $i<4; $i++){
+  unlink(__DIR__."/".$i.".txt");
 }
-
-file_put_contents('4.txt', $res);
-echo file_get_contents('4.txt');
-
-
 
 
 
