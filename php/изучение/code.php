8d323703c9d279a4b95d1b084426102227bbd0ee
UpdatedUserName <rodnovspb@mail.ru>
2022-02-06 02:09:44 +0300

    05.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -7,13 +7,21 @@
 
 
 $arr = array_diff(scandir('dir'), ['.', '..']);
-
-
+$arr1 = [];
 foreach ($arr as $item){
- echo file_get_contents('dir/'.$item)."<br>";
+  $arr1[] = $item;
 }
 
+$arr = array_slice($arr1, 0);
 
+for($i=0; $i<count($arr); $i++){
+  $data = file_get_contents('dir/'.$arr[$i]);
+  file_put_contents('dir/'.$arr[$i], $data."!");
+}
+
+foreach ($arr as $item){
+  echo file_get_contents('dir/'.$item)."<br>";
+}
 
 
 
