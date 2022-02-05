18a2f350485725240e8af294a56f12e6570a9d6f
UpdatedUserName <rodnovspb@mail.ru>
2022-02-06 02:38:41 +0300

    05.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -5,23 +5,13 @@
 <?php
 
 
-
 $arr = array_diff(scandir('dir'), ['.', '..']);
-$arr1 = [];
-foreach ($arr as $item){
-  $arr1[] = $item;
-}
-
-$arr = array_slice($arr1, 0);
-
-for($i=0; $i<count($arr); $i++){
-  $data = file_get_contents('dir/'.$arr[$i]);
-  file_put_contents('dir/'.$arr[$i], $data."!");
-}
 
 foreach ($arr as $item){
-  echo file_get_contents('dir/'.$item)."<br>";
+  if(is_dir("dir/".$item)) echo "папка"."<br>";
+  elseif (is_file("dir/".$item)) echo "файл"."<br>";
 }
+
 
 
 
