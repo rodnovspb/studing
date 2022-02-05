daa0de98cf05c9abb8887a9b329fc5313a1334b1
UpdatedUserName <rodnovspb@mail.ru>
2022-02-06 01:09:58 +0300

    05.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -4,7 +4,23 @@
 
 <?php
 
-echo filesize( 'Ben-Hur.1959.Pifagor..avi')/(1024*1024*1024)
+
+
+if(file_exists('message.txt')){
+  echo file_get_contents('message.txt');
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
