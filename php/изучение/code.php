bdf7246bf2f2bbb2791e05e8f71d830fa1d240b8
UpdatedUserName <rodnovspb@mail.ru>
2022-02-06 04:11:36 +0300

    05.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -5,16 +5,20 @@
 <?php
 
 
-$names = array_diff(scandir('dir'), ['.', '..']);
+$files = array_diff(scandir("dir"), ['.', '..']);
 
-echo "<ul>";
-	foreach ($names as $name){
-	  if(is_file("dir/".$name)){
-          echo "<li>$name</li>";
-	  }
-	}
-echo "</ul>";
+$arr = [];
 
+foreach ($files as $file){
+  $arr[] = $file;
+}
+
+$files = array_slice($files, 0);
+
+
+for($i=0; $i<count($files); $i++){
+  rename('dir/'.$files[$i], 'dir/'.$i.'-'.date("H-i-s", time()).".txt");
+}
 
 
 
