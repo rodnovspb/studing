1001764df667a87fdfe714c6c31eb5e517c627a5
UpdatedUserName <rodnovspb@mail.ru>
2022-02-09 00:56:38 +0300

    08.01.22

--- практика/api прогноз погоды/script.js
+++ практика/api прогноз погоды/script.js
@@ -1,11 +1,19 @@
 
+let div = document.querySelector('div')
 
-document.querySelector('select').addEventListener('change', function () {
+
+document.querySelector('select').addEventListener('input', function () {
+		div.innerHTML = null
 		let city = this.value
 		let id = "&appid=8aa018d83a80cbb17efbe36a3c7400fc"
 		let site = "https://api.openweathermap.org/data/2.5/weather?q="
 		let path = site + city + id
-	fetch(path).then(resp => resp.json()).then(data => console.log(data))
+		fetch(path).then(resp => resp.json()).then(data => {
+			console.log(data)
+		let t = (data.main.temp - 273.15).toFixed(2)
+		div.innerHTML = t + " градусов"
+
+	})
 
 		})
 
