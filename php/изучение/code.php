cf6ec420c61640115b7925b61cdba54f1a56dc00
UpdatedUserName <rodnovspb@mail.ru>
2022-02-14 21:55:49 +0300

    14.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -11,7 +11,12 @@ $link = mysqli_connect($host, $user, $pass, $name);
 mysqli_query($link, "SET NAMES 'utf8'");
 
 
-$query = "SELECT age, GROUP_CONCAT(name SEPARATOR '/ ') as names FROM users GROUP BY age" ;
+//$query = "SELECT users.name as user_name FROM users LEFT JOIN cities ON users.id=city_id" ;
+$query = "
+SELECT cats.cat as cats_cat, parents.cat as parents_cat, parents_parents.cat as parents_parents_cat, parents_parents_parents.cat as parents_parents_parents_cat  
+FROM cats LEFT JOIN cats as parents ON cats.parent_id=parents.id
+LEFT JOIN cats as parents_parents ON cats.parent_parent_id=parents_parents.id
+LEFT JOIN cats as parents_parents_parents ON cats.parent_parent_parent_id=parents_parents_parents.id" ;
 
 
 
