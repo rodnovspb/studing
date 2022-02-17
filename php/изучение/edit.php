82963514c6fb023b8b6d1ee392ec912cecd9ff42
UpdatedUserName <rodnovspb@mail.ru>
2022-02-18 02:41:10 +0300

    17.01.22

--- php/изучение/edit.php
+++ php/изучение/edit.php
@@ -9,25 +9,44 @@ mysqli_query($link, "SET NAMES 'utf8'");
 
 
 <?php
-if(isset($_GET['id'])){
-    $id = $_GET['id'];
-    $query = "SELECT * FROM users WHERE id='$id'";
-    $res = mysqli_query($link, $query) or die(mysqli_error($link));
-    $user = mysqli_fetch_assoc($res);
-    $name = $user['name'];
-    $age = $user['age'];
-    $salary = $user['salary'];
-}
-
-
+$query = "SELECT * FROM users";
+$res = mysqli_query($link, $query) or die(mysqli_error($link));
+for($arr=[]; $row = mysqli_fetch_assoc($res); $arr[]=$row);
 ?>
 
+<?php foreach ($arr as $elem): ?>
+<li><?php echo $elem['name']?> <a href="?id=<?=$elem['id'] ?>">Редактировать</a></li>
 
-<form action="save.php?id=<?php if(isset($id)) echo $id ?>" method="post">
-    <input name="name" placeholder="имя" value="<?php if(isset($name)) echo $name?>">
-    <input name="age" placeholder="возраст" value="<?php if(isset($age)) echo $age?>">
-    <input name="salary" placeholder="зп" value="<?php if(isset($salary)) echo $salary?>">
-    <input type="submit" value="Отправить">
-</form>
+<?php endforeach; ?>
+
+<?php
+
+
+if(isset($_GET['id'])):
+    $id = $_GET['id'];
+    $query = "SELECT * FROM users WHERE id = '$id'";
+    $res = mysqli_query($link, $query) or die(mysqli_error($link));
+    $user = mysqli_fetch_assoc($res)
+	?>
+  <form action="?id=<?=$user['id']?>" method="POST">
+  <input name="name" value="<?=$user['name']?>" placeholder="Имя">
+  <input name="age" value="<?=$user['age']?>" placeholder="Возраст">
+  <input name="salary" value="<?=$user['salary']?>" placeholder="ЗП">
+	<button type="submit">Редактировать</button>
+  </form>
+
+<?php endif; ?>
+
+<?php if(!empty($_POST)) {
+  $id = (int)($_GET['id']);
+  $name = $_POST['name'];
+  $age = (int)($_POST['age']);
+  $salary = (int)($_POST['salary']);
+  $query = "UPDATE users SET name='$name', age = '$age', salary = '$salary' WHERE id = '$id'";
+  mysqli_query($link, $query) or die(mysqli_error($link));
+  header('Location: edit.php');
+} ?>
+
+
 
 
