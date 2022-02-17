5308525891228de710e0ac7763ce562d0d331824
UpdatedUserName <rodnovspb@mail.ru>
2022-02-17 22:15:41 +0300

    14.01.22

--- php/изучение/code.php
+++ php/изучение/code.php
@@ -11,24 +11,39 @@ $link = mysqli_connect($host, $user, $pass, $name);
 mysqli_query($link, "SET NAMES 'utf8'");
 
 
-//$query = "SELECT users.name as user_name FROM users LEFT JOIN cities ON users.id=city_id" ;
-$query = "SELECT users.name as user_name, fathers.name as father_name, mothers.name as mother_name FROM users
-LEFT JOIN users as fathers ON fathers.id=users.father LEFT JOIN users as mothers ON mothers.id=users.mother" ;
+$query = "SELECT * FROM users";
 
 
 
 $res=mysqli_query($link, $query) or die(mysqli_error($link));
-//$data = mysqli_fetch_assoc($res);
-//
-//
-//echo $data['age'];
 
+$result = '';
 
 for($arr=[]; $row = mysqli_fetch_assoc($res); $arr[]=$row);
 
-echo "<pre>";
-print_r($arr);
-echo "</pre>";
+foreach ($arr as $elem){
+  $result .= "<p>";
+  $result.='<b>'.$elem['name'].'</b>';
+  $result.='<b>'.$elem['age'].'</b>';
+  $result.='<b>'.$elem['salary'].'</b>';
+  $result .= "</p>";
+}
+
+echo $result;
+
+
+
+
+
+
+
+
+
+
+
+//echo "<pre>";
+//print_r($arr);
+//echo "</pre>";
 
 
 
