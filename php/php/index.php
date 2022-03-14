1bbd1cb1bc76440ad6955bdec608b1e0ef0bb5d6
UpdatedUserName <rodnovspb@mail.ru>
2022-03-14 23:55:03 +0300

    14.03.22

--- php/php/index.php
+++ php/php/index.php
@@ -11,10 +11,11 @@ mysqli_query($link, "SET NAMES 'utf8'");
 
 <?php
 $url = $_SERVER['REQUEST_URI'];
-preg_match('#/page/(\d+)#', $url, $match);
-$id = $match[1];
+preg_match('#/page/([a-z0-9_-]+)#', $url, $match);
+$slug = $match[1];
 
-$query = "SELECT * FROM pages WHERE id = $id";
+
+$query = "SELECT * FROM pages WHERE slug='$slug'";
 $result = mysqli_query($link, $query) or die(mysqli_error($link));
 $page = mysqli_fetch_assoc($result);
 
