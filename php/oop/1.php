8af8fa4399e736390601e3eba677137fe86b2125
UpdatedUserName <rodnovspb@mail.ru>
2022-03-31 01:14:37 +0300

    31.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -12,10 +12,14 @@ class User {
     public function isAgeCorrect($age){
         return ($age>18 and $age<60);
     }
+    public function subAge($num){
+        if($this->isAgeCorrect($this->age-$num)) $this->age = $this->age-$num;
+    }
 }
 
 $one = new User;
 
-$one->addAge(61);
+$one->addAge(59);
+$one->subAge(20);
 echo $one->age;
 
