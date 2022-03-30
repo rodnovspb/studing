339e03c5c9f750ce34a838d71f842be41331b2b8
UpdatedUserName <rodnovspb@mail.ru>
2022-03-31 00:38:36 +0300

    31.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -2,13 +2,31 @@
 
 
 
-class User {
+class Employee {
     public $name;
-    public function show(){
+    public $age;
+    public $salary;
+    public function getName(){
         return $this->name;
+    }
+    public function getAge(){
+        return $this->age;
+    }
+    public function getSalary(){
+        return $this->salary;
+    }
+    public function checkAge(){
+        return $this->age>18;
     }
 }
 
-$one = new User;
-$one->name='Den';
-echo $one->show();
+$one = new Employee;
+
+$one->salary=1000;
+$one->age=25;
+
+$two = new Employee;
+$two->salary=2000;
+$two->age=17;
+
+echo $one->salary + $two->salary;
