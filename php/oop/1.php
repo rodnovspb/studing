03255ce4413f17e3b210666e545edb93ac538bf0
UpdatedUserName <rodnovspb@mail.ru>
2022-04-01 00:35:29 +0300

    31.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -10,9 +10,14 @@ class Arr{
         print_r($this->numbers);
         echo "</pre>";
     }
+    public function getAvg(){
+        return array_sum($this->numbers)/count($this->numbers);
+    }
 }
 
 $one = new Arr;
 $one->add([1,2,3,4]);
 $one->add([1,2,3,4]);
 $one->get();
+
+echo $one->getAvg();
