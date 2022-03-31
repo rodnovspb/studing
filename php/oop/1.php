5b9ee204e7ba8660255d3c692dd641d92815236a
UpdatedUserName <rodnovspb@mail.ru>
2022-03-31 03:26:53 +0300

    31.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,26 +1,32 @@
 <?php
 
 
-class User {
+class Employee {
     private $name;
     private $age;
-    public function __construct($name, $age){
-        $this->age=$age;
+    private $salary;
+    public function __construct($name, $age, $salary){
         $this->name=$name;
+        $this->age=$age;
+        $this->salary=$salary;
     }
     public function getName(){
         return $this->name;
     }
-    public function setAge($num){
-        $this->age=$num;
-    }
     public function getAge(){
         return $this->age;
     }
+    public function getSalary(){
+        return $this->salary;
+    }
+    public function setAge($var){
+        $this->age=$var;
+    }
+    public function setSalary($var){
+        $this->salary=$var;
+    }
 }
 
-$one = new User('Den', 35);
-
-echo $one->getName();
-$one->setAge(36);
-echo $one->getAge();
+$one = new Employee('Den', 35, 5000);
+$one->setSalary(7000);
+echo $one->getSalary();
