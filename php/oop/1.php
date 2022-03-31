f665bde3a1823e55b48307edfa38bed777f48fec
UpdatedUserName <rodnovspb@mail.ru>
2022-04-01 01:53:03 +0300

    31.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,32 +1,30 @@
 <?php
+class User
+{
+    private $name;
+    private $age;
 
-
-
-class City {
-    public $name;
-    public $foundation;
-    public $population;
-    public function __construct($name, $foundation, $population)
+    public function __construct($name, $age)
     {
-        $this->name=$name;
-        $this->foundation=$foundation;
-        $this->population=$population;
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
     }
 }
 
-$one=new City('Moscow', 1235, '20 bil');
-$props=['elem1'=>'name', 'foundation', 'population'];
+$one=new User('Den', 35);
+$methods = ['method1' => 'getName', 'method2' => 'getAge'];
 
-class Test{
-    public $value;
-    public function __construct($value)
-    {
-        $this->value=$value;
-    }
-    public function get(){
-        return $this->value;
-    }
-}
+echo $one->{$methods['method2']}();
 
-$two = new Test('population');
-echo $one->{$two->get()};
+
+
