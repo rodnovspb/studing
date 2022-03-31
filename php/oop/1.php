7d63aba8c325898a06e0db95dfa5c7b1d9884014
UpdatedUserName <rodnovspb@mail.ru>
2022-04-01 02:21:47 +0300

    31.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -9,11 +9,17 @@ class Numbers{
     }
     public function add($num){
         $this->nums[]=$num;
+        return $this;
     }
     public function getSum(){
         return array_sum($this->nums);
     }
+    public function push($arr){
+        $this->nums=array_merge($this->nums, $arr);
+        return $this;
+    }
 }
 
-echo (new Numbers([1,2,3,4,5]))->getSum()+(new Numbers([7,6,4,8]))->getSum();
+$one=new Numbers([1,2,3]);
 
+echo $one->add(7)->add(8)->push([4,5,8,6,7])->add(9)->getSum();
