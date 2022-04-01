b439bcdd57b6faf14638fc4326720b438a9c47fa
UpdatedUserName <rodnovspb@mail.ru>
2022-04-01 03:22:04 +0300

    31.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,75 +1,38 @@
 <?php
 
-class ArraySumHelper
-{
-    /*
-        Находит сумму первых
-        степеней элементов массива:
-    */
-    public function getAvg1($arr)
-    {
-        return $this->getSum($arr, 1);
+class User{
+    private $name;
+    private $age;
+    public function setName($name){
+        $this->name=$name;
     }
-
-    /*
-        Находит сумму вторых степеней
-        элементов массива и извлекает
-        из нее квадратный корень:
-    */
-    public function getAvg2($arr)
-    {
-        return $this->getSum($arr, 1/2);
+    public function setAge($age){
+        $this->age=$age;
     }
-
-    /*
-        Находит сумму третьих степеней
-        элементов массива и извлекает
-        из нее кубический корень:
-    */
-    public function getAvg3($arr)
-    {
-        return $this->getSum($arr, 1/3);
+    public function getName(){
+        return $this->name;
     }
-
-    /*
-        Находит сумму четвертых степеней
-        элементов массива и извлекает
-        из нее корень четвертой степени:
-    */
-    public function getAvg4($arr)
-    {
-        return $this->getSum($arr, 1/4);
-
-    }
-
-    /*
-        Вспомогательный метод, который параметром
-        принимает массив и степень и возвращает
-        сумму степеней элементов массива:
-    */
-    private function getSum($arr, $power)
-    {
-        $sum =0;
-        foreach ($arr as $item){
-            $sum+=$this->calcSqrt($item, $power);
-        }
-        return $sum;
-
-    }
-
-    /*
-        Вспомогательный метод, который параметром
-        принимает целое число и степень и возвращает
-        корень заданной степени из числа:
-    */
-    private function calcSqrt($num, $power)
-    {
-        return pow($num, $power);
-
+    public function getAge(){
+        return $this->age;
     }
 }
 
-$helper = new ArraySumHelper;
-echo $helper->getAvg4([256]);
+class Employee extends User{
+    private $salary;
+    public function setSalary($num){
+        $this->salary=$num;
+    }
+    public function getSalary(){
+        return $this->salary;
+    }
+}
 
+$one=new Employee;
+$one->setName('Den');
+$one->setAge(35);
+$one->setSalary(5000);
+
+echo $one->getName()."<br>";
+echo $one->getAge()."<br>";
+echo $one->getSalary()."<br>";
 
