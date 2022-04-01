8582c417b1f216652f401e663fafb914dec7e867
UpdatedUserName <rodnovspb@mail.ru>
2022-04-01 03:06:24 +0300

    31.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,23 +1,75 @@
 <?php
 
-class sumHelper{
-    public function getSum1($arr){
+class ArraySumHelper
+{
+    /*
+        Находит сумму первых
+        степеней элементов массива:
+    */
+    public function getAvg1($arr)
+    {
         return $this->getSum($arr, 1);
     }
-    public function getSum2($arr){
-        return $this->getSum($arr, 2);
+
+    /*
+        Находит сумму вторых степеней
+        элементов массива и извлекает
+        из нее квадратный корень:
+    */
+    public function getAvg2($arr)
+    {
+        return $this->getSum($arr, 1/2);
     }
-    private function getSum($arr, $pow){
-        $sum=0;
+
+    /*
+        Находит сумму третьих степеней
+        элементов массива и извлекает
+        из нее кубический корень:
+    */
+    public function getAvg3($arr)
+    {
+        return $this->getSum($arr, 1/3);
+    }
+
+    /*
+        Находит сумму четвертых степеней
+        элементов массива и извлекает
+        из нее корень четвертой степени:
+    */
+    public function getAvg4($arr)
+    {
+        return $this->getSum($arr, 1/4);
+
+    }
+
+    /*
+        Вспомогательный метод, который параметром
+        принимает массив и степень и возвращает
+        сумму степеней элементов массива:
+    */
+    private function getSum($arr, $power)
+    {
+        $sum =0;
         foreach ($arr as $item){
-            $sum+=pow($item, $pow);
+            $sum+=$this->calcSqrt($item, $power);
         }
         return $sum;
+
+    }
+
+    /*
+        Вспомогательный метод, который параметром
+        принимает целое число и степень и возвращает
+        корень заданной степени из числа:
+    */
+    private function calcSqrt($num, $power)
+    {
+        return pow($num, $power);
+
     }
 }
 
-$one = new sumHelper;
-
-echo $one->getSum2([2,3,4]);
+$helper = new ArraySumHelper;
+echo $helper->getAvg2([4,9,16]);
 
 
