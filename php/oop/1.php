85a3da3932703526d485086340279517ed63a350
UpdatedUserName <rodnovspb@mail.ru>
2022-04-04 01:10:11 +0300

    04.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -20,3 +20,32 @@ class Employee
         return $this->salary;
     }
 }
+
+class EmployeesCollection {
+    private $employees = [];
+    public function add($var){
+        if(!($this->isExist($var, $this->employees))){
+            $this->employees[]=$var;
+        };
+    }
+    public function get(){
+        return $this->employees;
+    }
+    private function isExist($newVar, $vars){
+        foreach ($vars as $var){
+            if(in_array($newVar, $vars)) return true;
+        }
+        return false;
+    }
+}
+
+$collection = new EmployeesCollection;
+
+$collection->add(new Employee('Den', 3000));
+$collection->add(new Employee('Den', 3000));
+$collection->add(new Employee('Den', 3000));
+
+
+echo "<pre>";
+print_r($collection->get());
+echo "</pre>";
