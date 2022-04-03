6b40f6a5c710cd7389caae27dfdc581800292b79
UpdatedUserName <rodnovspb@mail.ru>
2022-04-03 03:10:36 +0300

    03.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,46 +1,42 @@
 <?php
-class User {
-    private $name;
-    private $surname;
-    private $burthday;
-    private $age;
-    public function __construct($name, $surname, $burthday)
+
+class Arr {
+    private $nums=[];
+    private $sumHelper;
+    public function add($var){
+        $this->nums[]=$var;
+    }
+    public function __construct()
     {
-        $this->name=$name;
-        $this->surname=$surname;
-        if(preg_match('/^(\d{4}|\d{2})-(\d{2}|\d)-(\d{2}|\d)$/', $burthday)) {
-            $this->burthday = $burthday;
+        $this->sumHelper=new sumHelper;
+    }
+    public function getSum23(){
+        $arr=$this->nums;
+        return $this->sumHelper->getSum2($arr) + $this->sumHelper->getSum3($arr);
+    }
+    public function getNums(){
+        return $this->nums;
+    }
+}
+
+class sumHelper {
+    public function getSum2($arr){
+        return $this->getSum($arr, 2);
+    }
+    public function getSum3($arr){
+        return $this->getSum($arr, 3);
+    }
+    private function getSum($arr, $pow){
+        $sum=0;
+        foreach ($arr as $elem){
+            $sum+=$elem**$pow;
         }
-        $this->age=$this->calculateAge($this->burthday);
-    }
-    public function getName(){
-        return $this->name;
-    }
-    public function getSurname(){
-        return $this->surname;
-    }
-    public function getBirth(){
-        return $this->burthday;
-    }
-    public function getAge(){
-        return $this->age;
-    }
-    private function calculateAge($var){
-        $now = time();
-        return floor(($now-strtotime($var))/60/60/24/360);
+        return $sum;
     }
 }
 
-class Employee extends User{
-    private $salary;
-    public function __construct($name, $surname, $burthday, $salary){
-        parent::__construct($name, $surname, $burthday);
-        $this->salary = $salary;
-    }
-    public function getSalary(){
-        return $this->salary;
-    }
-}
-
-$one=new Employee('Den', "ew", '1986-10-14', 3000);
-echo $one->getSalary();
+$one = new Arr;
+$one->add(2);
+$one->add(3);
+$one->add(4);
+echo $one->getSum23();
