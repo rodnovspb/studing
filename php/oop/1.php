952832c9ff8d39a25c073fd8110dfc3e0bcbf1e9
UpdatedUserName <rodnovspb@mail.ru>
2022-04-04 00:19:37 +0300

    03.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,65 +1,28 @@
 <?php
-class Product {
+class User
+{
     private $name;
-    private $price;
-    private $quantity;
-    public function __construct($name, $price, $quantity)
+    private $age;
+
+    public function __construct($name, $age)
     {
-        $this->name=$name;
-        $this->price=$price;
-        $this->quantity=$quantity;
+        $this->name = $name;
+        $this->age  = $age;
     }
-    public function getName(){
+
+    public function getName()
+    {
         return $this->name;
     }
-    public function getPrice(){
-        return $this->price;
-    }
-    public function getQuantity(){
-        return $this->quantity;
-    }
-    public function getCost(){
-        return $this->quantity*$this->price;
-    }
-}
-class Cart {
-    private $products=[];
-    public function getProducts(){
-        return $this->products;
-    }
-    public function add($prod){
-        $this->products[] = $prod;
-    }
-    public function remove($nameOfProd){
-        $count = count($this->products);
-        for($i=0; $i<$count; $i++){
-            if($this->products[$i]->getName() == $nameOfProd){
-                unset($this->products[$i]);
-            }
-        }
-    }
-    public function getTotalCost(){
-        $sum = 0;
-        foreach ($this->products as $item){
-            $sum+=$item->getCost();
-        }
-        return $sum;
-    }
-    public function getTotalQuantity(){
-        $sum=0;
-        foreach ($this->getProducts() as $item){
-            $sum+=$item->getQuantity();
-        }
-        return $sum;
-    }
-    public function getAvgPrice(){
-        return $this->getTotalCost()/$this->getTotalQuantity();
+
+    public function getAge()
+    {
+        return $this->age;
     }
 }
 
-$one = new Cart;
-$one->add(new Product('Соль', 10, 20));
-$one->add(new Product('Сахар', 20, 30));
-$one->add(new Product('Перец', 30, 40));
+$user1 = new User('Den', 35);
+$user2 = new User('Den', '35');
+$user3 = $user1;
 
-echo $one->getTotalQuantity();
+var_dump($user1==$user2);
