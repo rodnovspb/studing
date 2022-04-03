f070d6277ca41669863615a02aa9493be0d43d20
UpdatedUserName <rodnovspb@mail.ru>
2022-04-03 04:10:08 +0300

    03.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -2,13 +2,16 @@
 
 class Arr {
     private $nums=[];
-    private $sumHelper;
+    public $sumHelper;
+    public $avgHelper;
     public function add($var){
         $this->nums[]=$var;
     }
     public function __construct()
     {
-        $this->sumHelper=new sumHelper;
+        $this->sumHelper=new SumHelper;
+        $this->avgHelper = new AvgHelper;
+
     }
     public function getSum23(){
         $arr=$this->nums;
@@ -17,9 +20,13 @@ class Arr {
     public function getNums(){
         return $this->nums;
     }
+    public function getAvgMeanSum(){
+        return $this->avgHelper->getAvg($this->getNums()) + $this->avgHelper->getMeanSquare($this->nums);
+    }
 }
 
-class sumHelper {
+
+class SumHelper {
     public function getSum2($arr){
         return $this->getSum($arr, 2);
     }
@@ -34,9 +41,39 @@ class sumHelper {
         return $sum;
     }
 }
+class AvgHelper {
+    private $getSquare;
+    public function getAvg($arr){
+        return array_sum($arr)/count($arr);
+    }
+    public function getMeanSquare($arr){
+        return pow($this->getSquare->getSum2($arr)/count($arr), 1/2);
+    }
+    public function __construct()
+    {
+        $this->getSquare=new sumHelper;
+    }
+
+}
 
 $one = new Arr;
 $one->add(2);
 $one->add(3);
-$one->add(4);
-echo $one->getSum23();
+
+
+
+
+echo "среднее арифметическое " . $one->avgHelper->getAvg($one->getNums());
+echo "<br>";
+echo "среднее квадратическое " . $one->avgHelper->getMeanSquare($one->getNums());
+echo "<br>";
+echo $one->getAvgMeanSum();
+
+//Добавьте в класс Arr метод getAvgMeanSum, который будет находить сумму среднего арифметического и среднего квадратичного из массива $this->nums.
+
+
+//квадратный корень, извлеченный из суммы квадратов элементов, деленной на количество)
+
+//средквадр  (4+9)/2 = 7,5  => 2,54
+
+//сред ариф 2,5
