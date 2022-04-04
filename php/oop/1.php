b80626db7ec2c92fced4f220bd4d120e4d126ce0
UpdatedUserName <rodnovspb@mail.ru>
2022-04-04 03:24:17 +0300

    04.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,38 +1,9 @@
 <?php
 
-class ArraySumHelper
-{
-    public static function getSum1($arr)
-    {
-        return self::getSum($arr, 1);
-    }
-
-    public static function getSum2($arr)
-    {
-        return self::getSum($arr, 2);
-    }
-
-    public static function getSum3($arr)
-    {
-        return self::getSum($arr, 3);
-    }
-
-    public static function getSum4($arr)
-    {
-        return self::getSum($arr, 4);
-    }
-
-    private static function getSum($arr, $power) {
-        $sum = 0;
-
-        foreach ($arr as $elem) {
-            $sum += pow($elem, $power);
-        }
-
-        return $sum;
-    }
+class Num{
+    public static $num1;
+    public static $num2;
 }
-
-$arr = [1,2,3];
-
-echo ArraySumHelper::getSum2($arr);
+Num::$num1=1;
+Num::$num2=2;
+echo Num::$num1 + Num::$num2;
