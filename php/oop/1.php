e99f4894224135961c6c888855657009851955c7
UpdatedUserName <rodnovspb@mail.ru>
2022-04-05 02:11:26 +0300

    05.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,15 +1,29 @@
 <?php
 
-class User {
+class Test1 {
     public $name;
-    public function __construct($name)
+    function __construct($name)
     {
         $this->name=$name;
     }
-    public function getClass(){
-        return get_class($this);
+}
+class Test2 {
+    public $name;
+    function __construct($name)
+    {
+        $this->name=$name;
     }
 }
 
-$one = new User('dasd');
-echo get_class($one);
+$one=new Test1('asd');
+$one1=new Test1('aesd');
+$one2=new Test1('asrd');
+$two = new Test2('dasf');
+$two1 = new Test2('dadsf');
+$two2 = new Test2('daasf');
+$arr = [$one, $one2, $two1, $one1, $two, $two2];
+
+foreach ($arr as $item){
+    echo $item->name. " " . get_class($item) . '<br>';
+
+}
