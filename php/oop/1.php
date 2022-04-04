916e09f798d697282c02cf6db7911a7922084252
UpdatedUserName <rodnovspb@mail.ru>
2022-04-05 01:26:39 +0300

    05.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,16 +1,12 @@
 <?php
 
-class Geometry {
-    private static $pi = 3.14;
-    public static function getSquare($radius){
-        return self::$pi * $radius**2;
-    }
-    public static function getLength($radius){
-        return self::$pi*2*$radius;
-    }
-    public static function getVolume($radius){
-        return (1+1/3)*self::$pi*pow($radius, 3);
+class Test {
+    public static $prop='qwe';
+    public $elem;
+    public static function f(){
+        echo self::$prop;
     }
 }
 
-echo Geometry::getVolume(1);
+$a = new Test;
+$a::f();
