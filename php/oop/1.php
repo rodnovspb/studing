8d3a7f17ab94b46a956d06e89c9b0e4b7b63ae61
UpdatedUserName <rodnovspb@mail.ru>
2022-04-05 02:42:08 +0300

    05.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,29 +1,20 @@
 <?php
 
-class Test1 {
-    public $name;
-    function __construct($name)
-    {
-        $this->name=$name;
+class Test {
+    public function method1 (){
+        echo '1';
     }
-}
-class Test2 {
-    public $name;
-    function __construct($name)
-    {
-        $this->name=$name;
+    public function method2 (){
+        echo '2';
+    }
+    public function method3 (){
+        echo '3';
     }
 }
 
-$one=new Test1('asd');
-$one1=new Test1('aesd');
-$one2=new Test1('asrd');
-$two = new Test2('dasf');
-$two1 = new Test2('dadsf');
-$two2 = new Test2('daasf');
-$arr = [$one, $one2, $two1, $one1, $two, $two2];
+$one = new Test;
 
+$arr = get_class_methods($one);
 foreach ($arr as $item){
-    echo $item->name. " " . get_class($item) . '<br>';
-
+    $one->$item();
 }
