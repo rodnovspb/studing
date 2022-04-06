afc36e8bd2179db4926e54c3f9e4854a3cdc3784
UpdatedUserName <rodnovspb@mail.ru>
2022-04-07 00:47:31 +0300

    07.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,61 +1,42 @@
 <?php
 
-abstract class Figure {
-    abstract public function getPerimeter();
-    abstract public function getSquare();
-    public function getRatio(){
-        return $this->getSquare()/$this->getPerimeter();
-    }
-    public function getSquarePerimeterSum(){
-        return $this->getPerimeter()+$this->getSquare();
-    }
+interface Figure {
+    public function getPerimeter();
+    public function getSquare();
 }
 
-class Rectangle extends Figure {
+class Quadrate implements Figure {
     private $a;
-    private $b;
-    public function __construct($a, $b)
+    public function __construct($a)
     {
         $this->a=$a;
-        $this->b=$b;
     }
-
     public function getPerimeter()
     {
-        return ($this->b+$this->b)*2;
+       return $this->a*4;
     }
     public function getSquare()
     {
-       return $this->b*$this->a;
+       return $this->a**2;
     }
 }
-class Circle extends Figure {
+
+class Circle implements Figure {
     const PI=3.14;
     private $radius;
     public function __construct($radius)
     {
         $this->radius=$radius;
     }
+    public function getPerimeter()
+    {
+        return 2*$this->radius*self::PI;
+    }
     public function getSquare()
     {
-        return $this->radius**2*self::PI;
-    }
-    public  function getPerimeter()
-    {
-        return 2*self::PI*$this->radius;
+        return self::PI*$this->radius**2;
     }
 }
 
-$one = new Rectangle(1,1);
-echo $one->getSquare();
-echo '<br>';
-echo $one->getRatio();
-echo '<br>';
-$two = new Circle(1);
-echo $two->getSquare();
-echo '<br>';
-echo $two->getPerimeter();
-echo '<br>';
-echo $two->getRatio();
-echo '<br>';
-echo $two->getSquarePerimeterSum();
+$one = new Circle(1);
+echo $one->getPerimeter();
