f8a6fa6006e02ea2d29ca2f3412e47f924bc977d
UpdatedUserName <rodnovspb@mail.ru>
2022-04-06 05:26:16 +0300

    06.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,16 +1,56 @@
 <?php
 
-$arr = ['prop1', 'prop2','prop3','prop4'];
 
-class User {
-    public $prop1='1';
-    public $prop3=2;
+
+abstract class User {
+    private $name;
+    public function setName($var){
+        $this->name=$var;
+    }
+    public function getName(){
+        return $this->name;
+    }
+    abstract public function increaseRevenue($value);
+    abstract public function decreaseRevenue($value);
 }
 
-$one = new User;
-
-foreach ($arr as $item){
-    if(property_exists($one, $item)) {
-        echo $one->$item;
+class Employee extends User {
+    private $salary;
+    public function setSalary ($var){
+        $this->salary=$var;
+    }
+    public function getSalary(){
+        return $this->salary;
+    }
+    public function increaseRevenue($value)
+    {
+        $this->salary=$this->salary+$value;
+    }
+    public function decreaseRevenue($value)
+    {
+        $this->salary=$this->salary-$value;
     }
 }
+
+class Student extends User {
+    private $scholarship;
+    public function setSchol($var){
+        $this->scholarship=$var;
+    }
+    public function getSchol(){
+        return $this->scholarship;
+    }
+    public function increaseRevenue($value)
+    {
+        $this->scholarship =  $this->scholarship + $value;
+    }
+    public function decreaseRevenue($value)
+    {
+        $this->scholarship=$this->scholarship-$value;
+    }
+}
+
+$one = new Student;
+$one->setSchol(1000);
+$one->increaseRevenue(500);
+echo $one->getSchol();
