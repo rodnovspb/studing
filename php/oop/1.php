ae71252fbfd4ab8ff0ce62e034ac1d4a5ebbc3cb
UpdatedUserName <rodnovspb@mail.ru>
2022-04-07 07:50:21 +0300

    07.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,27 +1,11 @@
 <?php
 
-interface iSphere {
-    const PI=3.14;
-    public function __construct($radius);
-    public function getSquare();
-    public function getVolume();
-}
+interface Test1{
 
-class Sphere implements iSphere {
-    private $radius;
-    public function __construct($radius)
-    {
-        $this->radius=$radius;
-    }
-    public function getSquare()
-    {
-        return $this->radius**2*self::PI;
-    }
-    public function getVolume()
-    {
-        return $this->radius**3*self::PI;
-    }
 }
+interface Test2 {
 
-$one = new Sphere(1);
-echo $one->getSquare();
+}
+echo '<pre>';
+print_r(get_declared_interfaces());
+echo '</pre>';
