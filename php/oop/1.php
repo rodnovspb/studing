2a632e3a043fdfe423a2f880831b561a95fc1b4d
UpdatedUserName <rodnovspb@mail.ru>
2022-04-07 03:03:42 +0300

    07.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,43 +1,27 @@
 <?php
 
-interface iProgrammer {
-    public function __construct($name, $salary);
-    public function getName();
-    public function getSalary();
-    public function addLangs($lang);
-    public function getLangs();
+interface iSphere {
+    const PI=3.14;
+    public function __construct($radius);
+    public function getSquare();
+    public function getVolume();
 }
 
-class Employee {
-    private $name;
-    private $salary;
-    public function __construct($name, $salary)
+class Sphere implements iSphere {
+    private $radius;
+    public function __construct($radius)
     {
-        $this->name=$name;
-        $this->salary=$salary;
+        $this->radius=$radius;
     }
-    public function getName(){
-        return $this->name;
-    }
-    public function getSalary(){
-        return $this->salary;
-    }
-}
-
-class Programmer extends Employee implements iProgrammer {
-    private $langs=[];
-    public function addLangs($lang)
+    public function getSquare()
     {
-        $this->langs[]=$lang;
+        return $this->radius**2*self::PI;
     }
-    public function getLangs()
+    public function getVolume()
     {
-        return $this->langs;
+        return $this->radius**3*self::PI;
     }
 }
 
-$one = new Programmer('Den', 5000);
-$one->addLangs('php');
-$one->addLangs('js');
-echo $one->getLangs();
-echo $one->getSalary();
+$one = new Sphere(1);
+echo $one->getSquare();
