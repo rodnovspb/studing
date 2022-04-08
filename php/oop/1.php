466b7ef427249f65511d154b7f11f31e7e750b44
UpdatedUserName <rodnovspb@mail.ru>
2022-04-08 08:55:27 +0300

    07.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,13 +1,21 @@
 <?php
 
-class User{
-    private $name=1;
-    private $age=2;
-    function __get($var)
+class User
+{
+    private $name;
+    private $age;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function __get($var)
     {
         return $this->$var;
     }
 }
 
-$one = new User;
-echo $one->ag2e;
+$one = new User('Den', 35);
+echo $one->age1;
