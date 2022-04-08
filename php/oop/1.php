7e14e382e1396c56ec40cd3f23701ffe2a3192e9
UpdatedUserName <rodnovspb@mail.ru>
2022-04-08 07:24:13 +0300

    07.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,28 +1,36 @@
 <?php
 
 trait Trait1 {
-    public function method(){
-        echo 44;
+    private function method () {
+        return 1;
     }
 }
+
 trait Trait2 {
-    public function method(){
-        echo 2;
+    private function method () {
+        return 2;
+    }
+}
+
+trait Trait3 {
+    private function method () {
+        return 3;
     }
 }
 
 class Test {
-    use Trait1, Trait2 {
-        Trait1::method insteadof Trait2;
-        Trait1::method as met1;
-        Trait2::method as met2;
-
+    use Trait1, Trait2, Trait3 {
+        Trait2::method insteadof Trait1;
+        Trait3::method insteadof Trait2;
+        Trait3::method as method3;
+        Trait2::method as method2;
+        Trait1::method as method1;
     }
-    public function __construct()
-    {
-        return $this->met1();
+    public function getSum(){
+        return $this->method1() + $this->method2() + $this->method3();
     }
-
 }
 
 $one = new Test;
+echo $one->getSum();
+
