05ea2ec27c4e1a0192383286fbffb956bad15517
UpdatedUserName <rodnovspb@mail.ru>
2022-04-11 00:13:16 +0300

    10.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -2,34 +2,59 @@
 
 class Tag {
     private $name;
-    private $attr;
-    function __construct($name, $attr=[])
+    private $attr=[];
+    function __construct($name)
     {
         $this->name=$name;
-        $this->attr=$attr;
     }
     public function open(){
         $name = $this->name;
-        $atrr = $this->createAttr();
-        return "<$name $atrr>";
+        $strAtr = '';
+        if(!empty($this->attr)){
+            foreach ($this->attr as $key=>$value){
+                if($value !==true){
+                $strAtr .= " $key=\"$value\"";
+                } else {
+                    $strAtr .= " $key ";
+                }
+            }
+        }
+
+        return "<$name $strAtr>";
     }
     public function close(){
         $name = $this->name;
         return "</$name>";
     }
-    private function createAttr(){
-        $strAtr = '';
-        if(!empty($this->attr)){
-            foreach ($this->attr as $key=>$value){
-                $strAtr .= " $key=\"$value\"";
-            }
-        }
-        return $strAtr;
+    public function add($name, $value){
+        $this->attr[$name] = $value;
+        return $this;
     }
+    public function remove($name){
+        if(isset($this->attr[$name])){
+            unset($this->attr[$name]);
+        }
+    }
+    public function setAttrs($attrs){
+        foreach ($attrs as $key=>$value){
+            $this->add($key, $value);
+        }
+        return $this;
+    }
+//    private function createAttr(){
+//        $strAtr = '';
+//        if(!empty($this->attr)){
+//            foreach ($this->attr as $key=>$value){
+//                $strAtr .= " $key=\"$value\"";
+//            }
+//        }
+//        return $strAtr;
+//    }
 }
 
 
-$input = new Tag('input', ['id'=>1, 'class'=>'elem']);
-echo $input->open();
+echo (new Tag('input'))->add('name', 'name1')->open();
+echo (new Tag('input'))->add('name', 'name2')->open();
+
 
 
