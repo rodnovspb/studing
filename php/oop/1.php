cc418cdc2ba705ef4143889b1026367099fba92c
UpdatedUserName <rodnovspb@mail.ru>
2022-04-11 17:59:30 +0300

    11.03.22

--- php/oop/1.php
+++ php/oop/1.php
@@ -1,65 +1,133 @@
 <?php
 
-class Tag {
+interface iTag {
+    public function __construct($name);
+    public function open();
+    public function close();
+    public function add($name, $value);
+    public function remove($name);
+    public function setAttrs($attrs);
+    public function addClass($className);
+    public function removeClass($className);
+    public function getName();
+    public function getText();
+    public function getAttrs();
+    public function getAttr($attr);
+    public function setText($text);
+    public function show();
+
+}
+
+class Tag implements iTag {
     private $name;
-    private $attr=[];
-    function __construct($name)
-    {
-        $this->name=$name;
+    private $attr = [];
+    private $text;
+
+    function __construct($name) {
+        $this->name = $name;
     }
-    public function open(){
+
+    public function open() {
         $name = $this->name;
         $strAtr = '';
-        if(!empty($this->attr)){
-            foreach ($this->attr as $key=>$value){
-                if($value !==true){
-                $strAtr .= " $key=\"$value\"";
+        if (!empty($this->attr)) {
+            foreach ($this->attr as $key => $value) {
+                if ($value !== true) {
+                    $strAtr .= " $key=\"$value\"";
                 } else {
                     $strAtr .= " $key ";
                 }
             }
         }
-
         return "<$name $strAtr>";
     }
-    public function close(){
+
+    public function close() {
         $name = $this->name;
         return "</$name>";
     }
-    public function add($name, $value){
+
+    public function show() {
+        return $this->open() . $this->text . $this->close();
+    }
+
+    public function add($name, $value) {
         $this->attr[$name] = $value;
         return $this;
     }
-    public function remove($name){
-        if(isset($this->attr[$name])){
+
+    public function remove($name) {
+        if (isset($this->attr[$name])) {
             unset($this->attr[$name]);
         }
     }
-    public function setAttrs($attrs){
-        foreach ($attrs as $key=>$value){
+
+    public function setAttrs($attrs) {
+        foreach ($attrs as $key => $value) {
             $this->add($key, $value);
         }
         return $this;
     }
-//    private function createAttr(){
-//        $strAtr = '';
-//        if(!empty($this->attr)){
-//            foreach ($this->attr as $key=>$value){
-//                $strAtr .= " $key=\"$value\"";
-//            }
-//        }
-//        return $strAtr;
-//    }
+
+    public function addClass($className) {
+        if (!isset($this->attr['class'])) {
+            $this->attr['class'] = $className;
+        } else {
+            $arr = explode(' ', $this->attr['class']);
+            if (!in_array($className, $arr)) {
+                $arr[] = $className;
+            }
+            $str = implode(' ', $arr);
+            $this->attr['class'] = $str;
+        }
+        return $this;
+    }
+
+    public function removeClass($className) {
+        if (isset($this->attr['class'])) {
+            $arr = explode(' ', $this->attr['class']);
+            if (in_array($className, $arr)) {
+                $arr = $this->removeElem($className, $arr);
+            }
+            $str = implode(' ', $arr);
+            $this->attr['class'] = $str;
+            if ($this->attr['class'] === '') {
+                unset($this->attr['class']);
+            }
+        }
+        return $this;
+    }
+
+    private function removeElem($elem, $arr) {
+        $key = array_search($elem, $arr);
+        array_splice($arr, $key, 1);
+        return $arr;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getText() {
+        return $this->attr['text'] ?? null;
+    }
+
+    public function getAttrs() {
+        return $this->attr;
+    }
+
+    public function getAttr($attr) {
+        return $this->attr[$attr] ?? null;
+    }
+
+    public function setText($text) {
+        $this->text = $text;
+        return $this;
+    }
 }
 
 
-echo (new Tag('input'))
-    ->add('name', 'name1')
-    ->open();
 
-echo (new Tag('input'))
-    ->add('name', 'name2')
-    ->open();
 
 
 
