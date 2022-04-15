2227e03b292a26e52d19ab03fd1e0476c8fb0dab
UpdatedUserName <rodnovspb@mail.ru>
2022-04-15 20:34:04 +0300

    13.03.22

--- php/learning/1.php
+++ php/learning/1.php
@@ -1,10 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Документ</title>
+</head>
+<body>
+
+
 <?php
+$nameError = $surnameError = $patrError = '';
+$name = $surname = $patronymic = '';
 
-$str = "The rain in England falls mainly on the plains.";
-$pattern = "/ain\b/";
+if(!empty($_POST['submit'])){
+    empty($_POST['name']) ? $nameError = 'Введите имя' : $name = $_POST['name'];
+    empty($_POST['surname']) ? $surnameError = 'Введите фамилию' : $surname = $_POST['surname'];
+    empty($_POST['patronymic']) ? $patrError = 'Введите отчество' : $patronymic = $_POST['patronymic'];
+}
+?>
 
-preg_match_all($pattern, $str, $match);
-echo "<pre>";
-print_r($match);
-echo "</pre>";
 
+<form action="" method="post">
+    <label>Введите имя <input type="text" name="name"></label><span>*<?= $nameError ?></span><br>
+    <label>Введите фамилию <input type="text" name="surname"></label><span>*<?= $surnameError ?></span><br>
+    <label>Введите отчество <input type="text" name="patronymic"></label><span>*<?= $patrError ?></span><br>
+    <input type="submit" name="submit">
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
