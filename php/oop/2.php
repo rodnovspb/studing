c82ddfcc72eba781403ed5d33258a7428e470345
UpdatedUserName <rodnovspb@mail.ru>
2022-04-17 03:25:26 +0300

    17.03.22

--- php/oop/2.php
+++ php/oop/2.php
@@ -26,17 +26,55 @@ class TagHelper {
     }
 }
 
-$div = new TagHelper();
-echo $div->open('div', ['class'=>'elem']) . 'text' . $div->close('div');
+class FormHelper extends TagHelper {
+    public function openForm($attrs){
+        return $this->open('form', $attrs);
+    }
+    public function closeForm() {
+        return $this->close('form');
+    }
+    public function input($attrs=[]){
+        if(isset($attrs['name'])){
+            $name = $attrs['name'];
+            if(isset($_REQUEST[$name])){
+                $attrs['value'] = $_REQUEST[$name];
+            }
+        }
+        return $this->open('input', $attrs);
+    }
+    public function password($attrs=[]){
+        $attrs['type'] = 'password';
+        return $this->input($attrs);
+    }
+    public function hidden($attrs=[]){
+        $attrs['type'] = 'hidden';
+        return $this->open('input', $attrs);
+    }
+    public function submit($attrs=[]){
+        $attrs['type'] = 'submit';
+        return $this->open('input', $attrs);
+    }
+    public function checkbox($attrs=[]){
+        $attrs['type'] = 'checkbox';
+        $attrs['value'] = 1;
+        if(isset($attrs['name'])){
+            $name =  $attrs['name'];
+            if(isset($_REQUEST[$name]) and $_REQUEST[$name] == 1){
+                $attrs['checked'] = true;
+            }
+            $hidden = $this->hidden(['name'=>$name, "value"=>0]);
+        } else {
+            $hidden='';
+        }
+        return $hidden . $this->open('input', $attrs);
+    }
+}
 
-$form = new TagHelper;
-$inputText = new TagHelper;
-$inputSubmit = new TagHelper;
-echo $form->open('form', ['method'=>'get', 'action'=>' ']);
-echo $inputText->open('input', ['name'=>'elem']);
-echo $inputSubmit->open('input', ['type'=>'submit']);
-echo $form->close('form');
+echo (new FormHelper)->openForm(['action'=>' ', 'method'=>'get']);
+echo (new FormHelper)->checkbox(['name'=>'elem1']);
+echo (new FormHelper)->input(['name'=>'year']);
+echo (new FormHelper)->submit();
+echo (new FormHelper)->closeForm();
 
-$span = new TagHelper;
-echo $span->show('span', 'это спан', ['class'=>'elem1']);
+
 
