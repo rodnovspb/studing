d50b6cef083037266b593370086052a2b3406a6a
UpdatedUserName <rodnovspb@mail.ru>
2022-04-18 21:42:10 +0300

    18.03.22

--- php/oop/3.php
+++ php/oop/3.php
@@ -1,5 +1,24 @@
 <?php
-setcookie('test1', 111, time()+1000);
-unset($_COOKIE['test1']);
 
-var_dump($_COOKIE['test1']);
+class CookieShell {
+    public function set($name, $value, $time){
+        setcookie($name, $value, time()+$time);
+        $_COOKIE[$name] = $value;
+    }
+    public function get($name){
+        return $_COOKIE[$name];
+    }
+    public function del($name){
+        setcookie($name, '', time());
+        unset($_COOKIE[$name]);
+    }
+    static function exists($name){
+        return isset($_COOKIE[$name]);
+    }
+}
+
+
+$one = new CookieShell;
+$one->set('aaa1', 222, 3600);
+$one->del('aaa1');
+var_dump($one->get('aaa1'));
