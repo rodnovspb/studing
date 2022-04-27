60b9126a64eb086efb3e7b7dc36f84588596903b
UpdatedUserName <rodnovspb@mail.ru>
2022-04-27 09:24:36 +0300

    27.03.22

--- сайт логистика/assets/js/app.js
+++ сайт логистика/assets/js/app.js
@@ -128,5 +128,32 @@ $(function () {
 			}, 200)
 		}
 
+		// Slick slider https://kenwheeler.github.io/slick/
+		let introSlider = $("#introSlider");
+
+		introSlider.slick({
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			// fade: true,
+			autoplay: true,
+			autoplaySpeed: 4000,
+		})
+
+		$("#introSliderPrev").on('click', function () {
+			introSlider.slick('slickPrev')
+		})
+
+		$("#introSliderNext").on('click', function () {
+			introSlider.slick('slickNext')
+		})
+
+
+
+
+
+
+
 })
 
