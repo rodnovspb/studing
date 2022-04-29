9c57c31b14ab0604124478d1fa8d882c970b394b
UpdatedUserName <rodnovspb@mail.ru>
2022-04-30 02:08:34 +0300

    30.03.22

--- практика/ajax fetch базы данных/мягкое удаление/ajax.php
+++ практика/ajax fetch базы данных/мягкое удаление/ajax.php
@@ -11,32 +11,42 @@ mysqli_query($link, "SET NAMES 'utf8'");
 ?>
 
 <?php
-if(isset($_GET['mark'])){
-    $id = $_GET['mark'];
-    $query = "UPDATE employees SET deleted = 1 WHERE id = $id";
-    mysqli_query($link, $query) or die(mysqli_error($link));
-    $isDel = mysqli_affected_rows($link) > 0;
-    exit(json_encode($isDel));
-}
+    if(!empty($_POST['name']) && !empty($_POST['surname']) && !empty($_POST['age']) && !empty($_POST['salary'])){
+        $name = $_POST['name'];
+        $surname = $_POST['surname'];
+        $age = $_POST['age'];
+        $salary = $_POST['salary'];
+        $query = "INSERT INTO employees SET name ='$name', surname ='$surname', age = '$age', salary = '$salary'";
+        mysqli_query($link, $query) or die(mysqli_error($link));
+        $id = mysqli_insert_id($link);
+        $query = "SELECT * FROM employees ORDER BY id DESC";
+        $res = mysqli_query($link, $query) or die(mysqli_error($link));
+        for($data = []; $row = mysqli_fetch_assoc($res); $data[]=$row);
+        $obj = ['id'=>$id, 'data'=>$data];
+        exit(json_encode($obj));
 
-elseif (isset($_GET['recover'])) {
-    $id = $_GET['recover'];
-    $query = "UPDATE employees SET deleted = 0 WHERE id = $id";
-    mysqli_query($link, $query) or die(mysqli_error($link));
-    $isRecover = mysqli_affected_rows($link) > 0;
-    exit(json_encode($isRecover));
-}
+    }elseif (isset($_POST['id'])){
+        $nameText = $_POST['nameText'];
+        $surnameText = $_POST['surnameText'];
+        $ageText = $_POST['ageText'];
+        $salaryText = $_POST['salaryText'];
+        $id = $_POST['id'];
+        $query = "UPDATE employees SET name ='$nameText', surname ='$surnameText', age = '$ageText', salary = '$salaryText' WHERE id = '$id'";
+        mysqli_query($link, $query) or die(mysqli_error($link));
+        $success = mysqli_affected_rows($link);
+        $query = "SELECT * FROM employees ORDER BY id DESC";
+        $res = mysqli_query($link, $query) or die(mysqli_error($link));
+        for($data = []; $row = mysqli_fetch_assoc($res); $data[]=$row);
+        exit(json_encode($data));
 
-elseif (isset($_GET['delete'])) {
-    $query = "DELETE FROM employees WHERE deleted=1";
-    mysqli_query($link, $query) or die(mysqli_error($link));
-    $isRemove = mysqli_affected_rows($link) > 0;
-    exit(json_encode($isRemove));
-}
-else {
-    $query = "SELECT id, surname, deleted FROM employees";
-    $res = mysqli_query($link, $query) or die(mysqli_error($link));
-    for($data=[]; $row = mysqli_fetch_assoc($res); $data[] = $row);
-    exit(json_encode($data));
-}
+    } else{
+        $query = "SELECT * FROM employees ORDER BY id DESC";
+        $res = mysqli_query($link, $query) or die(mysqli_error($link));
+        for($data = []; $row = mysqli_fetch_assoc($res); $data[]=$row);
+        exit(json_encode($data));
+    }
+
+
+
+
 ?>
