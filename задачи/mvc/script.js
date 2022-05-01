4aa7ff3b13cdfe93e195a36e786fdd63628dd1e9
UpdatedUserName <rodnovspb@mail.ru>
2022-05-01 09:34:48 +0300

    01.03.22

---
+++ задачи/mvc/script.js
@@ -0,0 +1,40 @@
+let model = {
+	number: 0,
+	calc: function (x,y) {
+		this.number=x*y
+		view.showNumber(this.number)
+	}
+
+}
+
+let view = {
+	showNumber: function(n){
+		document.querySelector('p').innerHTML = n;
+}
+}
+
+let controller = {
+	handle: function () {
+		model.calc(1000,1000)
+	}
+}
+
+
+;(function () {
+	let app = {
+		init: function () {
+			this.main()
+			this.event()
+		},
+		main: function () {
+
+		},
+		event: function () {
+			let input = document.querySelector('input')
+			input.onclick = controller.handle
+		},
+	}
+
+	app.init()
+
+})()
