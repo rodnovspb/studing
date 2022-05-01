903f518c6f5e85231abdd83ff476730e6ba43a34
UpdatedUserName <rodnovspb@mail.ru>
2022-05-01 07:43:50 +0300

    01.03.22

--- php/laravel/laravel.local/routes/web.php
+++ php/laravel/laravel.local/routes/web.php
@@ -17,18 +17,14 @@ Route::get('/', function () {
     return 'главная';
 });
 
-Route::get('/user/{id}/{name}', function ($id, $name) {
-    return "ID: $id, name: $name";
-})->where('name', '[a-z]{2,}')->where('id', '\d+');
+Route::get('/{id}', function ($id) {
+    return "whereNumber $id";
+})->whereNumber('id');
 
-Route::get('/posts/{date}', function ($date){
-    return "Дата: $date";
-})->where('date', '\d{4}-\d{2}-\d{2}');
+Route::get('/{slug}', function ($slug) {
+    return "$slug";
+})->whereAlpha('slug');
 
-Route::get('/date/{year}/{month}/{day}', function ($year, $month, $day){
-    return "Дата: $year-$month-$day";
-})->where('year', '\d{2,4}')->where('month', '\d{1,2}')->where('day', '\d{1,2}');
-
-Route::get('/{order}', function ($order){
-    return "$order";
-})->where('order', '(name|surname|age)');
+Route::get('/{slugNum}', function ($slugNum) {
+    return "whereAlphaNumeric $slugNum";
+})->whereAlphaNumeric('slugNum');
