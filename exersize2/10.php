66c49314b5815c4f692e82b719d3ba87b62a8efe
UpdatedUserName <rodnovspb@mail.ru>
2022-05-22 02:29:15 +0300

    15.03.22

--- exersize2/10.php
+++ exersize2/10.php
@@ -1,12 +1,47 @@
 <?php
 
-$str = ['e0'=>213, "qweqwe"=>"dasd"];
-
-$json = json_encode($str);
 
 
-$obj = json_decode($json);
 
-foreach ($obj as $item=>$value){
-    echo $item . ' ' . $value;
+function printValues($arr){
+    global $count;
+    global $values;
+
+    if(!is_array($arr)){
+        die("Это не массив");
+    }
+
+    foreach ($arr as $key=>$value){
+        if(is_array($value)){
+            printValues($value);
+        } else {
+            $values[] = $value;
+            $count++;
+        }
+    }
+
+    return array('total'=>$count, 'values'=>$values);
 }
+
+$json = '{
+    "book": {
+        "name": "Harry Potter and the Goblet of Fire",
+        "author": "J. K. Rowling",
+        "year": 2000,
+        "characters": ["Harry Potter", "Hermione Granger", "Ron Weasley"],
+        "genre": "Fantasy Fiction",
+        "price": {
+            "paperback": "$10.40", "hardcover": "$20.32", "kindle": "4.11"
+        }
+    }
+}';
+
+$arr = json_decode($json, true);
+
+echo "<pre>";
+print_r(printValues($arr));
+echo "</pre>";
+
+$a = printValues($arr);
+
+echo implode('<br>', $a['values']);
