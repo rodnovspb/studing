82c0ef5ef8d2bcb17f32fa8e410ec674401da6a4
UpdatedUserName <rodnovspb@mail.ru>
2022-05-22 02:14:11 +0300

    15.03.22

--- exersize2/10.php
+++ exersize2/10.php
@@ -1,11 +1,12 @@
 <?php
 
-function callback($var){
-    return strlen($var);
+$str = ['e0'=>213, "qweqwe"=>"dasd"];
+
+$json = json_encode($str);
+
+
+$obj = json_decode($json);
+
+foreach ($obj as $item=>$value){
+    echo $item . ' ' . $value;
 }
-
-$arr = ["Apple", "Asus", "Huawei", "Lenovo"];
-
-$lengts = array_map('callback', $arr);
-
-print_r($lengts);
