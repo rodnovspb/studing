f014955accac3378b47127812a11f22d2e6da8f5
UpdatedUserName <rodnovspb@mail.ru>
2022-05-22 03:24:40 +0300

    15.03.22

--- exersize2/10.php
+++ exersize2/10.php
@@ -2,46 +2,45 @@
 
 
 
-
-function printValues($arr){
-    global $count;
-    global $values;
-
-    if(!is_array($arr)){
-        die('не массив');
-    }
-
-    foreach ($arr as $key=>$value){
-        if(is_array($value)){
-            printValues($value);
-        } else {
-            $values[] = $value;
-            $count++;
+function divide($num1, $num2){
+    try{
+        if($num2 === 0){
+            throw new Exception('Деление на 0');
         }
+        echo $num1/$num2;
+    } catch (Exception $e){
+        echo $e;
+    } finally {
+        echo "Функция завершена";
     }
-
-    return array('total'=>$count, 'values'=>$values);
 }
 
-$json = '{
-    "book": {
-        "name": "Harry Potter and the Goblet of Fire",
-        "author": "J. K. Rowling",
-        "year": 2000,
-        "characters": ["Harry Potter", "Hermione Granger", "Ron Weasley"],
-        "genre": "Fantasy Fiction",
-        "price": {
-            "paperback": "$10.40", "hardcover": "$20.32", "kindle": "4.11"
+divide(3, 87878);
+
+
+?>
+
+<script>
+    function div(num1, num2){
+        if(num2 === 0){
+          throw new Error('Деление на 0')
         }
-    }
-}';
+        return num1/num2
+      }
 
-$arr = json_decode($json, true);
 
-echo "<pre>";
-print_r(printValues($arr));
-echo "</pre>";
 
-$a = printValues($arr);
 
-echo implode('<br>', $a['values']);
+try {
+  div(2,0)
+} catch (e){
+      console.log(e)
+} finally {
+  console.log('finally')
+}
+
+
+
+
+
+</script>
