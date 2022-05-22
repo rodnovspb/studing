8773c157aa23c40126f3f394fbeb4b59be085309
UpdatedUserName <rodnovspb@mail.ru>
2022-05-22 03:48:40 +0300

    15.03.22

--- exersize2/10.php
+++ exersize2/10.php
@@ -1,51 +1,22 @@
 <?php
 
+class EmptyEmail extends Exception {};
+class InvalidEmail extends Exception {};
 
 
-function divide($num1, $num2){
-    try{
-        if($num2 === 0){
-            throw new Exception('Деление на 0', 111);
+function checkEmail($email){
+    try {
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+            throw new InvalidEmail('Неправильная почта');
+        } elseif (empty($email)){
+            throw new EmptyEmail('Пустая почта');
         }
-        echo $num1/$num2;
-    } catch (Exception $e){
-        $message = $e->getMessage();
-        $code = $e->getCode();
-        $line = $e->getLine();
-        echo "$message  с кодом $code на строчке $line в файле {$e->getFile()}";
-
-    } finally {
-        echo "Функция завершена";
+        echo "Успешно";
+    } catch (InvalidEmail $e){
+        echo $e->getMessage();
+    } catch (EmptyEmail $e){
+        echo $e->getMessage();
     }
 }
 
-divide(3, 0);
-
-
-?>
-
-<script>
-    function div(num1, num2){
-        if(num2 === 0){
-          throw new ReferenceError('Деление на 0')
-        }
-        return num1/num2
-      }
-
-
-
-
-try {
-  div(2,0)
-} catch (e){
-      console.log(e.message)
-      console.log(e.name)
-} finally {
-  console.log('finally')
-}
-
-
-
-
-
-</script>
+checkEmail('asdf@sf.df');
