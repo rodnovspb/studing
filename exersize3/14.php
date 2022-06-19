e06a42d90f12ac24e41581442abd7dedf0bc64a3
UpdatedUserName <rodnovspb@mail.ru>
2022-06-19 23:15:20 +0300

    17.06.2022

---
+++ exersize3/14.php
@@ -0,0 +1,4 @@
+<?php
+
+
+file_get_contents('http://wordpress/?a=5');
--- php/wordpress/wp-content/themes/mytheme/index.php
+++ php/wordpress/wp-content/themes/mytheme/index.php
@@ -1,51 +1,45 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<title><?php echo wp_get_document_title(); ?></title>
+		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
 
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<title>
-	  <?php echo wp_get_document_title() ." - ". get_bloginfo('name')?>
-	</title>
-	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
-    <?php wp_head(); ?>
-</head>
-
-<body>
-	<header class="header">
-		<h1><?php bloginfo( 'name' ); ?></h1>
-		<h2><?php bloginfo( 'description' ); ?></h2>
-	</header>
-
-	<div class="middle">
-		<div><?= get_calendar() ?></div>
-	<?php
-	include_once "show.php";
-    // циклы вывода записей
-    // если записи найдены
-    if ( have_posts() ){
-        while ( have_posts() ){
-            the_post();
-
-            echo '<h3><a href="'. get_permalink() .'">'. get_the_title() .'</a></h3>';
+        <?php wp_head(); ?>
+	</head>
+	<body>
+		<header class="header">
+			<h1><?php bloginfo( 'name' ); ?></h1>
+			<h2><?php bloginfo( 'description' ); ?></h2>
+		</header>
+		<div class="middle">
+			<?php
+            if (have_posts()) {
+                while (have_posts()) {
+                    the_post();
 
 
-            echo "<div>" . get_the_content() . "</div>";
+                    if(is_single()){
+                      echo "<h1>" . get_the_title() ."</h1>";
+                      echo "<div>Автор: " . get_the_author() . "</div>";
+                      echo "<div>Время публикации: " . get_the_time() . "</div>";
+					} else {
+                      echo '<h2>'. get_the_title() .' </h2>';
+                      echo "<div><a href='" . get_permalink() . "'>подробнее</a></div>";
+                      echo "<div>" . the_post_thumbnail() . "</div>";
+					}
+						the_content();
 
-            echo "<br>";
-        }
-    }
-    // елси записей не найдено
-    else{
-        echo ' <p>Записей нет...</p>';
-    }
-    ?>
-	</div>
+                }
+            } else {
+                echo '<p>Записей нет...</p>';
+            }
+            ?>
+		</div>
+		<footer class="footer">
+			<?php echo date('Y') ?> © Я и компания моя
+		</footer>
 
-	<footer class="footer">
-		<?php echo date('Y') ?> © Я и компания моя
-	</footer>
-
-    <?php wp_footer(); ?>
-</body>
-
+        <?php wp_footer(); ?>
+	</body>
 </html>
