94febd2b58942f323ba99742be9a160f41555f84
UpdatedUserName <rodnovspb@mail.ru>
2022-06-22 01:34:41 +0300

    22.06.2022

--- exersize3/9.php
+++ exersize3/9.php
@@ -10,33 +10,33 @@ $name = 'mydb';
 require_once 'show.php';
 
 
-///////Процедурный Mysqli
-$link = mysqli_connect($host, $user, $pass, $name);
-mysqli_query($link, "SET NAMES 'utf8'");
-
-$query = "SELECT * FROM persons";
-$res = mysqli_query($link, $query) or die(mysqli_error($link));
-if(mysqli_num_rows($res)>0){
-    echo "<table>";
-    echo "<tr>";
-    echo "<th>id</th>";
-    echo "<th>first_name</th>";
-    echo "<th>last_name</th>";
-    echo "<th>email</th>";
-    echo "</tr>";
-    while ($row = mysqli_fetch_assoc($res)){
-        echo "<tr>";
-        echo "<td>$row[id]</td>";
-        echo "<td>$row[first_name]</td>";
-        echo "<td>$row[last_name]</td>";
-        echo "<td>$row[email]</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-    mysqli_free_result($res);
-} else {
-    echo "Записей, соответствующих вашему запросу, не найдено.";
-}
+/////////Процедурный Mysqli
+//$link = mysqli_connect($host, $user, $pass, $name);
+//mysqli_query($link, "SET NAMES 'utf8'");
+//
+//$query = "SELECT * FROM persons WHERE last_name='Роднов'";
+//$res = mysqli_query($link, $query) or die(mysqli_error($link));
+//if(mysqli_num_rows($res)>0){
+//    echo "<table>";
+//    echo "<tr>";
+//    echo "<th>id</th>";
+//    echo "<th>first_name</th>";
+//    echo "<th>last_name</th>";
+//    echo "<th>email</th>";
+//    echo "</tr>";
+//    while ($row = mysqli_fetch_assoc($res)){
+//        echo "<tr>";
+//        echo "<td>$row[id]</td>";
+//        echo "<td>$row[first_name]</td>";
+//        echo "<td>$row[last_name]</td>";
+//        echo "<td>$row[email]</td>";
+//        echo "</tr>";
+//    }
+//    echo "</table>";
+//    mysqli_free_result($res);
+//} else {
+//    echo "Записей, соответствующих вашему запросу, не найдено.";
+//}
 
 
 
@@ -49,30 +49,74 @@ if(mysqli_num_rows($res)>0){
 //}
 //
 //try{
-//    $query = "INSERT INTO persons2 (first_name, last_name, email) VALUES (:first_name, :last_name, :email)";
-//    $stmt = $pdo->prepare($query);
-//    $stmt->bindParam(':first_name', $first_name, PDO::PARAM_STR);
-//    $stmt->bindParam(':last_name', $last_name, PDO::PARAM_STR);
-//    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-//
-//    $first_name = "Hermione1";
-//    $last_name = "Grange2r";
-//    $email = "hermieonegranger@mail.com";
-//
-//    $stmt->execute();
-//
-//    $first_name = "Hermion2e1";
-//    $last_name = "Grangee2r";
-//    $email = "hermieonegrranger@mail.com";
-//
-//    $stmt->execute();
-//
-//    echo "Записи успешно вставлены.";
+//    $query = "SELECT * FROM persons2";
+//    $result = $pdo->query($query);
+//    if($result->rowCount()>0){
+//            echo "<table>";
+//    echo "<tr>";
+//    echo "<th>id</th>";
+//    echo "<th>first_name</th>";
+//    echo "<th>last_name</th>";
+//    echo "<th>email</th>";
+//    echo "</tr>";
+//    while ($row = $result->fetch()){
+//        echo "<tr>";
+//        echo "<td>$row[id]</td>";
+//        echo "<td>$row[first_name]</td>";
+//        echo "<td>$row[last_name]</td>";
+//        echo "<td>$row[email]</td>";
+//        echo "</tr>";
+//    }
+//    echo "</table>";
+//    unset($result);
+//    }
 //
 //} catch (PDOException $e){
 //    die("Ошибка: " . $e->getMessage());
 //}
 
 
+//////Выборка данных в стиле PDO (+ подготовленные операторы)
+    echo "<table style='border: solid 1px black;'>";
+    echo "<tr><th>Id</th><th>Имя</th><th>Фамилия</th><th>E-mail</th></tr>";
+
+    class Tablerows extends RecursiveIteratorIterator {
+        function __construct($it)
+        {
+            parent::__construct($it, self::LEAVES_ONLY);
+        }
 
 
+    function current(){
+        return "<td style='width: 150px; border: 1px solid black;'>" .
+            parent::current(). "</td>";
+    }
+
+        function beginChildren() {
+            echo "<tr>";
+        }
+
+        function endChildren() {
+            echo "</tr>" . "\n";
+        }
+    }
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$name",$user,$pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $pdo->prepare('SELECT * FROM persons WHERE id=10');
+    $stmt->execute();
+
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    foreach (new Tablerows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v){
+        echo $v;
+    }
+
+
+} catch (PDOException $e){
+    echo "Ошибка " . $e->getMessage();
+}
+
+$conn = null;
+echo "</table>";
