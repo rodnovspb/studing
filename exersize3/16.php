2e0c2cf7a5e31fc40392f94fbf21c74d53577953
UpdatedUserName <rodnovspb@mail.ru>
2022-06-23 02:28:00 +0300

    22.06.2022

---
+++ exersize3/16.php
@@ -0,0 +1,4 @@
+<?php
+$test;
+
+var_dump($test==true);
--- php/wordpress/wp-content/themes/mytheme/functions.php
+++ php/wordpress/wp-content/themes/mytheme/functions.php
@@ -14,3 +14,16 @@ function css_to_wp_head(){
     ";
     wp_add_inline_style('style2.css', $inline_style);
 }
+
+
+add_action('wp_head', 'js_to_wp_head');
+function js_to_wp_head() {
+    wp_enqueue_script('script0.js', get_stylesheet_directory_uri() . '/script0.js');
+    wp_enqueue_script('script.js', get_stylesheet_directory_uri() . '/script.js');
+    wp_enqueue_script('script2.js', get_stylesheet_directory_uri() . '/script2.js');
+    $script = "console.log(`четвертый скрипт`)";
+    wp_add_inline_script('script2.js', $script, 'after');
+}
+
+
+
