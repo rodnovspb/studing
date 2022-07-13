161ef4c78f987eab0b9a55a0564cfdd99583691b
UpdatedUserName <rodnovspb@mail.ru>
2022-07-13 23:51:21 +0300

    13.07.2022

--- php/wordpress/wp-content/themes/mytheme/single.php
+++ php/wordpress/wp-content/themes/mytheme/single.php
@@ -1,4 +1,14 @@
 	<?php get_header(); ?>
+	<?php
+	require_once 'show.php';
+	foreach(get_the_category() as $item){
+        echo sprintf('<a href="%s">%s</a>', get_category_link($item), $item->name) . "<br>";
+
+	}
+
+
+	?>
+
 	<div class="middle">
 		<?php the_post(); ?>
 	  	<h1><?php the_title(); ?></h1>
@@ -7,20 +17,7 @@
 		  <div>Дата публикации: <?php the_date();?></div>
 		</div>
 	  	<?php the_content();?>
-		<div class="single_flex">
-		  <div>
-			<?php echo do_shortcode("[site_url]")?>
-		  </div>
-		  <div>
-			<ul>
-			  <?php $cats = get_the_category(get_the_ID());
-			  foreach ($cats as $cat): ?>
-			  <li><?= sprintf('<a href="%s">%s</a>', get_category_link($cat), $cat->name); ?></li>
-			  <?php endforeach; ?>
-			</ul>
-		  </div>
-		</div>
-
+        <?php echo do_shortcode("[site_url]")?>
 		<?php comments_template(); ?>
 	</div>
     <?php get_footer(); ?>
