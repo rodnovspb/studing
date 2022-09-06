b956c10bca758d6c96d01f27c393d351f1c8a03f
UpdatedUserName <rodnovspb@mail.ru>
2022-09-07 01:02:37 +0300

    04.09.2022

--- openserver-sites/laravel.ru/app/Http/Controllers/HomeController.php
+++ openserver-sites/laravel.ru/app/Http/Controllers/HomeController.php
@@ -4,14 +4,25 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Support\Facades\DB;
+
 class HomeController extends Controller
 {
-    public function index(){
-        dump(config('services.mailgun.endpoint'));
-        return view('home', ['res'=>'Денис']);
+    public function index()
+    {
+        DB::insert("INSERT INTO posts (title, content) VALUES (?, ?)", ['Статья 4', 'Контент статьи 4']);
+
+
+        $posts = DB::select("SELECT * FROM posts");
+        dump($posts);
+
+
+//        dump(config('services.mailgun.endpoint'));
+//        return view('home', ['res'=>'Денис']);
     }
 
-    public function test(){
+    public function test()
+    {
         return __METHOD__;
     }
 
