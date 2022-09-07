0319aee3e9f87c371924aa9dec06ea6e9c6530a0
UpdatedUserName <rodnovspb@mail.ru>
2022-09-07 03:52:41 +0300

    07.09.2022

--- openserver-sites/laravel.ru/app/Http/Controllers/HomeController.php
+++ openserver-sites/laravel.ru/app/Http/Controllers/HomeController.php
@@ -10,12 +10,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-//        $data = DB::table('country')->select('Name', 'Capital')->get();
-
-//          $data = DB::table('city')->where('ID', '=', 15)->value('District');
-
-//        $data = DB::table('country')->pluck('Name');
-        $data = DB::table('country')->select('Name')->get();
+        $data = DB::table('country')->avg('Capital');
 
 
           dump($data) ;
