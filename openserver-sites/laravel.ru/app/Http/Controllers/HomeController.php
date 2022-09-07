2b98ab69b4099fe9811ff18105c30170c819692e
UpdatedUserName <rodnovspb@mail.ru>
2022-09-07 03:56:46 +0300

    07.09.2022

--- openserver-sites/laravel.ru/app/Http/Controllers/HomeController.php
+++ openserver-sites/laravel.ru/app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $data = DB::table('country')->avg('Capital');
+        $data = DB::table('city')->select('CountryCode')->distinct()->get();
 
 
           dump($data) ;
