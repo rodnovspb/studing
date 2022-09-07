a8dd5ee9099f80c58dba8255a76742008e401f1b
UpdatedUserName <rodnovspb@mail.ru>
2022-09-07 23:47:48 +0300

    07.09.2022

--- openserver-sites/laravel.ru/app/Http/Controllers/HomeController.php
+++ openserver-sites/laravel.ru/app/Http/Controllers/HomeController.php
@@ -10,7 +10,11 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $data = DB::table('city')->select('CountryCode')->distinct()->get();
+        $data = DB::table('city')
+            ->select('city.ID', 'city.Name as city_name', 'country.Code', 'country.Name as country_name')
+            ->limit(10)->join('country', 'city.CountryCode', '=', 'country.Code')
+            ->orderBy('city.ID')
+            ->get();
 
 
           dump($data) ;
