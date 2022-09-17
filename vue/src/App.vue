7bbe7b8d453e93d1bf7cb2b9a4263e2923d5ee7d
UpdatedUserName <rodnovspb@mail.ru>
2022-09-18 02:37:24 +0300

    14.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -9,23 +9,14 @@
 export default {
   data: function() {
     return {
-      num: 532423543453456214
+      num: "5321234324234234322411d32"
     };
   },
   methods: {
     cut3: function(value) {
-      let str = String(value)
-      let str2 = ''
-      for(let i=0; i<str.length; i++){
-        if(i%3===0){
-          str2 += str[i]
-          str2+= ' '
-        } else {
-          str2 += str[i]
-        }
+      return String(value).match(/.{1,3}/g).map(elem=>elem+=' ').join('')
 
-      }
-      return str2
+
     }
   }
 };
