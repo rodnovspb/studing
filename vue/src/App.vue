737260141c512177fb74e44f38682597832edca6
UpdatedUserName <rodnovspb@mail.ru>
2022-09-18 00:40:46 +0300

    14.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <button @mouseover="currentDate">Дата</button>
-
+  <p @load="func"></p>
 
 </template>
 
@@ -9,13 +9,18 @@
   export default {
       data() {
       return {
-        obj: {x: 1, y: 2, z: 3},
+        num1: 1,
+        num2: 2,
       }
     },
     methods: {
         currentDate(){
           let date = new Date;
           alert (`${date.getDate()}:${date.getMonth()+1}:${date.getFullYear()}`)
+        },
+        func(){
+          let sum = this.num1 + this.num2
+          alert(sum)
         }
     }
   }
