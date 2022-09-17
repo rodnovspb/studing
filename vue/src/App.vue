f1130e31f1029d7e0d55cfb2b7aaa219eeb738a0
UpdatedUserName <rodnovspb@mail.ru>
2022-09-17 23:26:00 +0300

    14.09.2022

---
+++ vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b>Привет, мир!</b>
+  </div>
+  <div>Имя: {{ name }}</div>
+  <div>Фамилия: {{ surname }}</div>
+</template>
+
+
+<script>
+  export default {
+    name: "App",
+    data(){
+      return {
+        text: 'page',
+        href: 'page.html',
+      }
+    }
+  }
+</script>
