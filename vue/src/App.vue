72e8adeca4319966b960838eb84299d32fdfb384
UpdatedUserName <rodnovspb@mail.ru>
2022-09-18 02:28:06 +0300

    14.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>{{ getDay(num) }}</h1>
+    <h1>{{ cut3(num) }}</h1>
   </div>
 </template>
 
@@ -9,13 +9,23 @@
 export default {
   data: function() {
     return {
-      num: 5
+      num: 532423543453456214
     };
   },
   methods: {
-    getDay: function(value) {
-      const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-      return week[value-1]
+    cut3: function(value) {
+      let str = String(value)
+      let str2 = ''
+      for(let i=0; i<str.length; i++){
+        if(i%3===0){
+          str2 += str[i]
+          str2+= ' '
+        } else {
+          str2 += str[i]
+        }
+
+      }
+      return str2
     }
   }
 };
