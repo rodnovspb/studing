ad7e7d76fb3650bec9b5775785d76ce0e0116cb8
UpdatedUserName <rodnovspb@mail.ru>
2022-09-20 08:19:50 +0300

    14.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,24 +1,19 @@
 <template>
   <div>
-    <h1>{{ round(root(num)) }}</h1>
+    <h1>{{ num }}</h1>
   </div>
 </template>
 
 
 <script>
-export default {
-  data: function() {
-    return {
-      num: 12124
-    };
-  },
-  methods: {
-    root: function(value) {
-      return Math.sqrt(value)
-    },
-    round(num){
-      return num.toFixed(2)
+  export default {
+    data(){
+      return {
+        num: 100,
+      }
     }
   }
-};
+
+
+
 </script>
