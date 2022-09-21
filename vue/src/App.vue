c520a11a88340ffd819cd51378b218fe680654f7
UpdatedUserName <rodnovspb@mail.ru>
2022-09-21 22:34:21 +0300

    14.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
-    <h1>{{ num }}</h1>
+    <h1>{{ round(2) }}</h1>
   </div>
 </template>
 
 
 <script>
+
   export default {
     data(){
       return {
-        num: 100,
+        num: 100.6857489,
+      }
+    },
+    methods: {
+      round(count){
+        return this.num.toFixed(count)
       }
     }
+
   }
 
 
