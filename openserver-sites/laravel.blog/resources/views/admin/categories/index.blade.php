ec4212be018b40bf0f25dc3ec54a30d0b3915fb0
UpdatedUserName <rodnovspb@mail.ru>
2022-09-22 01:02:33 +0300

    14.09.2022

---
+++ openserver-sites/laravel.blog/resources/views/admin/categories/index.blade.php
@@ -0,0 +1,82 @@
+@extends('admin.layouts.layout')
+
+@section('content')
+    <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Категории</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Blank Page</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Список категорий</h3>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+              <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+              <i class="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+        <div class="card-body">
+            <a class="btn btn-primary mb-3"  href="{{ route('categories.create') }}">Добавить категорию</a>
+            @if(!empty($categories))
+          <table class="table table-bordered">
+                <thead>
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th>Наименование</th>
+                      <th>Слаг</th>
+                      <th style="width: 40px">Действия</th>
+                    </tr>
+                </thead>
+                <tbody>
+                       <tr>
+                         <td>1.</td>
+                         <td>Title</td>
+                         <td>slug</td>
+                         <td>
+                             <a href="#" class="btn btn-info btn-sm float-left mr-1">
+                                 <i class="fas fa-pencil-alt"></i>
+                             </a>
+                             <form action="" method="post" class="float-left">
+                                 <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Подтвердите удаление')"></button>
+                             </form>
+                         </td>
+                       </tr>
+                </tbody>
+          </table>
+            @else
+            <p>Категорий нет</p>
+            @endif
+        </div>
+          <!-- /.card-body -->
+        <div class="card-footer">
+          Footer
+        </div>
+          <!-- /.card-footer-->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+@endsection
