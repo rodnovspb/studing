686102e5ff01b9021853d1fe929f44bc9a09b519
UpdatedUserName <rodnovspb@mail.ru>
2022-09-22 23:47:03 +0300

    14.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <button @click="toggle">{{ show ? 'скрыть':'показать'}}</button>
   <template v-if="show">
     <p>Lorem ipsum dolor sit amet.</p>
     <p>Lorem ipsum dolor sit amet.</p>
@@ -13,6 +14,11 @@
     data(){
       return {
         show: false
+      }
+    },
+    methods: {
+      toggle(){
+        this.show = !this.show
       }
     }
   }
