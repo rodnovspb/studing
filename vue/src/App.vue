e1dd5612898d81c8aa794da734691db583a0f9c3
UpdatedUserName <rodnovspb@mail.ru>
2022-09-22 23:24:51 +0300

    14.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,33 +1,16 @@
 <template>
-  <button @click="toggle1">{{ hidden1? 'показать':"скрыть" }}</button>
-  <button @click="toggle2">{{ hidden2? 'показать':"скрыть" }}</button>
-  <button @click="toggle3">{{ hidden3? 'показать':"скрыть" }}</button>
-  <p v-if="!hidden1">Абзац 1</p>
-  <p v-if="!hidden2">Абзац 2</p>
-  <p v-if="!hidden3">Абзац 3</p>
+  <p v-if="isAuth">авторизован</p>
+  <p v-else>не авторизован</p>
 </template>
 
 
 <script>
 
   export default {
-   data(){
-     return {
-       hidden1: true,
-       hidden2: true,
-       hidden3: true,
-     }
-   },
-    methods: {
-     toggle1(){
-       this.hidden1 = !this.hidden1
-     },
-      toggle2(){
-        this.hidden2 = !this.hidden2
-      },
-      toggle3(){
-        this.hidden3 = !this.hidden3
-      },
+    data(){
+      return {
+        isAuth: false
+      }
     }
   }
 
