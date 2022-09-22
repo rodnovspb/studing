5e80e5b07106f894f067cde30764c44b5dc9de7f
UpdatedUserName <rodnovspb@mail.ru>
2022-09-22 23:11:23 +0300

    14.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <button @click="toggle">изменить</button>
-	<p v-if="!hidden">ыфвфывфы</p>
+  <button @click="toggle1">Абзац 1</button>
+  <button @click="toggle2">Абзац 2</button>
+  <button @click="toggle3">Абзац 3</button>
+  <p v-if="!hidden1">Абзац 1</p>
+  <p v-if="!hidden2">Абзац 2</p>
+  <p v-if="!hidden3">Абзац 3</p>
 </template>
 
 
@@ -9,13 +13,21 @@
   export default {
    data(){
      return {
-       hidden: false
+       hidden1: true,
+       hidden2: true,
+       hidden3: true,
      }
    },
     methods: {
-     toggle(){
-       this.hidden = !this.hidden
-     }
+     toggle1(){
+       this.hidden1 = !this.hidden1
+     },
+      toggle2(){
+        this.hidden2 = !this.hidden2
+      },
+      toggle3(){
+        this.hidden3 = !this.hidden3
+      },
     }
   }
 
