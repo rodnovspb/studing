f0e936c6e03b75c3a1b80fe4f841d6ff7e0c1c6d
UpdatedUserName <rodnovspb@mail.ru>
2022-09-24 16:07:20 +0300

    14.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,8 +1,12 @@
 <template>
 
-  <ul>
-    <li v-for="elem of hrefs" :key="elem"><a href="{{ elem.href }}">{{ elem.text }}</a></li>
-  </ul>
+  <table>
+    <tr v-for="elem in products" :key="elem">
+        <td>{{ elem.name }}</td>
+        <td>{{ elem.price }}</td>
+        <td>{{ elem.quantity }}</td>
+    </tr>
+  </table>
 
 
 
@@ -15,10 +19,22 @@
   export default {
     data(){
       return {
-        hrefs: [
-          {href: `https://ya.ru/`, text: 'text1'},
-          {href: '2.html', text: 'text2'},
-          {href: '3.html', text: 'text3'},
+        products: [
+          {
+            name: 'product1',
+            price: 100,
+            quantity: 5
+          },
+          {
+            name: 'product2',
+            price: 200,
+            quantity: 4
+          },
+          {
+            name: 'product3',
+            price: 300,
+            quantity: 3
+          },
         ],
       }
     },
