6cd53e536383e79275fe615e221c10a2db710f2a
UpdatedUserName <rodnovspb@mail.ru>
2022-09-24 16:09:44 +0300

    14.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,14 +1,6 @@
 <template>
 
-  <table>
-    <tr v-for="elem in products" :key="elem">
-        <td>{{ elem.name }}</td>
-        <td>{{ elem.price }}</td>
-        <td>{{ elem.quantity }}</td>
-    </tr>
-  </table>
-
-
+  <p v-for="num of 10" :key="num">qwe</p>
 
 
 </template>
@@ -19,23 +11,7 @@
   export default {
     data(){
       return {
-        products: [
-          {
-            name: 'product1',
-            price: 100,
-            quantity: 5
-          },
-          {
-            name: 'product2',
-            price: 200,
-            quantity: 4
-          },
-          {
-            name: 'product3',
-            price: 300,
-            quantity: 3
-          },
-        ],
+
       }
     },
     methods: {
