db3f1942ffa5cce66d2cfb86faf45e92e3a8ebb1
UpdatedUserName <rodnovspb@mail.ru>
2022-09-25 19:57:28 +0300

    25.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,13 @@
 <template>
-    <p :class="{active: true}">Lorem ipsum dolor sit amet.</p>
+    <p :class="{active: true, 'with-error': true}">Lorem ipsum dolor sit amet.</p>
 </template>
 
 <style>
   .active {
     text-decoration: underline;
+  }
+  .with-error {
+    color: #f00;
   }
 </style>
 
