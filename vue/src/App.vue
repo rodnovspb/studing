491d6fb1b7f3395c0ae5dc9f80bd65e848c0dfb3
UpdatedUserName <rodnovspb@mail.ru>
2022-09-25 19:25:23 +0300

    25.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <p :class="cssClasses">Lorem ipsum dolor sit amet.</p>
+  <p :class="obj">Lorem ipsum dolor sit amet.</p>
 </template>
 
 <style>
-  .active {
-    border: 1px solid blue;
-
+  .done {
+    background-color: blue;
   }
 </style>
 
@@ -14,7 +13,10 @@
   export default {
       data(){
         return {
-          cssClasses: 'active valid'
+         obj: {
+           done: true,
+           selected: false
+         }
         }
       }
   }
