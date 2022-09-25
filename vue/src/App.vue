76d373bc67d6b8af1ea92df21c5e1e47586eeed1
UpdatedUserName <rodnovspb@mail.ru>
2022-09-25 20:37:16 +0300

    25.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <p :class="{black: isActive, white: isDisabled}">Lorem ipsum dolor sit amet.</p>
+
+
+  <p :style="{color: 'green', backgroundColor: 'yellow'}">Lorem ipsum dolor sit amet.</p>
+  <p :style="{fontWeight: 'bold', 'font-style': 'Italic'}">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, ratione.</p>
+
 </template>
 
 <style>
-  .black {
-    background-color: #bf1f1f;
-  }
-  .white {
-    color: #fff;
-  }
+
 </style>
 
 <script>
