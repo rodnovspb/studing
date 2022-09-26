1315fc57ca7eb58921c8f68622852e6202973eca
UpdatedUserName <rodnovspb@mail.ru>
2022-09-27 00:32:35 +0300

    25.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <input type="text" v-model="text">
-  <button @click="cut">Разбить</button>
-  <p>{{ surname }}</p>
-  <p>{{ name }}</p>
-  <p>{{ patronymic }}</p>
+  <textarea v-model="text"></textarea>
+  <p>{{ text }}</p>
 
 </template>
 
@@ -12,18 +9,7 @@
   export default {
         data(){
           return {
-            surname: '',
-            name: '',
-            patronymic: '',
             text: ''
-          }
-        },
-        methods: {
-          cut(){
-            let arr = this.text.match(/\p{L}+/gui);
-            this.surname = arr[0]
-            this.name = arr[1]
-            this.patronymic = arr[2]
           }
         }
       }
