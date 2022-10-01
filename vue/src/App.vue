1df58c04f5e6303b44b6125c585798afe44c99bf
UpdatedUserName <rodnovspb@mail.ru>
2022-10-01 23:22:31 +0300

    25.09.2022

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,43 +1,27 @@
 <template>
-  <MyEmployee
-      v-for="user of users" :key="user.id"
-      :name="user.name"
-      :salary="user.salary"
-      :age="user.age"
-  />
+
+
+  <MyComponent :func="func"/>
+
+
 </template>
 
 <script>
-  import MyEmployee from "./components/MyEmployee.vue";
+  import MyComponent from "@/components/MyComponent";
 
   export default {
     components: {
-      MyEmployee
+      MyComponent
     },
     data(){
       return {
-        users: [
-          {
-            id: 1,
-            name: 'name1',
-            salary: 100,
-            age: 30,
-          },
-          {
-            id: 2,
-            name: 'name2',
-            salary: 200,
-            age: 40,
-          },
-          {
-            id: 3,
-            name: 'name3',
-            salary: 300,
-            age: 50,
-          },
-        ],
-      }
 
+      }
+    },
+    methods: {
+      func(){
+        console.log('!!!')
+      }
     }
   }
 
---
+++ vue/src/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <button v-on:click="func">Нажать</button>
+</template>
+
+<script>
+  export default {
+    props: {
+      func: Function,
+    },
+    data(){
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
+
+</script>
