95f902c3059248cbbd735ba1e283e6157a9c230b
UpdatedUserName <rodnovspb@mail.ru>
2022-10-07 15:25:37 +0300

    05.10.2022

--- ооп/наследование классов/classes/Product.php
+++ ооп/наследование классов/classes/Product.php
@@ -6,39 +6,25 @@ class Product
     public $name;
     public $price;
     
-    public $cpu;
-    public $numPages;
-    
-    public function __construct($name, $price, $cpu = null, $numPages = null)
+    public function __construct($name, $price)
     {
          $this->name = $name;
          $this->price = $price;
-         $this->cpu = $cpu;
-         $this->numPages = $numPages;
     }
     
-    public function getCpu()
-    {
-        return $this->cpu;
-    }
-    
-    public function getNumPages()
-    {
-        return $this->numPages;
-    }
-    
-    public function getProduct($type = 'notebook') {
-        $out = "<hr><b>О товаре</b><br>
-                Наименование: {$this->name}<br>
-                Цена: {$this->price}<br>";
+    public function getProduct() {
+        return  "<b>О товаре</b><br>Наименование: {$this->name}<br>Цена: {$this->price}<br>";
         
-        if($type === 'notebook') {
-            $out .= "Процессор: {$this->cpu}<br>";
-        } else {
-            $out .= "Количество страниц: {$this->numPages}";
-        }
-        
-        return $out;
+    }
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    public function getPrice()
+    {
+        return $this->price;
     }
     
 }
