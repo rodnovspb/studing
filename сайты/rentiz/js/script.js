1a30e55b6cf41ec41b1b5919773cec9fe52b0047
UpdatedUserName <rodnovspb@mail.ru>
2022-10-09 23:55:52 +0300

    09.10.2022

--- сайты/rentiz/js/script.js
+++ сайты/rentiz/js/script.js
@@ -29,10 +29,18 @@ if(filter){
 // ---------Swiper-----------------------------//
 const swiper = new Swiper('.popular-slider', {
 	spaceBetween: 20,
-	slidesPerView: 3,
+	slidesPerView: 1,
 	loop: true,
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
+	breakpoints: {
+		992: {
+			slidesPerView: 3,
+		},
+		660: {
+			slidesPerView: 2,
+		},
+	}
 });
