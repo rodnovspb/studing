e1bafab001ded34fa06510e4ac8379d9eeca4888
UpdatedUserName <rodnovspb@mail.ru>
2022-10-10 01:12:25 +0300

    09.10.2022

--- ооп/абстрактные классы и интерфейсы/index.php
+++ ооп/абстрактные классы и интерфейсы/index.php
@@ -1,20 +1,27 @@
 <?php
 error_reporting(-1);
 require_once 'classes/Product.php';
-require_once 'classes/BookProduct.php';
 require_once 'classes/i3D.php';
+require_once 'classes/BookProduct.php';
 require_once 'show.php';
 
 
 $book = new BookProduct("Три мушкетера", 200, 'Дюма');
 
 
-$book->test();
+show($book);
 
+class A{};
+class B extends A{};
+class C{};
 
+$a = new A;
+$b = new B;
+$c = new C;
 
-
-
+function offerCase($product) {
+    echo "<p>Предлагаем чехол</p>";
+}
 
 
 
