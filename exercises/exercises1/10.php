5a39797ccef9b7132b1770af1abf27b497a47d64
UpdatedUserName <rodnovspb@mail.ru>
2022-10-13 21:15:44 +0300

    10.10.2022

---
+++ exercises/exercises1/10.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="ru">
+<head>
+<meta charset="utf-8">
+<meta name="viewport"
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Документ</title>
+<style>
+
+
+</style>
+</head>
+<body>
+
+
+<a href="?num=5">Ссылка</a>
+
+
+<script>
+
+
+
+
+
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
