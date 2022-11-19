b86f58ec0b3593960f46bb182ed8e5795cc96648
UpdatedUserName <rodnovspb@mail.ru>
2022-11-19 16:08:02 +0300

    19.11.2022

--- openserver-sites/api.loc/index.php
+++ openserver-sites/api.loc/index.php
@@ -4,8 +4,13 @@ require 'show.php';
 
 
 
-if(!empty($_GET['num1']) && !empty($_GET['num2'])){
-    echo rand($_GET['num1'], $_GET['num2']);
+if(!empty($_GET['date1']) && !empty($_GET['date2'])){
+    
+    $timestamp1 = strtotime($_GET['date1']);
+    $timestamp2 = strtotime($_GET['date2']);
+    
+    echo ($timestamp2 - $timestamp1)/60/60/24;
+    
     
 } else {
     echo 'ошибка';
