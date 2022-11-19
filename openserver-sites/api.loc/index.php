f926cc4baf711ca6cb5bc24548a853b5ad99ef0b
UpdatedUserName <rodnovspb@mail.ru>
2022-11-19 16:20:37 +0300

    19.11.2022

--- openserver-sites/api.loc/index.php
+++ openserver-sites/api.loc/index.php
@@ -6,10 +6,15 @@ require 'show.php';
 
 if(!empty($_GET['date1']) && !empty($_GET['date2'])){
     
-    $timestamp1 = strtotime($_GET['date1']);
-    $timestamp2 = strtotime($_GET['date2']);
+    $regxp = '#^\d{4}-\d{1,2}-\d{1,2}$#';
     
-    echo ($timestamp2 - $timestamp1)/60/60/24;
+    if(preg_match($regxp, $_GET['date1']) && preg_match($regxp, $_GET['date2'])){
+        $timestamp1 = strtotime($_GET['date1']);
+        $timestamp2 = strtotime($_GET['date2']);
+        echo ($timestamp2 - $timestamp1)/60/60/24;
+    } else {
+        echo 'ошибка';
+    }
     
     
 } else {
