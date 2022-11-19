8cc0a838e383501b686bc7a70d01aea312446f53
UpdatedUserName <rodnovspb@mail.ru>
2022-11-20 02:14:57 +0300

    19.11.2022

--- openserver-sites/api.loc/index.php
+++ openserver-sites/api.loc/index.php
@@ -1,23 +1,9 @@
 <?php
 
 require 'show.php';
+header('Content-Type: application/json'); // укажем MIME
 
+$arr = range($_GET['num1'], $_GET['num2']);
 
-if(!empty($_GET['date1']) && !empty($_GET['date2'])){
-    
-    $regxp = '#^\d{4}-\d{1,2}-\d{1,2}$#';
-    
-    if(preg_match($regxp, $_GET['date1']) && preg_match($regxp, $_GET['date2'])){
-        $timestamp1 = strtotime($_GET['date1']);
-        $timestamp2 = strtotime($_GET['date2']);
-        echo ($timestamp2 - $timestamp1)/60/60/24;
-    } else {
-        echo 'ошибка';
-    }
-    
-    
-} else {
-    echo 'ошибка';
-}
-
+exit(json_encode($arr));
 
--- openserver-sites/tst.loc/test.php
+++ openserver-sites/tst.loc/test.php
@@ -1,8 +1,10 @@
 <?php
+require 'show.php';
+
+$url = 'http://api.loc/?num1=10&num2=5555';
 
 
-$url = 'http://api.loc';
+$res = json_decode(file_get_contents($url));
 
-
-$res = file_get_contents($url);
+show($res, 1);
 
