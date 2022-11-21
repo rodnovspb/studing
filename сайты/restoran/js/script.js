3275b783498184fee42e3a84619cf3f898768d7f
UpdatedUserName <rodnovspb@mail.ru>
2022-11-21 12:12:13 +0300

    21.11.22

--- сайты/restoran/js/script.js
+++ сайты/restoran/js/script.js
@@ -1,11 +1,11 @@
 const reviewsSlider = new Swiper('.menu__slider', {
 	spaceBetween: 30,
-	slidesPerView: 3,
+	slidesPerView: 2,
 	loop: true,
-	autoplay: false,
+	autoplay: true,
 	breakpoints: {
-		660: {
-			// slidesPerView: 1,
+		1000: {
+			slidesPerView: 3,
 		},
 	}
 });
