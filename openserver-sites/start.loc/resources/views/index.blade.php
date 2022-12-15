30c2a0d3797a0bbfdf87cce9e01b86e1d6d2c8ab
UpdatedUserName <rodnovspb@mail.ru>
2022-12-15 06:59:09 +0300

    15.12.2022

--- openserver-sites/start.loc/resources/views/index.blade.php
+++ openserver-sites/start.loc/resources/views/index.blade.php
@@ -5,10 +5,8 @@
 <main>
 
     <section class="container mx-auto text-white text-center pt-10">
-        <h1 class="text-4xl mb-10">Lorem ipsum dolor sit amet.</h1>
-        <p class="mb-5 text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aspernatur commodi consequuntur error esse ipsa nobis quae quia sapiente sed!</p>
-        <p class="mb-5 text-2xl hover:scale-[1.05] transition">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aspernatur commodi consequuntur error esse ipsa nobis quae quia sapiente sed!</p>
-        <p class="text-2xl leading-10 translate-x-24">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aspernatur commodi consequuntur error esse ipsa nobis quae quia sapiente sed!</p>
+
+        <p class="bg-sky-500 hover:bg-sky-700">Lorem ipsum dolor sit amet.</p>
 
     </section>
 </main>
