538612cbfc8a3be734ecb5c3f9dee16ac34ceca1
UpdatedUserName <rodnovspb@mail.ru>
2022-12-22 18:22:44 +0300

    15.12.2022

--- openserver-sites/start.loc/resources/views/index.blade.php
+++ openserver-sites/start.loc/resources/views/index.blade.php
@@ -2,25 +2,17 @@
 
 @section('content')
 
-    <main>
-
-    <section class="container mx-auto text-white text-center pt-10 text-left">
-
-    <div class="text-left">Lorem ipsum <span class="inline">dolor sit amet</span>, consectetur adipisicing elit. Aperiam consequuntur culpa cupiditate ducimus earum eligendi et facere impedit ipsa, ipsam, laudantium maiores non odio quibusdam saepe, suscipit veniam voluptas. Sit!</div>
-
-        <ul class="hidden ">
-            <li>111</li>
-            <li>111</li>
-            <li>111</li>
-            <li>111</li>
-        </ul>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, magnam.</p>
 
 
+    <form action="" method="post">
+        @csrf
+        <input type="radio" name="radio" value="1">
+        <input type="radio" name="radio" value="2">
+        <input type="checkbox" name="checkbox">
+        <input type="submit">
+    </form>
 
 
-    </section>
-</main>
 
 
 
