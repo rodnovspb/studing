3d4d1a1e9de0edd34348a62e6abdf9e436be2115
UpdatedUserName <rodnovspb@mail.ru>
2023-01-07 03:05:34 +0300

    07.01.2023

--- openserver-sites/shop.loc/app/controllers/admin/DownloadController.php
+++ openserver-sites/shop.loc/app/controllers/admin/DownloadController.php
@@ -44,6 +44,24 @@ class DownloadController extends AppController
         $this->setMeta("Админка :: {$title}");
         $this->set(compact('title'));
     }
+    
+    public function deleteAction() {
+        $id = get('id');
+        if(R::count('order_download', 'download_id = ?', [$id])){
+            $_SESSION['errors'] = 'Невозможно удалить, файл приобретался';
+            redirect();
+        }
+        if(R::count('product_download', 'download_id = ?', [$id])){
+            $_SESSION['errors'] = 'Невозможно удалить, файл прикреплен к товару';
+            redirect();
+        }
+        if($this->model->download_delete($id)){
+            $_SESSION['success'] = 'Файл удален';
+        } else {
+            $_SESSION['errors'] = 'Ошибка удаления';
+        }
+        redirect();
+    }
 }
 
 
