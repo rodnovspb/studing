74839aa49b4c35a6d4e8dfa77ebdd4564f46e655
UpdatedUserName <rodnovspb@mail.ru>
2023-01-07 07:49:44 +0300

    07.01.2023

--- openserver-sites/start.loc/resources/views/index.blade.php
+++ openserver-sites/start.loc/resources/views/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
 
-    <div class="border-black border-2 h-96"><img src="images/1.jpg" alt="" class="w-40 absolute top-[100px]"></div>
-
+<div class="text-4xl ">123235475686789678</div>
+<div class="text-4xl tabular-nums">123235475686789678</div>
 
 
 
