a2c740269dd0dc2c07a251c4dee386c0e412b9ec
UpdatedUserName <rodnovspb@mail.ru>
2023-01-07 07:35:16 +0300

    07.01.2023

--- openserver-sites/start.loc/resources/views/layouts/layout.blade.php
+++ openserver-sites/start.loc/resources/views/layouts/layout.blade.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <link rel="stylesheet" href="/tailwind-temp/tailwind-v2.min.css">
-{{--@vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 <title>Документ</title>
 </head>
 <body class="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen">
