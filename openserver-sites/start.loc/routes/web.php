35160d625d9b90df2c823cb1e119728d7b9f8a98
UpdatedUserName <rodnovspb@mail.ru>
2023-01-10 03:37:04 +0300

    10.01.2023

---
+++ openserver-sites/start.loc/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [MainController::class, 'index'])->name('main')->middleware('tokenisvalid');
+Route::post('/', [MainController::class, 'store'])->name('store');
+
+Route::any('/page', [PageController::class, 'index'])->name('page');
+
+Route::get('/user/{user}', function (User $user) {
+    return $user;
+});
+
+
+Route::resource('users', UserController::class)->missing(function (Request $request) {
+    return redirect()->route('users.index');
+});
+
+Route::fallback(function (){
+    return redirect('/');
+});
+
+
