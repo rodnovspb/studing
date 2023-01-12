86a861346a80c679ca69599eb8a15a697e3b31bf
UpdatedUserName <rodnovspb@mail.ru>
2023-01-12 07:46:58 +0300

    12.01.2023

--- openserver-sites/start.loc/app/Http/Controllers/MainController.php
+++ openserver-sites/start.loc/app/Http/Controllers/MainController.php
@@ -13,19 +13,15 @@ use Illuminate\Support\Facades\Storage;
 class MainController extends Controller
 {
     public function index() {
-
-        dd(Storage::get('files/UWq550R8TDRERgYTo0UKIiQmcxsAz7fS6FOhhnEo.txt'));
-        return Storage::get('files/UWq550R8TDRERgYTo0UKIiQmcxsAz7fS6FOhhnEo.txt');
         return view('index');
     }
 
     public function store(Request $request) {
-        if (!$request->hasFile('file')){ return redirect()->back(); }
-        $file = $request->file('file');
-        $originalName = $file->getClientOriginalName();
-        $path = Storage::putFile('files', new File($file));
-
+        dd($request->file('file')->hashName());
+        return $request->file('file')->store('files');
     }
+
+
 
 
 
