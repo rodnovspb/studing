21d95ba2a242f3cb6482a8d3af0f98ba59b6f8ea
UpdatedUserName <rodnovspb@mail.ru>
2023-01-15 01:58:18 +0300

    14.01.2023

--- openserver-sites/start.loc/app/Http/Controllers/MainController.php
+++ openserver-sites/start.loc/app/Http/Controllers/MainController.php
@@ -7,16 +7,20 @@ use Illuminate\Http\Request;
 
 use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Support\Facades\App;
+
 use Illuminate\Support\Facades\Http;
 
 class MainController extends Controller
 {
     public function index() {
 
-        $response = Http::get('https://lenta.ru/');
 
-        dd($response->body());
-        return $response->body();
+        return  Http::get('https://lenta.ru/');
+
+
+
+
+        return view('main');
 
 
 
