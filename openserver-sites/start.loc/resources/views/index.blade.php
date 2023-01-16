6d328c8bc5654ee0a72421d3bb137a9191a2ffe7
UpdatedUserName <rodnovspb@mail.ru>
2023-01-16 06:14:53 +0300

    16.01.2023

--- openserver-sites/start.loc/resources/views/index.blade.php
+++ openserver-sites/start.loc/resources/views/index.blade.php
@@ -4,7 +4,28 @@
 
 
 
-    <div class="backdrop-invert h-80 w-80 border border-black m-52"></div>
+    <table class="border-separate border-spacing-2 table-auto border border-slate-400 ...">
+  <thead>
+    <tr>
+      <th class="border border-slate-300 ...">Штат</th>
+      <th class="border border-slate-300 ...">Город</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="border border-slate-300 ...">Индиана</td>
+      <td class="border border-slate-300 ...">Индианаполис</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-300 ...">Огайо</td>
+      <td class="border border-slate-300 ...">Колумбус</td>
+    </tr>
+    <tr>
+      <td class="border border-slate-300 ...">Мичиган</td>
+      <td class="border border-slate-300 ...">Детройт</td>
+    </tr>
+  </tbody>
+</table>
 
 
 
