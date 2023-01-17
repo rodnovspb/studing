08eb62d8dc11d99ae5ac002dcb29db5a5b5b43af
UpdatedUserName <rodnovspb@mail.ru>
2023-01-17 06:53:21 +0300

    16.01.2023

--- openserver-sites/start.loc/resources/views/index.blade.php
+++ openserver-sites/start.loc/resources/views/index.blade.php
@@ -5,9 +5,7 @@
 
 
 
-    <svg  class="h-[200px] w-[200px] stroke-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path  stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0l6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z" />
-</svg>
+  <p class="prose prose-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias amet assumenda corporis distinctio dolor doloribus, dolorum eveniet ex exercitationem fugiat id illo, nostrum perspiciatis placeat porro provident quo quod, reiciendis rem rerum saepe sit. Enim exercitationem ipsam minus, nisi omnis porro provident quisquam quod voluptatum? Aspernatur assumenda atque beatae consequuntur cum cupiditate, dignissimos dolor doloribus dolorum eaque enim est facilis ipsam ipsum iusto labore maxime minima molestiae nam necessitatibus nihil nobis non nostrum odit officia praesentium quis quod repellendus reprehenderit repudiandae sequi tempora unde vel vero voluptatem voluptates voluptatibus! Consequuntur, cum cumque delectus dolores eos minima nihil! Aliquid, exercitationem.</p>
 
 
 
