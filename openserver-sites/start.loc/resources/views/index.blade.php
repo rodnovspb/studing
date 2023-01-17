9fcbb8c93d0a4e2fa2b1ea64d5ce19abf9f9e6d6
UpdatedUserName <rodnovspb@mail.ru>
2023-01-17 06:59:40 +0300

    16.01.2023

--- openserver-sites/start.loc/resources/views/index.blade.php
+++ openserver-sites/start.loc/resources/views/index.blade.php
@@ -2,13 +2,32 @@
 
 @section('content')
 
+    <select class="form-select px-4 py-3 rounded-full">
+  <option value="">qweqweqwe</option>
+  <option value="">qweqweqwe</option>
+  <option value="">qweqweqwe</option>
+  <option value="">qweqweqwe</option>
+  <option value="">qweqweqwe</option>
+</select>
 
 
-
-
-<input type="text">
-<input type="password">
-    <input type="email">
+    input[type='text']
+    input[type='password']
+    input[type='email']
+    input[type='number']
+    input[type='url']
+    input[type='date']
+    input[type='datetime-local']
+    input[type='month']
+    input[type='week']
+    input[type='time']
+    input[type='search']
+    input[type='tel']
+    input[type='checkbox']
+    input[type='radio']
+    select
+    select[multiple]
+    textarea
 
 
 @endsection
