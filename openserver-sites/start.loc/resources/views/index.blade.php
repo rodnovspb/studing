28ec80f311a31bb9ee421b15c9f7f33cc63d3cda
UpdatedUserName <rodnovspb@mail.ru>
2023-01-17 06:55:53 +0300

    16.01.2023

--- openserver-sites/start.loc/resources/views/index.blade.php
+++ openserver-sites/start.loc/resources/views/index.blade.php
@@ -5,9 +5,10 @@
 
 
 
-  <p class="prose prose-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias amet assumenda corporis distinctio dolor doloribus, dolorum eveniet ex exercitationem fugiat id illo, nostrum perspiciatis placeat porro provident quo quod, reiciendis rem rerum saepe sit. Enim exercitationem ipsam minus, nisi omnis porro provident quisquam quod voluptatum? Aspernatur assumenda atque beatae consequuntur cum cupiditate, dignissimos dolor doloribus dolorum eaque enim est facilis ipsam ipsum iusto labore maxime minima molestiae nam necessitatibus nihil nobis non nostrum odit officia praesentium quis quod repellendus reprehenderit repudiandae sequi tempora unde vel vero voluptatem voluptates voluptatibus! Consequuntur, cum cumque delectus dolores eos minima nihil! Aliquid, exercitationem.</p>
 
-
+<input type="text">
+<input type="password">
+    <input type="email">
 
 
 @endsection
