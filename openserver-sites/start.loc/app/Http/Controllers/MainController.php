dfcbae5f91aafd9b49cbc83e3042836858fc0063
UpdatedUserName <rodnovspb@mail.ru>
2023-02-04 01:48:20 +0300

    04.02.2023

--- openserver-sites/start.loc/app/Http/Controllers/MainController.php
+++ openserver-sites/start.loc/app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Events\GoodDeleted;
+use App\Http\Resources\UserResource;
 use App\Models\Good;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
@@ -17,15 +18,20 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Hash;
 
+use function MongoDB\BSON\toJSON;
+
 
 class MainController extends Controller
 {
     public function index(Request $request)
     {
+        $user = User::find(41);
 
-        $user = User::find(41)->toJson();
 
-        dump($user);
+        dd($user);
+
+
+
 
 
 
