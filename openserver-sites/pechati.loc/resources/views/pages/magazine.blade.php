16b22b264db7590d59c0407d607647b2c6945ec9
UpdatedUserName <rodnovspb@mail.ru>
2023-02-19 09:33:31 +0300

    18.02.2023

--- openserver-sites/pechati.loc/resources/views/pages/magazine.blade.php
+++ openserver-sites/pechati.loc/resources/views/pages/magazine.blade.php
@@ -5,20 +5,93 @@
 @endsection
 
 @section('content')
-    <main class="main main-page">
-      <div class="container">
-        <section>
-            {!! $data->content !!}
-            {!! $options['order_methods'] !!}
+<main class="main main-page">
+    <div class="container">
+    {!! $data->content !!}
+    {!! $options['order_methods'] !!}
+
+        <form action="#" method="post" enctype="multipart/form-data">
+        @csrf
+        <section class="templates">
             <h3>{!! $options['step_1'] ?? null !!}</h3>
-            <div style="text-align: center;">Блок с печатями</div>
+            <div class="templates__list">
+                <a href="#">Стандартные (22 шт.)</a>
+                <a href="#">Дизайнерские (15 шт.)</a>
+                <a href="#">С лого и цветные (9 шт.)</a>
+                <a href="#">Все (52 шт.)</a>
+            </div>
+
+            <div class="templates__images">
+                <button class="arrow left-arrow" type="button">❮</button>
+                <button class="arrow right-arrow" type="button">❯</button>
+                @for($i=1; $i<=6; $i++)
+                    <div class="templates__item">
+                        <div class="templates__number">{{ $i }}</div>
+                        <label for="templates_radio_{{ $i }}">
+                            <img class="templates__img" src="https://via.placeholder.com/300x300/33CCFF/FFF/" alt="" style="border-radius: 50%">
+                        </label>
+                        <input id="templates_radio_{{ $i }}" type="radio" name="template" value="{{ $i }}">
+                        <div class="templates__price">300 р</div>
+                    </div>
+                @endfor
+            </div>
+        </section>
+
+        <section class="cases">
             <h3>{!! $options['step_2'] ?? null !!}</h3>
-            <div style="text-align: center;">Блок с оснастками</div>
+            <div class="cases__list">
+                <a href="#">Часто выбирают</a>
+                <a href="#">Автоматические</a>
+                <a href="#">Карманные</a>
+                <a href="#">Металлические</a>
+            </div>
+
+            <div class="cases__images">
+                <button class="arrow left-arrow" type="button">❮</button>
+                <button class="arrow right-arrow" type="button">❯</button>
+                @for($i=1; $i<=6; $i++)
+                    <div class="cases__item">
+                        <label for="cases_radio_{{ $i }}">
+                            <img class="cases__img" src="https://i.ibb.co/hK3WwWJ/shiny.png" alt="">
+                        </label>
+                        <input id="cases_radio_{{ $i }}" type="radio" name="case" value="{{ $i }}">
+                        <div class="cases__price">300 р</div>
+                        <div class="cases__name">Shiny R542 <a href="#"><img src="/storage/images/youtube.png" alt=""></a></div>
+                    </div>
+                @endfor
+            </div>
+        </section>
+
+        <section class="requisites">
             <h3>{!! $options['step_3'] ?? null !!}</h3>
             <div style="text-align: center;">Блок с реквизитами</div>
+        </section>
+
+      <section class="delivery">
             <h3>{!! $options['step_4'] ?? null !!}</h3>
             <div style="text-align: center;">Блок доставки</div>
-        </section>
-      </div>
-    </main>
+      </section>
+
+      <section class="order-btn">
+            <div class="dfc">
+                <h3>Общая стоимость</h3>
+                <button type="submit">Заказать</button>
+                <button>Добавить в корзину</button>
+            </div>
+      </section>
+
+      <section class="other-goods">
+            <h3>Вас также могут заинтересовать</h3>
+      </section>
+
+      </form>
+
+
+
+
+
+
+
+  </div>
+</main>
 @endsection
--- openserver-sites/pechati.loc/resources/views/parts/sticky.blade.php
+++ openserver-sites/pechati.loc/resources/views/parts/sticky.blade.php
@@ -1,30 +1,26 @@
 <div class="sticky">
         <div class="sticky__logo">
-            <a href="{{ route('index') }}" class="sticky__img">
-                {!! $options['logo'] ?? null !!}
-            </a>
+            <div class="sticky__wrapper">
+                <a href="{{ route('index') }}" class="sticky__img">{!! $options['logo'] ?? null !!}</a>
+            </div>
         </div>
         <div class="sticky__place">
-          <div class="sticky__time">{!! $options['shedule'] ?? null !!}</div>
-          <div class="sticky__address">{!! $options['address'] ?? null !!}</div>
+            <div class="sticky__wrapper wrapper-place">
+                <div class="sticky__time">{!! $options['shedule'] ?? null !!}</div>
+                <div class="sticky__address">{!! $options['address'] ?? null !!}</div>
+            </div>
         </div>
         <div class="sticky__phone">
-          <div class="sticky__messengers">
-            <div class="sticky__whatsapp dfc">
-              {!! $options['whatsapp'] ?? null !!}
+            <div class="sticky__wrapper">
+                <div class="sticky__messengers">
+                    <div class="sticky__whatsapp dfc">{!! $options['whatsapp'] ?? null !!}</div>
+                    <div class="sticky__viber dfc">{!! $options['viber'] ?? null !!}</div>
+                    <div class="sticky__telegram dfc">{!! $options['telegram'] ?? null !!}</div>
+                </div>
+                <div class="sticky__number">{!! $options['phone_1'] ?? null !!}</div>
             </div>
-            <div class="sticky__viber dfc">
-              {!! $options['viber'] ?? null !!}
-            </div>
-            <div class="sticky__telegram dfc">
-              {!! $options['telegram'] ?? null !!}
-            </div>
-          </div>
-          <div class="sticky__number">
-            {!! $options['phone_1'] ?? null !!}
-          </div>
         </div>
         <div class="sticky__email">
-          {!! $options['email'] ?? null !!}
+            <div class="sticky__wrapper">{!! $options['email'] ?? null !!}</div>
         </div>
     </div>
