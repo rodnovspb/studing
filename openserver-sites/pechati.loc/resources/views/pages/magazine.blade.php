76051889723d8a58cc8e02e3a98240ed41867cb7
UpdatedUserName <rodnovspb@mail.ru>
2023-02-19 10:57:42 +0300

    18.02.2023

--- openserver-sites/pechati.loc/resources/views/pages/magazine.blade.php
+++ openserver-sites/pechati.loc/resources/views/pages/magazine.blade.php
@@ -51,12 +51,21 @@
                 <button class="arrow right-arrow" type="button">❯</button>
                 @for($i=1; $i<=6; $i++)
                     <div class="cases__item">
-                        <label for="cases_radio_{{ $i }}">
-                            <img class="cases__img" src="https://i.ibb.co/hK3WwWJ/shiny.png" alt="">
-                        </label>
+                        <div class="cases__top">
+                            <label for="cases_radio_{{ $i }}">
+                                <img class="cases__img" src="https://i.ibb.co/hK3WwWJ/shiny.png" alt="">
+                            </label>
                         <input id="cases_radio_{{ $i }}" type="radio" name="case" value="{{ $i }}">
                         <div class="cases__price">300 р</div>
-                        <div class="cases__name">Shiny R542 <a href="#"><img src="/storage/images/youtube.png" alt=""></a></div>
+                        </div>
+                        <div class="cases__bottom">
+                            <div class="cases__name">
+                                <div class="cases__title">Shiny-542</div>
+                                <div class="cases__video">
+                                    <a href="#"><img class="cases__youtube" src="/storage/images/youtube.png" alt=""></a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 @endfor
             </div>
