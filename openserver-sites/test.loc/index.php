5cd41aa53e5719e9a3b9d8e6186063c85663dd23
UpdatedUserName <rodnovspb@mail.ru>
2023-03-24 15:10:58 +0300

    24.02.2023

--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -3,22 +3,18 @@ require_once 'show.php';
 
 
 
-$str = '<h2>111</h2>
-<p>
-	---
-</p>
+$str = 'text
+<footer id="footer" class="footer">
+	<p>
+		text
+	</p>
+	<p>
+		text
+	</p>
+</footer>
+text';
 
-<h2>222</h2>
-<p>
-	---
-</p>
-
-<h2 class="last">333</h2>
-<p>
-	---
-</p>';
-
-preg_match_all('#<h2[^>]*(.+?)</h2>#', $str, $match, PREG_PATTERN_ORDER);
+preg_match_all('#<footer[^>]*>(.+?)</footer>#su', $str, $match, PREG_PATTERN_ORDER);
 
 show($match[0], 1);
 
