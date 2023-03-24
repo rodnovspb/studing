b3263364e8c2ab2585690b729ba495b2c570923e
UpdatedUserName <rodnovspb@mail.ru>
2023-03-24 17:31:41 +0300

    24.02.2023

--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -5,6 +5,8 @@ require_once 'show.php';
 
 $str = file_get_contents('http://targ.loc/');
 
-preg_match_all('#<link[^>]+href\s*=\s*(["\'])(.+?)\1>#', $str, $match, PREG_PATTERN_ORDER);
+preg_match('#<body[^>]*>(.+?)</body>#su', $str, $match);
 
-show($match[2], 1);
+preg_match_all('#href\s*=\s*(["\'])(.+?)\1#', $match[1], $match1);
+
+show($match1[2], 1);
