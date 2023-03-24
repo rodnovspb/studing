febe648933affca133aaccb03dec25ec6283cf04
UpdatedUserName <rodnovspb@mail.ru>
2023-03-24 15:46:44 +0300

    24.02.2023

--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -5,12 +5,6 @@ require_once 'show.php';
 
 $str = file_get_contents('http://targ.loc/');
 
-preg_match_all('#<aside[^>]*>(.+?)</aside>#su', $str, $match, PREG_PATTERN_ORDER);
+preg_match_all('#href\s*=\s*(["\'])(.+?)\1#su', $str, $match, PREG_PATTERN_ORDER);
 
-
-
-preg_match_all('#<h2[^>]*>(.+?)</h2>#su', $match[0][0], $match1, PREG_PATTERN_ORDER);
-
-show($match1[1], 1);
-
-
+show($match[2], 1);
