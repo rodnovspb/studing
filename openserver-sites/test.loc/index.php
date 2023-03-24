49ce6661323ea3d7b047569dfea945d980882665
UpdatedUserName <rodnovspb@mail.ru>
2023-03-24 17:13:56 +0300

    24.02.2023

--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -9,6 +9,6 @@ preg_match_all('#<main[^>]*>(.+?)</main>#su', $str, $match, PREG_PATTERN_ORDER);
 
 
 
-preg_match_all('#src\s*=\s*(["\'])(.+?)\1#su', $match[1][0], $match1, PREG_PATTERN_ORDER);
+preg_match_all('#alt\s*=\s*(["\'])(.+?)\1#su', $match[1][0], $match1, PREG_PATTERN_ORDER);
 
 show($match1[2], 1);
