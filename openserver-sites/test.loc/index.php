343273e7798d42d557e27b0812bb42bcb5ad7a09
UpdatedUserName <rodnovspb@mail.ru>
2023-03-24 16:56:04 +0300

    24.02.2023

--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -5,17 +5,12 @@ require_once 'show.php';
 
 $str = file_get_contents('http://targ.loc/');
 
-preg_match_all('#<h2[^>]*>(.+?)</h2>#', $str, $match, PREG_PATTERN_ORDER);
+preg_match_all('#<nav[^>]*>(.+?)</nav>#su', $str, $match, PREG_PATTERN_ORDER);
 
-$arr = [];
+$str1 = $match[1][0];
 
-foreach ($match[1] as $item){
-    $item = trim($item);
-    $item = strip_tags($item);
-    $item = preg_replace('#\t+#', ' ', $item);
-    $arr[] = $item;
-}
+preg_match_all('#href\s*=\s*(["\'])(.+?)\1#su', $str1, $match1, PREG_PATTERN_ORDER);
 
-show($arr, 1);
+show($match1[2], 1);
 
 
