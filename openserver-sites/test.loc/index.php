061cf96be78735f53de938193fa6e9e8d8d5d92a
UpdatedUserName <rodnovspb@mail.ru>
2023-03-24 17:24:13 +0300

    24.02.2023

--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -5,10 +5,6 @@ require_once 'show.php';
 
 $str = file_get_contents('http://targ.loc/');
 
-preg_match_all('#<main[^>]*>(.+?)</main>#su', $str, $match, PREG_PATTERN_ORDER);
+preg_match_all('#<link[^>]+href\s*=\s*(["\'])(.+?)\1>#', $str, $match, PREG_PATTERN_ORDER);
 
-
-
-preg_match_all('#alt\s*=\s*(["\'])(.+?)\1#su', $match[1][0], $match1, PREG_PATTERN_ORDER);
-
-show($match1[2], 1);
+show($match[2], 1);
