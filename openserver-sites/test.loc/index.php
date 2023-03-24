4329e143eafa74b47eb94a3d60f34e9a062f5f33
UpdatedUserName <rodnovspb@mail.ru>
2023-03-24 16:36:08 +0300

    24.02.2023

--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -5,10 +5,17 @@ require_once 'show.php';
 
 $str = file_get_contents('http://targ.loc/');
 
-$str = preg_replace('#<!--.*?-->#su', '', $str);
+preg_match_all('#<h2[^>]*>(.+?)</h2>#', $str, $match, PREG_PATTERN_ORDER);
 
+$arr = [];
 
+foreach ($match[1] as $item){
+    $item = trim($item);
+    $item = strip_tags($item);
+    $item = preg_replace('#\t+#', ' ', $item);
+    $arr[] = $item;
+}
 
-show($str, 1);
+show($arr, 1);
 
 
