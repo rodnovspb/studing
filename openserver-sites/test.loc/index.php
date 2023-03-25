e7b0b27954b1dfedf545c7e948a73188003e1e36
UpdatedUserName <rodnovspb@mail.ru>
2023-03-25 18:14:25 +0300

    25.02.2023

--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -4,13 +4,19 @@ require_once 'show.php';
 require_once './vendor/autoload.php';
 use DiDom\Document;
 
-$url = 'http://targ.loc/';
+$startMemory = memory_get_usage();
+
+show(microtime(true), 1);
+
+$url = 'https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0';
 
 $document = new Document($url, true);
 
 $arr = [];
 foreach ($document->find('a') as $item) {
-    $arr[] = ['href' => $item->href, 'text' => $item->text()];
+    $arr[] =  $item->text();
 }
 
-show($arr, 1);
+$startMemory = memory_get_usage();
+
+show($startMemory, 1);
