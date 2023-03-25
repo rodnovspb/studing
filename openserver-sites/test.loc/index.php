986eb99db0aab7b7f010fadc4355acf5e91bb6fe
UpdatedUserName <rodnovspb@mail.ru>
2023-03-25 19:42:44 +0300

    25.02.2023

--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -3,20 +3,15 @@ require_once 'show.php';
 
 require_once './vendor/autoload.php';
 use DiDom\Document;
+use function Symfony\Component\String\s;
 
-$startMemory = memory_get_usage();
 
-show(microtime(true), 1);
-
-$url = 'https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0';
+$url = 'http://targ.loc/';
 
 $document = new Document($url, true);
 
-$arr = [];
-foreach ($document->find('a') as $item) {
-    $arr[] =  $item->text();
+foreach ($document->find('nav a') as $item){
+    echo $item->href . '<br>';
 }
 
-$startMemory = memory_get_usage();
 
-show($startMemory, 1);
