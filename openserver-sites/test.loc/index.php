0a924002c2ce68876d77cd9dfee640acadc28d26
UpdatedUserName <rodnovspb@mail.ru>
2023-03-26 20:26:16 +0300

    26.03.2023

--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -6,50 +6,22 @@ use DiDom\Document;
 
 
 
-$domain = 'http://targ.loc/cat/';
-$domainS = 'https://targ.loc';
-
 $document = new Document('http://targ.loc', true);
 
-$links = $document->find('a');
+$charset = $document->first('meta')->charset ?? $document->first('meta')->content;
 
-$arr = [];
+if($charset = $document->first('meta')->charset){
+    echo $charset;
+} elseif ($charset = $document->first('meta')->content){
 
-foreach ($links as $link){
-    if(str_starts_with($link->href, 'http://targ.loc') || str_starts_with($link->href, $domainS) || !preg_match('#^http#', $link->href)){
-        $arr[] = normalize($domain, $link->href);
-    }
+    preg_match('#charset\s*=\s*([\w-]+)\s*#', $charset, $match);
+    echo $match[1];
 }
 
-show($arr, 1);
 
-function normalize($target, $path){
-    preg_match('#(http://[^/]+)/?#', $target, $match);
-    $domain = $match[1];
 
-    if(str_starts_with($path, 'http')){
-        return $path;
-    } elseif (str_starts_with($path, '../')){
-        preg_match_all('#\.\.\/#', $path, $match);
-        $count = count($match[0]);
-        for ($i = 0; $i < $count; $i++){
-            $path = preg_replace('#^\.\.\/#', '', $path);
-            $target = preg_replace('#[^\/]+\/$#', '', $target);
-        }
-        $res = $target . $path;
-        return $res;
-    } elseif ($path === '/'){
-        return $domain . '/';
-    } elseif (preg_match('#^/#', $path)){
-        $res = $domain . $path;
-        return $res;
-    } elseif (preg_match('#^(\w+|\.\/{1})#', $path)){
-        $path = preg_replace('#^\.\/#', '',$path);
-        $res = $target . $path;
-        return $res;
-    }
 
-}
+
 
 
 
