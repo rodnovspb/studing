95e7477430fac3f2367c57ab1756c2bcc447d1a6
UpdatedUserName <rodnovspb@mail.ru>
2023-03-26 17:12:04 +0300

    25.02.2023

--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -5,32 +5,23 @@ require_once './vendor/autoload.php';
 use DiDom\Document;
 use function Symfony\Component\String\s;
 
-$domain = 'http://targ.loc';
 
-$url = 'http://targ.loc/';
+$url = 'http://targ.loc';
 
 
 $document = new Document($url, true);
 
+$url = 'http://targ.loc/cat/sat/';
 
-$items = $document->find('[href]');
-$items1 = $document->find('[src]');
-
-foreach ($items as $item){
-    if(str_starts_with($item->href, 'http')){
-        echo $item->href . "<br>";
-    } else {
-        echo $domain . $item->href . "<br>";
-    }
+foreach ($document->find('a') as $item){
+    echo $url . $item->href . "<br>";
 }
 
-foreach ($items1 as $item){
-    if(str_starts_with($item->src, 'http')){
-        echo $item->src . "<br>";
-    } else {
-        echo $domain . $item->src . "<br>";
-    }
-}
+
+
+
+
+
 
 
 
