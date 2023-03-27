7917b5fcf088e6edd5ba65bf2d07755b0ae78631
UpdatedUserName <rodnovspb@mail.ru>
2023-03-27 22:57:13 +0300

    27.03.2023

--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -19,44 +19,53 @@ mysqli_query($link, "SET NAMES 'utf8'");
 
 
 
+$paths = [
+    'https://pechat74.ru',
+];
 
-$url  = 'http://targ.loc';
+$i = 0;
 
-$document = new Document($url, true);
-
-$items = $document->find('nav a');
-
-
-foreach ($items as $item) {
-    $page = new Document($url . $item->href, true);
-    $links = $page->find('nav a');
-        foreach ($links as $link1){
-            $page = new Document($url . $link1->href, true);
-            $title =  $page->first('title')->text();
-            $text =  $page->first('p')->text();
-            $query = "INSERT INTO pages SET title='$title', text='$text'";
-            mysqli_query($link, $query) or die(mysqli_error($link));
+while ($i < count($paths)){
+    $path = $paths[$i];
+    $page = new Document($path, true);
+    $text = $page->first('span')->text();
+    $hrefs = $page->find('a');
+    foreach ($hrefs as $href){
+        $href = normalize($paths[0], $paths[$i], $href->href);
+        if(!in_array($href, $paths) && $href != null){
+            $paths[] = $href;
         }
+    }
+    echo $i . ' '. $text . "<br>";
+    $i++;
+
+    if($i === 100){
+        break;
+    }
+
+    var_dump($paths);
 }
 
 
 
+//foreach ($items as $item) {
+//    $page = new Document($url . $item->href, true);
+//    $links = $page->find('nav a');
+//        foreach ($links as $link1){
+//            $page = new Document($url . $link1->href, true);
+//            $title =  $page->first('title')->text();
+//            $text =  $page->first('p')->text();
+//            $query = "INSERT INTO pages SET title='$title', text='$text'";
+//            mysqli_query($link, $query) or die(mysqli_error($link));
+//        }
+//}
 
 
 
 
 
 
-
-
-
-
-
-
-
-function normalize($target, $path){
-    preg_match('#(https://[^/]+)/?#', $target, $match);
-    $domain = $match[1];
+function normalize($domain, $target, $path){
 
     if(str_starts_with($path, 'http')){
         return $path;
@@ -67,20 +76,50 @@ function normalize($target, $path){
             $path = preg_replace('#^\.\.\/#', '', $path);
             $target = preg_replace('#[^\/]+\/$#', '', $target);
         }
-        $res = $target . $path;
-        return $res;
+        return $target . $path;
     } elseif ($path === '/'){
         return $domain . '/';
     } elseif (preg_match('#^/#', $path)){
-        $res = $domain . $path;
-        return $res;
+        return $domain . $path;
     } elseif (preg_match('#^(\w+|\.\/{1})#', $path)){
-        $path = preg_replace('#^\.\/#', '',$path);
-        $res = $target . $path;
-        return $res;
+        $path = preg_replace('#^\.\/#', '', $path);
+        return $target . $path;
     }
 
 }
+
+
+
+
+
+
+//function normalize($target, $path){
+//    preg_match('#(http://[^/]+)/?#', $target, $match);
+//    $domain = $match[1];
+//
+//    if(str_starts_with($path, 'http')){
+//        return $path;
+//    } elseif (str_starts_with($path, '../')){
+//        preg_match_all('#\.\.\/#', $path, $match);
+//        $count = count($match[0]);
+//        for ($i = 0; $i < $count; $i++){
+//            $path = preg_replace('#^\.\.\/#', '', $path);
+//            $target = preg_replace('#[^\/]+\/$#', '', $target);
+//        }
+//        $res = $target . $path;
+//        return $res;
+//    } elseif ($path === '/'){
+//        return $domain . '/';
+//    } elseif (preg_match('#^/#', $path)){
+//        $res = $domain . $path;
+//        return $res;
+//    } elseif (preg_match('#^(\w+|\.\/{1})#', $path)){
+//        $path = preg_replace('#^\.\/#', '',$path);
+//        $res = $target . $path;
+//        return $res;
+//    }
+//
+//}
 
 
 
