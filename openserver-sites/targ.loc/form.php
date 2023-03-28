6ecbe356d3024c71bcdc21dbd082617c33b3dbbd
UpdatedUserName <rodnovspb@mail.ru>
2023-03-29 00:09:01 +0300

    27.03.2023

--- openserver-sites/targ.loc/form.php
+++ openserver-sites/targ.loc/form.php
@@ -8,11 +8,13 @@
 		<form action="" method="POST">
 			<input name="num1">
 			<input name="num2">
-			<input type="submit" value="submit" name="submit">
+			<input type="hidden" name="eee" value="1">
+			<input type="submit">
 		</form>
+		
 		<?php
 			if (isset($_POST['num1']) and isset($_POST['num2'])) {
-				if (isset($_POST['submit'])) {
+				if (isset($_POST['eee']) and $_POST['eee'] == '1') {
 					echo '<p id="res">' . 
 						$_POST['num1'] * $_POST['num2'] 
 					. '</p>';
--- openserver-sites/test.loc/index.php
+++ openserver-sites/test.loc/index.php
@@ -22,9 +22,9 @@ $client = new Client();
 try {
     $response = $client->request('POST', $url, [
         'form_params' => [
-            'num1' => 11,
-            'num2' => 22,
-            'submit' => 'submit',
+            'num1' => 1212,
+            'num2' => 121212,
+            'eee' => '1',
         ]
     ])->getBody()->getContents();
 
