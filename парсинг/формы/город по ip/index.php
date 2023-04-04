6778ed74bd1d1419c1b566283620aaff627cfd9d
UpdatedUserName <rodnovspb@mail.ru>
2023-04-04 04:17:25 +0300

    04.04.23

--- парсинг/формы/город по ip/index.php
+++ парсинг/формы/город по ip/index.php
@@ -9,20 +9,13 @@ $token = "3a3164b3b82f2d86aa1ff7625eef3eeb3ba4202e";
 $secret = "8eefefbeddf3678c71d5d9d4749d617510d4cb32";
 $dadata = new \Dadata\DadataClient($token, $secret);
 
-$result = $dadata->iplocate("46.226.227.20");
+$result = $dadata->iplocate("176.59.18.255")['value'];
 
-$ip = $_SERVER['REMOTE_ADDR'];
+show($result, 1);
 
 ?>
 
-<script>
-    function getIPFromAmazon() {
-		fetch("https://checkip.amazonaws.com/", { mode: "cors" }).then(res => res.text()).then(data => console.log(data))
-	}
 
-	getIPFromAmazon()
- 
-</script>
 
 
 
