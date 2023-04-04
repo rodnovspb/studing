1fc6d2bab89e24c38e54b178b3814d5c51ae5313
UpdatedUserName <rodnovspb@mail.ru>
2023-04-04 03:47:32 +0300

    04.04.23

--- парсинг/формы/город по ip/index.php
+++ парсинг/формы/город по ip/index.php
@@ -11,46 +11,20 @@ $dadata = new \Dadata\DadataClient($token, $secret);
 
 $result = $dadata->iplocate("46.226.227.20");
 
+$ip = $_SERVER['REMOTE_ADDR'];
+
+?>
+
+<script>
+    function getIPFromAmazon() {
+		fetch("https://checkip.amazonaws.com/", { mode: "cors" }).then(res => res.text()).then(data => console.log(data))
+	}
+
+	getIPFromAmazon()
+ 
+</script>
 
 
 
-function getClientIp() {
-    $ip = '';
-    $ipAll = []; // networks IP
-    $ipSus = []; // suspected IP
-    $serverVariables = [
-        'HTTP_X_FORWARDED_FOR',
-        'HTTP_X_FORWARDED',
-        'HTTP_X_CLUSTER_CLIENT_IP',
-        'HTTP_X_COMING_FROM',
-        'HTTP_FORWARDED_FOR',
-        'HTTP_FORWARDED',
-        'HTTP_COMING_FROM',
-        'HTTP_CLIENT_IP',
-        'HTTP_FROM',
-        'HTTP_VIA',
-        'REMOTE_ADDR',
-    ];
-    foreach ($serverVariables as $serverVariable) {
-        $value = '';
-        if (isset($_SERVER[$serverVariable])) {
-            $value = $_SERVER[$serverVariable];
-        } elseif (getenv($serverVariable)) {
-            $value = getenv($serverVariable);
-        }
-        if (!empty($value)) {
-            $tmp = explode(',', $value);
-            $ipSus[] = $tmp[0];
-            $ipAll = array_merge($ipAll, $tmp);
-        }
-    }
-    $ipSus = array_unique($ipSus);
-    $ipAll = array_unique($ipAll);
-    $ip = (sizeof($ipSus) > 0) ? $ipSus[0] : $ip;
-    return [
-        'ip' => $ip,
-        'suspected' => $ipSus,
-        'network' => $ipAll,
-    ];
-}
+
 
