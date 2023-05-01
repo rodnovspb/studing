ab60bc4027594ad08c64496e6f08659acc54bec4
UpdatedUserName <rodnovspb@mail.ru>
2023-05-01 12:03:06 +0300

    01.05.2023

---
+++ openserver-sites/avito.loc/app/Http/Controllers/Cabinet/HomeController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers\Cabinet;
+
+class HomeController
+{
+    public function index(){
+        return 111;
+    }
+}
--- openserver-sites/rodnovspb.ru/resources/views/layouts/layout.blade.php
+++ openserver-sites/rodnovspb.ru/resources/views/layouts/layout.blade.php
@@ -12,27 +12,42 @@
 	        <header class="header">
 		        <div class="container">
                     <div class="header__block">
-                        <a href="{{ route('index') }}">
-                            <svg class="header__logo">
+                        <a class="header__logo" href="{{ route('index') }}">
+                            <svg>
                                 <use href="{{ asset('storage/images/sprite.svg#logo') }}"></use>
                             </svg>
                         </a>
-                        <nav class="header__menu menu">
-                            <a href="#" class="menu__link">Сайты</a>
-                            <a href="#" class="menu__link">Боты</a>
-                            <a href="#" class="menu__link">Парсинг</a>
-                            <a href="#" class="menu__link">Дополнительный функционал</a>
-                            <a href="#" class="menu__link">Контакты</a>
-                        </nav>
+{{--                        <nav class="header__menu menu">--}}
+{{--                            <a href="#" class="menu__link">Сайты</a>--}}
+{{--                            <a href="#" class="menu__link">Боты</a>--}}
+{{--                            <a href="#" class="menu__link">Парсинг</a>--}}
+{{--                        </nav>--}}
+                        <div class="header__contacts">
+                            <div class="header__phone">
+                                <div class="header__messengers">
+                                    <a class="header__whatsapp" href="https://wa.me/79507261797" target="_blank" title="whatsapp">
+                                        <svg>
+                                            <use href="{{ asset('storage/images/sprite.svg#whatsapp') }}"></use>
+                                        </svg>
+                                    </a>
+                                    <a class="header__telegram" href="https://telegram.me/cinedbs" target="_blank" title="telegram">
+                                        <svg>
+                                            <use href="{{ asset('storage/images/sprite.svg#telegram') }}"></use>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <a class="header__tel" href="tel:+79507261797">89507261797</a>
+                            </div>
+                            <a class="header__email" href="mailto:rodnovspb@mail.ru">rodnovspb@mail.ru</a>
+                        </div>
                     </div>
-
 
 		        </div>
 	        </header>
 	        <main class="main">
 	            <section class="content">
 		            <div class="container">
-
+                        <h1 class="content__title">Самое сложное&nbsp;&mdash; <span class="content__subtitle">сделать просто</span></h1>
 		            </div>
 	            </section>
 	        </main>
