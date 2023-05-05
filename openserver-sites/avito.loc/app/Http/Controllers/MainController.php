9a950d96d93049d859051474f880f19e47e53201
UpdatedUserName <rodnovspb@mail.ru>
2023-05-05 10:52:52 +0300

    05.05.2023

--- openserver-sites/avito.loc/app/Http/Controllers/MainController.php
+++ openserver-sites/avito.loc/app/Http/Controllers/MainController.php
@@ -9,6 +9,20 @@ class MainController extends Controller
 {
     public function index()
     {
+        try {
+
+            self::func();
+        } catch (\Exception $e){
+                echo 'LogicException';
+        } catch (\Exception $e){
+                echo 'Exception';
+        }
+
+
         return view('pages.index');
+    }
+
+    public static function func(){
+        throw new \LogicException('ошибка');
     }
 }
