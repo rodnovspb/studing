5c4fd5781668467cf768da415571fd567ffbc07d
UpdatedUserName <rodnovspb@mail.ru>
2023-05-10 21:10:24 +0300

    10.05.2023

--- openserver-sites/avito.loc/app/Http/Controllers/MainController.php
+++ openserver-sites/avito.loc/app/Http/Controllers/MainController.php
@@ -22,12 +22,23 @@ class MainController extends Controller
 
     public function index()
     {
-        $a = new SMSRU('F42F1F6B-E9DC-0497-BE58-CC17DD10FF0F');
+        $sms = new SMSRU('F42F1F6B-E9DC-0497-BE58-CC17DD10FF0F');
         $data = new stdClass();
 
-        $place = ['city' => 'Pokhara', 'country' =>'Nepal'];
-        $obj = (object)$place;
-        echo $obj->city;
+        $data->to = '79507261797';
+        $data->text = 'Привет';
+
+        $sms = $sms->send_one($data);
+
+        if ($sms->status == "OK") { // Запрос выполнен успешно
+            echo "Сообщение отправлено успешно. ";
+            echo "ID сообщения: $sms->sms_id. ";
+            echo "Ваш новый баланс: $sms->balance";
+        } else {
+            echo "Сообщение не отправлено. ";
+            echo "Код ошибки: $sms->status_code. ";
+            echo "Текст ошибки: $sms->status_text.";
+        }
     }
 
 
