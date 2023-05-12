d5527893a02b89fd0efa07190db4cfc27d45b576
UpdatedUserName <rodnovspb@mail.ru>
2023-05-12 16:20:06 +0300

    10.05.2023

--- openserver-sites/avito.loc/app/Http/Controllers/MainController.php
+++ openserver-sites/avito.loc/app/Http/Controllers/MainController.php
@@ -2,16 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
-use App\Models\User;
-use App\MyClasses\MyClass;
-use App\MyClasses\SmsClass;
-use App\MyClasses\SMSRU;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Str;
 
+
+use Illuminate\Http\Request;
 use stdClass;
 
 use function Termwind\render;
@@ -22,8 +15,11 @@ class MainController extends Controller
 
     public function index()
     {
-         $key = config('cache.default');
-         dd(config("cache.stores.$key"));
+        return view('pages.index');
+    }
+
+    public function give(Request $request){
+        return $request->all();
     }
 
 
--- openserver-sites/avito.loc/resources/views/pages/index.blade.php
+++ openserver-sites/avito.loc/resources/views/pages/index.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.layout')
 
 @section('content')
-
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script>
+        fetch('{{ route('give') }}', {
+            method: "POST",
+            headers: {
+                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'},
+            body: JSON.stringify({a: 1, b: 2})})
+        .then(res=>res.json())
+        .then(res=>console.log(res))
+        .catch(e=>console.log(e))
+    </script>
 
 
 @endsection
