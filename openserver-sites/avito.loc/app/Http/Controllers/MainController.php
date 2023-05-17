23bfbaedc09d75daf69e9ee6846247f95b929568
UpdatedUserName <rodnovspb@mail.ru>
2023-05-18 02:54:27 +0300

    16.05.2023

--- openserver-sites/avito.loc/app/Http/Controllers/MainController.php
+++ openserver-sites/avito.loc/app/Http/Controllers/MainController.php
@@ -7,17 +7,14 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\User;
 use Barryvdh\Debugbar\Facades\Debugbar;
-use Carbon\Carbon;
-use Illuminate\Http\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
 
-use Meilisearch\Client;
 
-use PDOStatement;
+use Memcache;
 
 use function Illuminate\Events\queueable;
 
@@ -27,12 +24,17 @@ class MainController extends Controller
 
     public function index()
     {
-        $arr = [1, null, 2, false, 3, 0, '', ' '];
-
-        var_dump(array_filter([1, null, 2, false, 3, 0, '', ' ']));
 
 
-//        return view('pages.index');
+
+
+        Cache::put('key', 1119);
+
+        Session::put('key', 2229);
+
+
+
+        return view('pages.index');
     }
 
 
