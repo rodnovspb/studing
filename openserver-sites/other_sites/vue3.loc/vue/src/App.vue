e90788dd292772e5d80d1136b30943d7a9ea2ac5
UpdatedUserName <rodnovspb@mail.ru>
2023-10-04 21:10:06 +0300

    04.10.2023

--- openserver-sites/other_sites/vue3.loc/vue/src/App.vue
+++ openserver-sites/other_sites/vue3.loc/vue/src/App.vue
@@ -2,21 +2,13 @@
   <div>
     <h2>Инпуты</h2>
 
-    <label>
-      <input v-model="social" type="radio" value="111"> 111
-    </label>
-
-    <label>
-      <input v-model="social" type="radio" value="222"> 222
-    </label>
-
-    <label>
-      <input v-model="social" type="radio" value="333"> 333
-    </label>
+    <select>
+      <option v-for="s in social" :selected="s === def">{{ s }}</option>
+    </select>
 
     <hr>
 
-    <p>{{ social }}</p>
+
   </div>
 </template>
 
@@ -25,7 +17,8 @@
 export default {
   data(){
     return {
-      social: '333'
+      social: ['111', '222', '333'],
+      def: '333'
     }
   }
 }
