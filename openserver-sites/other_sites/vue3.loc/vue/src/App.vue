4075a38018246d353d8b2ec4030e377295ebbc53
UpdatedUserName <rodnovspb@mail.ru>
2023-10-04 20:44:45 +0300

    04.10.2023

---
+++ openserver-sites/other_sites/vue3.loc/vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Инпуты</h2>
+    <input type="text" v-model.lazy="name">
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  data(){
+    return {
+      name: ''
+    }
+  }
+}
+
+</script>
+
+
+
+
