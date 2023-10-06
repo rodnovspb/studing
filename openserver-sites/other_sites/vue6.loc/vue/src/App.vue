40a79530a997f8d92b5944ee9536350406ee9674
UpdatedUserName <rodnovspb@mail.ru>
2023-10-06 21:28:03 +0300

    06.10.2023

---
+++ openserver-sites/other_sites/vue6.loc/vue/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container pt-2">
+    <div class="form-group">
+      <label for="name">Имя</label>
+      <input type="text" id="name" class="form-control" v-model.trim="carName">
+    </div>
+
+    <div class="form-group">
+      <label for="year">Имя</label>
+      <input type="text" id="year" class="form-control" v-model.number="carYear">
+    </div>
+
+    <button class="btn btn-success m-3" @click="createCar">Создать</button>
+    <button class="btn btn-primary m-3" @click="loadCars">Загрузить</button>
+
+    <hr>
+
+    <ul class="list-group">
+      <li class="list-group-item" v-for="car in cars" :key="car.id">
+        <strong>{{ car.name }} - {{ car.year }}</strong>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data(){
+    return {
+      carName: '',
+      carYear: 2018,
+      cars: []
+    }
+  },
+  methods: {
+    createCar(){
+      const car = {
+        name: this.carName,
+        year: this.carYear
+      }
+      this.$http.post('http://localhost:3000/cars', car)
+          .then(res => res.json())
+          .then(res => console.log(res))
+    },
+    loadCars(){
+      this.$http.get('http://localhost:3000/cars')
+          .then(res => res.json())
+          .then(res => this.cars = res)
+    }
+  }
+}
+
+
+
+</script>
+
+
