e73219b20f883a47600317684247aa83917f31db
UpdatedUserName <rodnovspb@mail.ru>
2023-10-07 21:37:09 +0300

    07.10.2023

---
+++ openserver-sites/other_sites/vue8.loc/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>Количество лайков: {{ likes }}</div>
+    <div>Количество дизлайков: {{ dislikes }}</div>
+    <button @click="addLike">+</button>
+    <button @click="addDisLike">-</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        likes: 0,
+        dislikes: 0
+      }
+    },
+    methods: {
+      addLike(){
+        this.likes +=1
+      },
+      addDisLike(){
+        this.dislikes +=1
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
