24a3da64985016764d907dac45555694b1851389
UpdatedUserName <rodnovspb@mail.ru>
2023-10-09 20:16:46 +0300

    09.10.2023

--- openserver-sites/other_sites/vue8.loc/src/App.vue
+++ openserver-sites/other_sites/vue8.loc/src/App.vue
@@ -1,34 +1,51 @@
 <template>
   <div class="app">
-    <post-form @create="createPost"/>
+    <h1>Страница со статьями</h1>
+    <my-button @click="fetchPosts">Фетч</my-button>
+    <my-button @click="showDialog" style="margin: 20px 0">Создать пост</my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost"/>
+    </my-dialog>
     <post-list :posts="posts" @remove="removePost"/>
-
   </div>
 </template>
 
 <script>
   import PostForm from "@/components/PostForm.vue";
   import PostList from "@/components/PostList.vue";
+  import MyDialog from "@/components/UI/MyDialog.vue";
+  import MyButton from "@/components/UI/MyButton.vue";
+  import axios from "axios";
 
   export default {
     data(){
       return {
-        posts: [
-          { id: 1, title: 'Название 1', body: 'Lorem ipsum dolor sit amet.'},
-          { id: 2, title: 'Название 2', body: 'Lorem ipsum dolor sit amet.'},
-          { id: 3, title: 'Название 3', body: 'Lorem ipsum dolor sit amet.'},
-        ],
+        posts: [],
+        dialogVisible: false,
       }
     },
     components: {
-      PostForm, PostList
+      MyButton,
+      MyDialog, PostForm, PostList
     },
     methods: {
       createPost(post){
        this.posts.push(post)
+        this.dialogVisible = false
       },
       removePost(post){
         this.posts = this.posts.filter(p => p.id !== post.id)
+      },
+      showDialog(){
+        this.dialogVisible = true
+      },
+      async fetchPosts(){
+        try {
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+          this.posts = response.data
+        } catch (e){
+          console.log('ошибка')
+        }
       }
     }
   }
---
+++ openserver-sites/other_sites/vue8.loc/src/components/UI/MyDialog.vue
@@ -0,0 +1,43 @@
+<template>
+ <div class="dialog" v-if="show" @click="hideDialog">
+    <div class="dialog__content" @click.stop>
+      <slot></slot>
+    </div>
+ </div>
+</template>
+
+<script>
+ export default {
+   props: {
+     show: {
+       type: Boolean,
+       default: false
+     }
+   },
+   methods: {
+     hideDialog(){
+       this.$emit('update:show', false)
+     }
+   }
+ };
+</script>
+
+<style scoped>
+  .dialog {
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    background: rgba(0,0,0, 0.5);
+    position: fixed;
+    display: flex;
+  }
+  .dialog__content {
+    margin: auto;
+    background: aliceblue;
+    border-radius: 12px;
+    min-height: 50px;
+    min-width: 300px;
+    padding: 20px;
+  }
+</style>
