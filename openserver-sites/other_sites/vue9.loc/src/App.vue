b9b03a11ef0cdf40aa2ded7bf71b3e8d64b99726
UpdatedUserName <rodnovspb@mail.ru>
2023-10-13 02:36:49 +0300

    11.10.2023

---
+++ openserver-sites/other_sites/vue9.loc/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+
+<select v-model="elem">
+  <option disabled value="">Выберите</option>
+  <option v-for="item in options" :value="item.text">{{ item.value }}</option>
+</select>
+
+  {{ elem }}
+
+</template>
+
+<script>
+
+
+  export default {
+    data(){
+      return {
+        elem: '',
+        options: [
+          { text: 'Один', value: 'A' },
+          { text: 'Два', value: 'B' },
+          { text: 'Три', value: 'C' }
+        ]
+      }
+    },
+    methods: {
+
+    }
+
+  }
+
+</script>
+
+<style>
+
+</style>
+<script setup>
+</script>
