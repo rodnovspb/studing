dcb159424ef6f17a86bda0444873cf77e97eb8cf
UpdatedUserName <rodnovspb@mail.ru>
2023-10-15 04:02:24 +0300

    14.10.2023

--- openserver-sites/other_sites/vue9.loc/src/App.vue
+++ openserver-sites/other_sites/vue9.loc/src/App.vue
@@ -1,9 +1,12 @@
 <template>
 
   <div>
-    <div id="div" :class="{'shake': flag}" style="width: 180px; height: 180px; border: 2px #00d084 solid; background-color: #00d084"></div>
-
     <button @click="flag = !flag">Кнопка</button>
+    <transition name="fade">
+      <div v-if="flag" id="div" style="width: 180px; height: 180px; border: 2px #00d084 solid; background-color: #00d084"></div>
+    </transition>
+
+
 
 
   </div>
@@ -34,43 +37,14 @@
 </script>
 
 <style>
-.shake {
-  animation: bounceInDown 1s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
-  transform: translate3d(0, 0, 0);
-  backface-visibility: hidden;
-  perspective: 1000px;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 5s ease;
 }
 
-@keyframes bounceInDown {
-  from,
-  60%,
-  75%,
-  90%,
-  to {
-    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
-  }
-
-  0% {
-    opacity: 0;
-    transform: translate3d(0, -3000px, 0) scaleY(3);
-  }
-
-  60% {
-    opacity: 1;
-    transform: translate3d(0, 25px, 0) scaleY(0.9);
-  }
-
-  75% {
-    transform: translate3d(0, -10px, 0) scaleY(0.95);
-  }
-
-  90% {
-    transform: translate3d(0, 5px, 0) scaleY(0.985);
-  }
-
-  to {
-    transform: translate3d(0, 0, 0);
-  }
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 
 </style>
