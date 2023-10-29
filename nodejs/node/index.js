dad367e1c4c152baed70d487846ccb8964ac68e0
UpdatedUserName <rodnovspb@mail.ru>
2023-10-29 14:04:10 +0300

    29.10.2023

--- nodejs/node/index.js
+++ nodejs/node/index.js
@@ -1,11 +1,12 @@
 let http = require('http')
+let url = require('url')
 
 let server = new http.Server
 
 server.listen(80, '127.0.0.1')
 
-let counter = 0
-
 server.on('request', function (req, res){
-	res.end('counter: ' + counter++)
+	let parsedUrl = url.parse(req.url, true)
+	console.log(parsedUrl)
+	res.end(parsedUrl.query.q)
 })
