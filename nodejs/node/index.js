9c1229997ae207600b92b3d47a62d0ecf0f63c1d
UpdatedUserName <rodnovspb@mail.ru>
2023-10-29 14:17:31 +0300

    29.10.2023

--- nodejs/node/index.js
+++ nodejs/node/index.js
@@ -1,5 +1,6 @@
 let http = require('http')
 let url = require('url')
+let fs = require('fs')
 
 let server = new http.Server
 
@@ -7,6 +8,21 @@ server.listen(80, '127.0.0.1')
 
 server.on('request', function (req, res){
 	let parsedUrl = url.parse(req.url, true)
-	console.log(parsedUrl)
-	res.end(parsedUrl.query.q)
+	fs.readFile(getPageNameByPath(parsedUrl.pathname) + '.html', function (err, data){
+		if(err) throw new Error(err);
+		res.end(data)
+	})
 })
+
+function getPageNameByPath(path){
+	switch (path){
+		case '/':
+			return 'index';
+		case '/about':
+			return 'about';
+		default:
+			return '404';
+
+
+	}
+}
