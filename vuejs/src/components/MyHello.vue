d29e3133affcd6b70b63e36c3ec2a602127897ca
UpdatedUserName <rodnovspb@mail.ru>
2024-10-11 02:35:40 +0300

    повтор вью

---
+++ vuejs/src/components/MyHello.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import {ref} from "vue";
+
+const count = ref(0)
+
+const increment = () => count.value++
+
+</script>
+
+
+
+
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="increment">+1</button>
+  <input @keyup.ctrl.enter = "increment">
+
+</template>
+
+
+
+
+<style scoped>
+
+</style>
