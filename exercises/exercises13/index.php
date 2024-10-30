3ec2782df44966bf7f108038f4cedc50ed2b9935
UpdatedUserName <rodnovspb@mail.ru>
2024-10-30 23:36:59 +0300

    повторение

--- exercises/exercises13/index.php
+++ exercises/exercises13/index.php
@@ -2,10 +2,14 @@
 
 require_once 'show.php';
 
-$arr1 = [1, 2, 3, 4, 5];
 
-$arr2 = &$arr1;
 
-$arr2[0] = 0;
 
-show($arr1);
+$arr = [1, 2, 3, 4, 5];
+
+foreach ($arr as &$elem) {
+  $elem = $elem ** 2;
+}
+
+
+show($arr);
