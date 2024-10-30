5acbc0b0d6e40148da9d586347e1077b034680ba
UpdatedUserName <rodnovspb@mail.ru>
2024-10-30 23:56:56 +0300

    повторение

--- exercises/exercises13/index.php
+++ exercises/exercises13/index.php
@@ -3,13 +3,13 @@
 require_once 'show.php';
 
 
-
-
 $arr = [1, 2, 3, 4, 5];
 
-foreach ($arr as &$elem) {
-  $elem = $elem ** 2;
+function func(&$arr)
+{
+  $arr[0] = '!';
 }
 
-
+func($arr);
 show($arr);
+
