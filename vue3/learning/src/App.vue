1d263840dfd755cf011bb2ad8d976fc7250a7864
rodnov <rodnovspb@mail.ru>
2025-01-20 23:06:57 +0300

    up

---
+++ vue3/learning/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import {ref} from "vue";
+
+  const className = ref('red')
+
+  const func = () => {
+    className.value === 'red' ? className.value = 'green' : className.value = 'red'
+  }
+
+</script>
+
+
+<template>
+<div :class="className">Lorem ipsum dolor sit amet.</div>
+
+<button @click="func">Изменить</button>
+
+
+</template>
+
+
+<style scoped>
+  .red {
+    color: red;
+  }
+
+  .green {
+    color: green;
+  }
+
+</style>
