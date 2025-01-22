eabbb6d08a0b3f3c08a49611ab091fbc5d175f3e
rodnov <rodnovspb@mail.ru>
2025-01-22 23:32:12 +0300

    up

--- vue3/learning/src/App.vue
+++ vue3/learning/src/App.vue
@@ -1,89 +1,30 @@
 <script setup>
+import { ref } from 'vue'
+import Modal from "@/components/Modal.vue";
 
-import { ref, reactive } from 'vue'
-import PolyGraph from "@/components/PolyGraph.vue";
-
-const stats = reactive([
-  { label: 'A', value: 100 },
-  { label: 'B', value: 100 },
-  { label: 'C', value: 100 },
-  { label: 'D', value: 100 },
-  { label: 'E', value: 100 },
-  { label: 'F', value: 100 }
-])
-
-const newLabel = ref('')
-
-function add(){
-  if(newLabel.value) {
-    stats.push({
-      label: newLabel.value,
-      value: 100
-    })
-    newLabel.value = ''
-  }
-}
-
-function remove(stat){
-  if(stats.length > 3){
-    stats.splice(stats.indexOf(stat), 1)
-  } else {
-    alert('Нельзя удалить')
-  }
-}
-
+const showModal = ref(false)
 
 </script>
 
 <template>
-  <svg width="200" height="200">
-    <PolyGraph :stats="stats" />
-  </svg>
+  <button @click="showModal = true">Показать модальное</button>
 
-
-  <form>
-    <input type="text" v-model="newLabel">
-    <button @click.prevent="add">Добавить</button>
-  </form>
-
-  <div v-for="(stat, index) in stats" :key="index">
-    <label>{{ stat.label }}</label>
-    <input type="range" v-model="stat.value" min="0" max="100">
-    <span>{{ stat.value }}</span>
-    <button @click="remove(stat)" style="margin-left: 10px;">х</button>
-  </div>
-
-  <pre id="raw">{{ stats }}</pre>
+  <Teleport to="body">
+    <Modal @close="showModal=false" :show="showModal">
+      <template #header>
+        <h3>Пробрасываемый заголовок</h3>
+      </template>
+      <template #body>
+        <p>Пробрасываемое тело</p>
+      </template>
+      <template #footer>
+        Пробрасываемый подвал
+      </template>
+    </Modal>
+  </Teleport>
 
 </template>
 
-<style>
-
-circle {
-  fill: transparent;
-  stroke: black;
-}
-
-polygon {
-  fill: transparent;
-  stroke: limegreen;
-}
-
-text {
-  font-size: 10px;
-  fill: #666;
-}
-
-label {
-  display: inline-block;
-  margin-left: 10px;
-  width: 20px;
-}
-
-#raw {
-  position: absolute;
-  top: 0;
-  left: 300px;
-}
+<style scoped>
 
 </style>
---
+++ vue3/learning/src/components/Modal.vue
@@ -0,0 +1,86 @@
+<script setup>
+defineProps({
+  show: Boolean
+})
+
+const emit = defineEmits(['close'])
+</script>
+
+<template>
+  <Transition name="modal">
+    <div class="modal-mask" v-if="show">
+      <div class="modal-container">
+        <div class="modal-header">
+          <slot name="header">
+            <h3>Заголовок по умолчанию</h3>
+          </slot>
+        </div>
+        <div class="modal-body">
+          <slot name="body">
+            Тело по умолчанию
+          </slot>
+        </div>
+        <div class="modal-footer">
+          <slot name="footer">
+            Подвал по умолчанию
+          </slot>
+        </div>
+        <button class="modal-default-button" @click="emit('close')">Закрыть</button>
+      </div>
+    </div>
+  </Transition>
+
+
+
+</template>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1000;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  transition: opacity 0.3s ease;
+}
+
+.modal-container {
+  width: 300px;
+  margin: auto;
+  background-color: bisque;
+  padding: 20px 40px;
+  border-radius: 10px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+.modal-enter-from {
+  opacity: 0;
+}
+
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from .modal-container,
+.modal-leave-to .modal-container {
+  transform: scale(1.1);
+}
+
+</style>
