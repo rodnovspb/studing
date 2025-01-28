5e562742fcac260f0b5b3b0a9758121d381d6a8d
rodnov <rodnovspb@mail.ru>
2025-01-28 03:05:01 +0300

    up

--- vue3/learning2/vue/src/App.vue
+++ vue3/learning2/vue/src/App.vue
@@ -1,29 +1,37 @@
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed } from 'vue';
 
-const users = ref([
-  { name: 'Денис', age: 38 },
-  { name: 'Вася', age: 39 },
-  { name: 'Женя', age: 40 },
-  { name: 'Митя', age: 41 }
+const products = ref([
+  { title: 'Бананы', price: 1000 },
+  { title: 'Апельсины', price: 2000 },
+  { title: 'Арбузы', price: 3000 },
+  { title: 'Грибы', price: 4000 },
+
 ])
+
+const filtered = computed(() => products.value.filter((item) => item.startsWith(search.value)))
+
+const reverseQuery = computed(() => {
+  return search.value.split('').reverse().join('')
+})
+
+const search = ref('')
+
+
 
 </script>
 
 <template>
+  <input type="search" placeholder="Введите" v-model="search">
+  <div v-if="reverseQuery">{{ reverseQuery }}</div>
   <ul>
-    <li
-        v-for="(item, index) in users"
-        :key="index"
-        :class="{ red: item.age > 39 }"
-    >
-      {{ item.name }}</li>
+    <li v-for="product in filtered" :key="product">
+      {{ product }}
+    </li>
   </ul>
 
 </template>
 
 <style>
-  .red {
-    color: red;
-}
+
 </style>
