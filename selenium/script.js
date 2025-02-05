98d351c25ffed1cae5fca1899882168dbcd4ccea
rodnov <rodnovspb@mail.ru>
2025-02-05 23:29:46 +0300

    up

--- selenium/script.js
+++ selenium/script.js
@@ -1,26 +1,82 @@
+
+
 const { Builder, By, until, Key } = require('selenium-webdriver');
 
 const chrome = require('selenium-webdriver/chrome')
-const path = require('path');
+const path = require('path')
 
 
 let options = new chrome.Options()
 
-options.addArguments(`user-data-dir=${path.join(__dirname, 'profile')}`)
-options.addArguments(`profile-directory=Default`);
 
+// options.addArguments(`user-data-dir=${path.join(__dirname, 'profile')}`)
+// options.addArguments(`profile-directory=Default`);
+
+options.addArguments(['start-maximized'])
 
 ;(async function () {
 
     let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
 
+
+
     try {
-        await driver.get('https://2ip.io/');
+
+        await driver.get('https://uslugi.yandex.ru/profile/Avegaprint-1047311');
+
+        await driver.findElement(By.css('a.WorkerControls-Control_chat')).click();
+
+        await driver.findElement(By.xpath("//div[contains(text(), 'Я.Мессенджер')]")).click();
+
+        let frame = await driver.wait(until.elementLocated(By.css('iframe.ya-chat-base__iframe')), 8000);
+
+        await driver.switchTo().frame(frame);
+
+        let textarea = await driver.wait(until.elementLocated(By.xpath("//textarea[@placeholder='Сообщение…']")), 8000);
+
+        await textarea.sendKeys('Добрый день!', Key.ENTER)
+
+        // let frames = await driver.wait(until.elementsLocated(By.css('iframe.ya-chat-base__iframe')), 8000);
+
+        // // Проверка наличия iframe
+        // if (frames.length > 0) {
+        //     console.log('Iframe найден');
+        //     // Переключение на первый найденный iframe
+        //     await driver.switchTo().frame(frames[0]);
+        //
+        //     let textarea = await driver.wait(until.elementLocated(By.xpath("//textarea[@placeholder='Сообщение…']")), 8000);
+        //
+        //     await textarea.sendKeys('Добрый день!', Key.ENTER)
+        //
+        //
+        // } else {
+        //     console.log('Iframe не найден');
+        // }
+
+
+
+
+
+
+
+
+
 
     } catch (e) {
         console.log('Ошибка:', e); }
 
 
+
+    // const fs = require('fs');
+    // let proxy = require('selenium-webdriver/proxy');
+
+    // let proxyAddress = '136.243.175.104:47046'
+
+// options.setProxy(proxy.manual({http: proxyAddress, https: proxyAddress}));
+
+
+    // options.addArguments(`user-data-dir=${path.join(__dirname, 'profile')}`)
+    // options.addArguments(`profile-directory=Default`);
 
 
     // let elem = await driver.findElement(By.css('.divisions__card:nth-child(5)'));
